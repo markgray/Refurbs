b6ac3883f65be7d2c6e722cfff90c49e64fd18ac
markgray <mark.t.gray@gmail.com>
2023-05-03 08:32:01 -0400

    Initial commit kotlin version of BitmapAllocation

---
+++ BitmapAllocation/app/src/main/java/com/example/android/bitmapallocation/BitmapAllocation.kt
@@ -0,0 +1,132 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+@file:Suppress("ReplaceNotNullAssertionWithElvisReturn", "MemberVisibilityCanBePrivate")
+
+package com.example.android.bitmapallocation
+
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.widget.CheckBox
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * This example shows how to speed up bitmap loading and reduce garbage collection
+ * by reusing existing bitmaps.
+ *
+ *
+ * Watch the associated video for this demo on the DevBytes channel of developer.android.com,
+ * or on YouTube at [...](https://www.youtube.com/watch?v=rsQet4nBVi8).
+ */
+class BitmapAllocation : AppCompatActivity() {
+    // There are some assumptions in this demo app that don't carry over well to the real world:
+    // it assumes that all bitmaps are the same size and that loading all bitmaps as the activity
+    // starts is good enough. A real application would be take a more flexible and robust
+    // approach. But these assumptions are good enough for the purposes of this tutorial,
+    // which is about reusing existing bitmaps of the same size.
+    /**
+     * The current index into our array of image ids.
+     */
+    var mCurrentIndex: Int = 0
+
+    /**
+     * The current bitmap being displayed.
+     */
+    var mCurrentBitmap: Bitmap? = null
+
+    /**
+     * Bitmap options we use for all `BitmapFactory` calls.
+     */
+    var mBitmapOptions: BitmapFactory.Options? = null
+
+    /**
+     * Called when the activity is starting. First we call our super's implementation of `onCreate`,
+     * then we set our content view to our layout file R.layout.activity_bitmap_allocation. We initialize
+     * our variable `int[] imageIDs` with the resource id's of the 6 jpg's we display, initialize
+     * `CheckBox checkbox` by finding the view in our layout with id R.id.checkbox ("Reuse Bitmap"
+     * if checked the old bitmap will be reused), initialize `TextView durationTextview` by finding
+     * the view with id R.id.loadDuration (we will display the time it took to decode and display our images
+     * here), and initialize `ImageView imageview` by finding the view with id R.id.imageview (we will
+     * display our images here). We initialize our field `BitmapFactory.Options mBitmapOptions` with
+     * a new instance, and set its `inJustDecodeBounds` field to true (the decoder will return null
+     * (no bitmap), but the size fields of `mBitmapOptions` will still be set, allowing the caller
+     * to query the bitmap without having to allocate the memory for its pixels). We then call the
+     * `decodeResource` method of `BitmapFactory` to set the fields of `mBitmapOptions`
+     * given the jpg with resource id R.drawable.a. We then create a bitmap for `Bitmap mCurrentBitmap`
+     * using the `outWidth` field of `mBitmapOptions` as the width of the bitmap and the
+     * `outHeight` field as the height of the bitmap, using ARGB_8888 as the bitmap configuration
+     * (each pixel is stored on 4 bytes, with each channel (RGB and alpha for translucency) stored with
+     * 8 bits of precision). We then set the `inJustDecodeBounds` field of `mBitmapOptions`
+     * to false, set its `inBitmap` field to `mCurrentBitmap` (decode methods that take the
+     * Options object will attempt to reuse this bitmap when loading content), and set its `inSampleSize`
+     * field to 1 (no sub-sampling). We then call the `decodeResource` method of `BitmapFactory`
+     * to decode the jpg with resource id R.drawable.a into `Bitmap mCurrentBitmap`, then set the
+     * content of `ImageView imageview` to it. Finally we set the `OnClickListener` of
+     * `imageview` to an anonymous class which cycles through the resource id's in `imageIDs`
+     * displaying each jpg in turn and reusing `mCurrentBitmap` it the checkbox `checkbox` is
+     * checked.
+     *
+     * @param savedInstanceState we do not override `onSaveInstanceState` so do not use
+     */
+    @SuppressLint("SetTextI18n")
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bitmap_allocation)
+        val imageIDs = intArrayOf(R.drawable.a, R.drawable.b, R.drawable.c, R.drawable.d,
+            R.drawable.e, R.drawable.f)
+        val checkbox = findViewById<CheckBox>(R.id.checkbox)
+        val durationTextview = findViewById<TextView>(R.id.loadDuration)
+        val imageview = findViewById<ImageView>(R.id.imageview)
+
+        // Create bitmap to be re-used, based on the size of one of the bitmaps
+        mBitmapOptions = BitmapFactory.Options()
+        mBitmapOptions!!.inJustDecodeBounds = true
+        BitmapFactory.decodeResource(resources, R.drawable.a, mBitmapOptions)
+        mCurrentBitmap = Bitmap.createBitmap(mBitmapOptions!!.outWidth,
+            mBitmapOptions!!.outHeight, Bitmap.Config.ARGB_8888)
+        mBitmapOptions!!.inJustDecodeBounds = false
+        mBitmapOptions!!.inBitmap = mCurrentBitmap
+        mBitmapOptions!!.inSampleSize = 1
+        BitmapFactory.decodeResource(resources, R.drawable.a, mBitmapOptions)
+        imageview.setImageBitmap(mCurrentBitmap)
+
+        // When the user clicks on the image, load the next one in the list
+        imageview.setOnClickListener {
+            mCurrentIndex = (mCurrentIndex + 1) % imageIDs.size
+            var bitmapOptions: BitmapFactory.Options? = null
+            if (checkbox.isChecked) {
+                // Re-use the bitmap by using BitmapOptions.inBitmap
+                bitmapOptions = mBitmapOptions
+                bitmapOptions!!.inBitmap = mCurrentBitmap
+            }
+            val startTime = System.currentTimeMillis()
+            mCurrentBitmap = BitmapFactory.decodeResource(resources,
+                imageIDs[mCurrentIndex], bitmapOptions)
+            imageview.setImageBitmap(mCurrentBitmap)
+
+            // One way you can see the difference between reusing and not is through the
+            // timing reported here. But you can also see a huge impact in the garbage
+            // collector if you look at logcat with and without reuse. Avoiding garbage
+            // collection when possible, especially for large items like bitmaps,
+            // is always a good idea.
+            durationTextview.text = "Load took " +
+                (System.currentTimeMillis() - startTime)
+        }
+    }
+}
