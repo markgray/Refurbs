bb6ce05612cfe0d4a33cffe732818ec779adac41
markgray <mark.t.gray@gmail.com>
2023-05-19 07:28:35 -0400

    Initial commit FloatingActionButtonsBasic

---
+++ FloatingActionButtonBasic/Application/src/main/java/com/example/android/floatingactionbuttonbasic/FloatingActionButton.kt
@@ -0,0 +1,223 @@
+/*
+ * Copyright 2014, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+@file:Suppress("ReplaceNotNullAssertionWithElvisReturn")
+
+package com.example.android.floatingactionbuttonbasic
+
+
+import android.content.Context
+import android.graphics.Outline
+import android.util.AttributeSet
+import android.view.View
+import android.view.ViewOutlineProvider
+import android.widget.Checkable
+import android.widget.FrameLayout
+
+/**
+ * A Floating Action Button is a [android.widget.Checkable] view distinguished by a circled
+ * icon floating above the UI, with special motion behaviors.
+ * Perform inflation from XML and apply a class-specific base style from a theme attribute or
+ * style resource. Called by our three other constructors. First we call our super's constructor.
+ * In our `init` block:
+ * We call the `setClickable(true)` method to make our view clickable. We set the outline
+ * provider for this view to an anonymous `ViewOutlineProvider` whose `getOutline`
+ * sets our `Outline` to an oval fitting the height and width of our view. Finally we call
+ * the `setClipToOutline(true)` method to enable clipping to the outline, using the provider
+ * we set above.
+ *
+ * @param context The Context the view is running in, through which it can
+ * access the current theme, resources, etc.
+ * @param attrs The attributes of the XML tag that is inflating the view.
+ * @param defStyleAttr An attribute in the current theme that contains a
+ * reference to a style resource that supplies default values for
+ * the view. Can be 0 to not look for defaults.
+ * @param defStyleRes A resource identifier of a style resource that
+ * supplies default values for the view, used only if
+ * defStyleAttr is 0 or can not be found in the theme. Can be 0
+ * to not look for defaults.
+ */
+class FloatingActionButton @JvmOverloads constructor(
+    context: Context?,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = 0
+) : FrameLayout(context!!, attrs, defStyleAttr, defStyleRes), Checkable {
+    /**
+     * Interface definition for a callback to be invoked when the checked state
+     * of a compound button changes.
+     */
+    interface OnCheckedChangeListener {
+        /**
+         * Called when the checked state of a FAB has changed.
+         *
+         * @param fabView   The FAB view whose state has changed.
+         * @param isChecked The new checked state of buttonView.
+         */
+        fun onCheckedChanged(fabView: FloatingActionButton?, isChecked: Boolean)
+    }
+
+    /**
+     * A boolean that tells if the FAB is checked or not.
+     */
+    private var mChecked = false
+
+    /**
+     * A listener to communicate to that the FAB has changed it's state
+     */
+    private var mOnCheckedChangeListener: OnCheckedChangeListener? = null
+
+    init {
+        isClickable = true
+
+        // Set the outline provider for this view. The provider is given the outline which it can
+        // then modify as needed. In this case we set the outline to be an oval fitting the height
+        // and width.
+        outlineProvider = object : ViewOutlineProvider() {
+            /**
+             * Called to get the provider to populate the Outline. This method will be called by a
+             * View when its owned Drawables are invalidated, when the View's size changes, or if
+             * [View.invalidateOutline] is called explicitly. The input outline is empty and
+             * has an alpha of `1.0f`. We set our parameter `Outline outline` to an
+             * oval fitting the height and width of our view.
+             *
+             * @param view The view building the outline.
+             * @param outline The empty outline to be populated.
+             */
+            override fun getOutline(view: View, outline: Outline) {
+                outline.setOval(0, 0, width, height)
+            }
+        }
+
+        // Finally, enable clipping to the outline, using the provider we set above
+        clipToOutline = true
+    }
+
+    /**
+     * Sets the checked/unchecked state of the FAB, part of the `Checkable` interface. If our
+     * field `mChecked` is already equal to our parameter `checked` we return having
+     * done nothing. Otherwise we set `mChecked` to `checked` and call the method
+     * `refreshDrawableState` to force our view to update its drawable state. This will cause
+     * the `drawableStateChanged` method to be called on this view. If our field
+     * `OnCheckedChangeListener mOnCheckedChangeListener` we call its `onCheckedChanged`
+     * method with this as the FAB view whose state has changed and `checked` as the new checked
+     * state.
+     *
+     * @param checked value to set our state to.
+     */
+    override fun setChecked(checked: Boolean) {
+        // If trying to set the current state, ignore.
+        if (checked == mChecked) {
+            return
+        }
+        mChecked = checked
+
+        // Now refresh the drawable state (so the icon changes)
+        refreshDrawableState()
+        if (mOnCheckedChangeListener != null) {
+            mOnCheckedChangeListener!!.onCheckedChanged(this, checked)
+        }
+    }
+
+    /**
+     * Register a callback to be invoked when the checked state of this button changes. We just save
+     * our parameter `OnCheckedChangeListener listener` in our field `mOnCheckedChangeListener`.
+     *
+     * @param listener the callback to call on checked state change
+     */
+    fun setOnCheckedChangeListener(listener: OnCheckedChangeListener?) {
+        mOnCheckedChangeListener = listener
+    }
+
+    /**
+     * Returns the current checked state of the view, which is our field `boolean mChecked`.
+     *
+     * @return The current checked state of the view
+     */
+    override fun isChecked(): Boolean {
+        return mChecked
+    }
+
+    /**
+     * Change the checked state of the view to the inverse of its current state, we just call our
+     * `setChecked` method with the inverse of our field `boolean mChecked`.
+     */
+    override fun toggle() {
+        isChecked = !mChecked
+    }
+
+    /**
+     * Override performClick() so that we can toggle the checked state when the view is clicked, we
+     * call our method `toggle` to change the checked state of the view to the inverse of its
+     * current state, then return the value returned by our super's implementation of `performClick`.
+     *
+     * @return True there was an assigned OnClickListener that was called, false
+     * otherwise is returned.
+     */
+    override fun performClick(): Boolean {
+        toggle()
+        return super.performClick()
+    }
+
+    /**
+     * This is called during layout when the size of this view has changed. If you were just added
+     * to the view hierarchy, you're called with the old values of 0. We call our super's implementation
+     * of `onSizeChanged`, then call the `invalidateOutline` to rebuild this View's Outline
+     * from its [outline provider][ViewOutlineProvider] so that it corresponds to our new size.
+     *
+     * @param w Current width of this view.
+     * @param h Current height of this view.
+     * @param oldw Old width of this view.
+     * @param oldh Old height of this view.
+     */
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        // As we have changed size, we should invalidate the outline so that is the the
+        // correct size
+        invalidateOutline()
+    }
+
+    /**
+     * Generate the new [android.graphics.drawable.Drawable] state for this view. This is called
+     * by the view system when the cached Drawable state is determined to be invalid. To retrieve the
+     * current state, you should use [.getDrawableState]. We initialize `int[] drawableState`
+     * with the array returned by our super's implementation of `onCreateDrawableState` when we
+     * request one more drawable state than our caller requested. If our method `isChecked` returns
+     * true (we are currently checked) we call the `mergeDrawableStates` method to merge our
+     * CHECKED_STATE_SET into `drawableState`. In any case we return `drawableState` to our
+     * caller.
+     *
+     * @param extraSpace if non-zero, this is the number of extra entries you would like in the
+     * returned array in which you can place your own states.
+     * @return Returns an array holding the current `Drawable` state of the view.
+     */
+    override fun onCreateDrawableState(extraSpace: Int): IntArray {
+        val drawableState = super.onCreateDrawableState(extraSpace + 1)
+        if (isChecked) {
+            mergeDrawableStates(drawableState, CHECKED_STATE_SET)
+        }
+        return drawableState
+    }
+
+    companion object {
+        /**
+         * An array of states.
+         */
+        private val CHECKED_STATE_SET = intArrayOf(
+            android.R.attr.state_checked
+        )
+    }
+}
---
+++ FloatingActionButtonBasic/Application/src/main/java/com/example/android/floatingactionbuttonbasic/FloatingActionButtonBasicFragment.kt
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2014, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+@file:Suppress("ReplaceNotNullAssertionWithElvisReturn")
+
+package com.example.android.floatingactionbuttonbasic
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.android.common.logger.Log
+
+/**
+ * This fragment inflates a layout with two Floating Action Buttons and acts as a listener to
+ * changes on them.
+ */
+class FloatingActionButtonBasicFragment : Fragment(), FloatingActionButton.OnCheckedChangeListener {
+    /**
+     * Called to have the fragment instantiate its user interface view. We initialize `View rootView`
+     * to the view that our parameter `LayoutInflater inflater` inflates from our layout file
+     * R.layout.fab_layout using our parameter `ViewGroup container` for LayoutParams without
+     * attaching to it. We initialize `FloatingActionButton fab1` by finding the view in `rootView`
+     * with id R.id.fab_1 and set its `OnCheckedChangeListener` to this, and we initialize
+     * `FloatingActionButton fab2` by finding the view in `rootView` with id R.id.fab_2
+     * and set its `OnCheckedChangeListener` to this. Finally we return `rootView` to the
+     * caller.
+     *
+     * @param inflater The LayoutInflater object that can be used to inflate
+     * any views in the fragment,
+     * @param container If non-null, this is the parent view that the fragment's
+     * UI should be attached to.  The fragment should not add the view itself,
+     * but this can be used to generate the LayoutParams of the view.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     * @return Return the View for the fragment's UI, or null.
+     */
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val rootView = inflater.inflate(R.layout.fab_layout, container, false)
+
+        // Make this {@link Fragment} listen for changes in both FABs.
+        val fab1 = rootView.findViewById<FloatingActionButton>(R.id.fab_1)
+        fab1.setOnCheckedChangeListener(this)
+        val fab2 = rootView.findViewById<FloatingActionButton>(R.id.fab_2)
+        fab2.setOnCheckedChangeListener(this)
+        return rootView
+    }
+
+    /**
+     * Called when the checked state of a FAB has changed. We switch on the id of our parameter
+     * `FloatingActionButton fabView`:
+     *
+     *  *
+     * R.id.fab_1: We log the formatted string "FAB 1 was checked" is our parameter `isChecked`
+     * is true, or the string "FAB 1 was unchecked" is our parameter `isChecked` is false,
+     * then break.
+     *
+     *  *
+     * R.id.fab_2: We log the formatted string "FAB 2 was checked" is our parameter `isChecked`
+     * is true, or the string "FAB 2 was unchecked" is our parameter `isChecked` is false,
+     * then break.
+     *
+     *
+     *
+     * @param fabView   The FAB view whose state has changed.
+     * @param isChecked The new checked state of buttonView.
+     */
+    override fun onCheckedChanged(fabView: FloatingActionButton?, isChecked: Boolean) {
+        // When a FAB is toggled, log the action.
+        when (fabView!!.id) {
+            R.id.fab_1 -> Log.d(TAG, String.format("FAB 1 was %s.", if (isChecked) "checked" else "unchecked"))
+            R.id.fab_2 -> Log.d(TAG, String.format("FAB 2 was %s.", if (isChecked) "checked" else "unchecked"))
+            else -> {}
+        }
+    }
+
+    companion object {
+        /**
+         * TAG used for logging.
+         */
+        private const val TAG = "FloatingActionButtonBasicFragment"
+    }
+}
