358d739bbaeabbc9a6daa4d40a81c3f29a4faf87
markgray <mark.t.gray@gmail.com>
2023-05-24 06:45:26 -0400

    Copied over kotlin version of common/logger in JobScheduler

--- JobScheduler/Application/src/main/java/com/example/android/common/logger/LogWrapper.kt
+++ JobScheduler/Application/src/main/java/com/example/android/common/logger/LogWrapper.kt
@@ -13,63 +13,53 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.android.common.logger;
+@file:Suppress("ReplaceNotNullAssertionWithElvisReturn")
 
-import android.util.Log;
+package com.example.android.common.logger
+
+import android.util.Log
 
 /**
  * Helper class which wraps Android's native Log utility in the Logger interface.  This way
  * normal DDMS output can be one of the many targets receiving and outputting logs simultaneously.
  */
-public class LogWrapper implements LogNode {
-
-    // For piping:  The next node to receive Log data after this one has done its work.
-    private LogNode mNext;
-
+class LogWrapper : LogNode {
     /**
-     * Returns the next LogNode in the linked list.
+     * For piping:  The next node to receive Log data after this one has done its work.
      */
-    public LogNode getNext() {
-        return mNext;
-    }
-
-    /**
-     * Sets the LogNode data will be sent to..
-     */
-    public void setNext(LogNode node) {
-        mNext = node;
-    }
+    var next: LogNode? = null
 
     /**
      * Prints data out to the console using Android's native log mechanism.
+     *
      * @param priority Log level of the data being logged.  Verbose, Error, etc.
      * @param tag Tag for for the log data.  Can be used to organize log statements.
      * @param msg The actual message to be logged. The actual message to be logged.
      * @param tr If an exception was thrown, this can be sent along for the logging facilities
-     *           to extract and print useful information.
+     * to extract and print useful information.
      */
-    @Override
-    public void println(int priority, String tag, String msg, Throwable tr) {
+    override fun println(priority: Int, tag: String?, msg: String?, tr: Throwable?) {
         // There actually are log methods that don't take a msg parameter.  For now,
         // if that's the case, just convert null to the empty string and move on.
-        String useMsg = msg;
+        var msgLocal: String? = msg
+        var useMsg = msgLocal
         if (useMsg == null) {
-            useMsg = "";
+            useMsg = ""
         }
 
         // If an exeption was provided, convert that exception to a usable string and attach
         // it to the end of the msg method.
         if (tr != null) {
-            msg += "\n" + Log.getStackTraceString(tr);
+            msgLocal += "\n${Log.getStackTraceString(tr)}"
         }
 
         // This is functionally identical to Log.x(tag, useMsg);
         // For instance, if priority were Log.VERBOSE, this would be the same as Log.v(tag, useMsg)
-        Log.println(priority, tag, useMsg);
+        Log.println(priority, tag, useMsg)
 
         // If this isn't the last node in the chain, move things along.
-        if (mNext != null) {
-            mNext.println(priority, tag, msg, tr);
+        if (next != null) {
+            next!!.println(priority, tag, msgLocal, tr)
         }
     }
 }
