4659270a010677d08a65d5b209a12ff0e8e47219
markgray <mark.t.gray@gmail.com>
2023-06-10 07:27:52 -0400

    Initial commit PropertyAnimations

---
+++ PropertyAnimations/app/src/main/java/com/example/android/propertyanimations/PropertyAnimations.kt
@@ -0,0 +1,166 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+@file:Suppress("ReplaceNotNullAssertionWithElvisReturn", "MemberVisibilityCanBePrivate")
+
+package com.example.android.propertyanimations
+
+import android.animation.Animator
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
+import android.animation.ValueAnimator
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.CheckBox
+
+/**
+ * This example shows how to use property animations, specifically ObjectAnimator, to perform
+ * various view animations. Compare this approach to that of the ViewAnimations demo, which
+ * shows how to achieve similar effects using the pre-3.0 animation APIs.
+ *
+ *
+ * Watch the associated video for this demo on the DevBytes channel of developer.android.com
+ * or on YouTube at [...](https://www.youtube.com/watch?v=3UbJhmkeSig)
+ */
+class PropertyAnimations : Activity() {
+    /**
+     * `CheckBox` in our layout with id R.id.checkbox ("Use Animation Resources") selects the
+     * use of objectAnimator xml resource files instead of the java constructed animations
+     */
+    var mCheckBox: CheckBox? = null
+
+    /**
+     * Called when the activity is starting. First we call our super's implementation of `onCreate`,
+     * then we set our content view to our layout file R.layout.activity_property_animations. We initialize
+     * our field `CheckBox mCheckBox` by finding the view with id R.id.checkbox ("Use Animation
+     * Resources"), then initialize `Button alphaButton` by finding the view with id R.id.alphaButton
+     * ("Alpha"), `Button translateButton` by finding the view with id R.id.translateButton ("Translate"),
+     * `Button rotateButton` by finding the view with id R.id.rotateButton ("Rotate"), `Button scaleButton`
+     * by finding the view with id R.id.scaleButton ("Scale"), and `Button setButton` by finding the view
+     * with id R.id.setButton ("Set"). We initialize `ObjectAnimator alphaAnimation` with an instance
+     * which will animate the ALPHA property of `alphaButton` to 0, set its repeat count to 1, and set
+     * its repeat mode to REVERSE. We initialize `ObjectAnimator translateAnimation` with an instance
+     * which will animate the TRANSLATION_X property of `translateButton` to 800, set its repeat count
+     * to 1, and set its repeat mode to REVERSE. We initialize `ObjectAnimator rotateAnimation` with an
+     * instance which will animate the ROTATION property of `rotateButton` to 360, set its repeat count
+     * to 1, and set its repeat mode to REVERSE. We initialize `PropertyValuesHolder pvhX` with an instance
+     * which will animate the SCALE_X property to 2, initialize `PropertyValuesHolder pvhY` with an instance
+     * which will animate the SCALE_Y property to 2, then initialize `ObjectAnimator scaleAnimation` with
+     * an instance which will apply the animations `pvhX` and `pvhY` to `scaleButton`, set
+     * its repeat count to 1, and set its repeat mode to REVERSE. We initialize `AnimatorSet setAnimation`
+     * with a new instance, create a `Builder` for it that will use `translateAnimation` as a dependency
+     * to run after `alphaAnimation` and before `rotateAnimation`, then create another `Builder`
+     * for it that will use `rotateAnimation` as a dependency to have it run before `scaleAnimation`.
+     *
+     *
+     * Having set up the animations to be used we call our method `setupAnimation` to have it add an
+     * `OnClickListener` to `alphaButton` whose `onClick` override runs `alphaAnimation`
+     * if `mCheckBox` is unchecked or runs the xml animation with resource id R.animator.fade if it is
+     * checked, call our method `setupAnimation` to have it add an `OnClickListener` to
+     * `translateButton` whose `onClick` override runs `translateAnimation` on it if
+     * `mCheckBox` is unchecked or runs the xml animation with resource id R.animator.move if it is
+     * checked, call our method `setupAnimation` to have it add an `OnClickListener` to
+     * `rotateButton` whose `onClick` override runs `rotateAnimation` if `mCheckBox`
+     * is unchecked or runs the xml animation with resource id R.animator.spin if it is checked, call our
+     * method `setupAnimation` to have it add an `OnClickListener` to `scaleButton`
+     * whose `onClick` override runs `scaleAnimation` if `mCheckBox` is unchecked or
+     * runs the xml animation with resource id R.animator.scale if it is checked, and finally call our
+     * method `setupAnimation` to have it add an `OnClickListener` to `setButton` whose
+     * `onClick` override will run `setAnimation` to animate the other buttons if `mCheckBox`
+     * is unchecked or runs the xml animation with resource id R.animator.combo on itself if it is checked.
+     *
+     * @param savedInstanceState we do not override `onSaveInstanceState` so do not use.
+     */
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_property_animations)
+        mCheckBox = findViewById(R.id.checkbox)
+        val alphaButton = findViewById<Button>(R.id.alphaButton)
+        val translateButton = findViewById<Button>(R.id.translateButton)
+        val rotateButton = findViewById<Button>(R.id.rotateButton)
+        val scaleButton = findViewById<Button>(R.id.scaleButton)
+        val setButton = findViewById<Button>(R.id.setButton)
+
+        // Fade the button out and back in
+        val alphaAnimation = ObjectAnimator.ofFloat(alphaButton, View.ALPHA, 0f)
+        alphaAnimation.repeatCount = 1
+        alphaAnimation.repeatMode = ValueAnimator.REVERSE
+
+        // Move the button over to the right and then back
+        val translateAnimation = ObjectAnimator.ofFloat(translateButton, View.TRANSLATION_X, 800f)
+        translateAnimation.repeatCount = 1
+        translateAnimation.repeatMode = ValueAnimator.REVERSE
+
+        // Spin the button around in a full circle
+        val rotateAnimation = ObjectAnimator.ofFloat(rotateButton, View.ROTATION, 360f)
+        rotateAnimation.repeatCount = 1
+        rotateAnimation.repeatMode = ValueAnimator.REVERSE
+
+        // Scale the button in X and Y. Note the use of PropertyValuesHolder to animate
+        // multiple properties on the same object in parallel.
+        val pvhX = PropertyValuesHolder.ofFloat(View.SCALE_X, 2f)
+        val pvhY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 2f)
+        val scaleAnimation = ObjectAnimator.ofPropertyValuesHolder(scaleButton, pvhX, pvhY)
+        scaleAnimation.repeatCount = 1
+        scaleAnimation.repeatMode = ValueAnimator.REVERSE
+
+        // Run the animations above in sequence
+        val setAnimation = AnimatorSet()
+        setAnimation.play(translateAnimation).after(alphaAnimation).before(rotateAnimation)
+        setAnimation.play(rotateAnimation).before(scaleAnimation)
+        setupAnimation(alphaButton, alphaAnimation, R.animator.fade)
+        setupAnimation(translateButton, translateAnimation, R.animator.move)
+        setupAnimation(rotateButton, rotateAnimation, R.animator.spin)
+        setupAnimation(scaleButton, scaleAnimation, R.animator.scale)
+        setupAnimation(setButton, setAnimation, R.animator.combo)
+    }
+
+    /**
+     * Sets the `OnClickListener` of our parameter `View view` to an anonymous instance which
+     * runs the animation `Animator animation` if `CheckBox mCheckBox` is unchecked, or runs
+     * the xml `Animator` whose resource id is `int animationID` on the `View view`
+     * if `CheckBox mCheckBox` is checked.
+     *
+     * @param view `View` we are to add an `OnClickListener` to
+     * @param animation `Animator` to run if `CheckBox mCheckBox` is unchecked
+     * @param animationID Resource id of an xml `Animator` to run if `CheckBox mCheckBox` is checked
+     */
+    private fun setupAnimation(view: View, animation: Animator, animationID: Int) {
+        view.setOnClickListener(View.OnClickListener { v ->
+            /**
+             * Called when a view has been clicked. If `CheckBox mCheckBox` is checked we initialize
+             * `Animator anim` by inflating the xml `Animator` with the resource id `int animationID`,
+             * set its target to `v`, start it running and return. If `CheckBox mCheckBox` is unchecked
+             * we just start `Animator animation` running.
+             *
+             * @param v The view that was clicked.
+             */
+            // If the button is checked, load the animation from the given resource
+            // id instead of using the passed-in animation parameter. See the xml files
+            // for the details on those animations.
+            if (mCheckBox!!.isChecked) {
+                val anim = AnimatorInflater.loadAnimator(this@PropertyAnimations, animationID)
+                anim.setTarget(v)
+                anim.start()
+                return@OnClickListener
+            }
+            animation.start()
+        })
+    }
+}
