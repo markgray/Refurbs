e45568d4ee9ca6c6c8ae7d111a9e1cbd1e40e3d2
markgray <mark.t.gray@gmail.com>
2023-09-15 07:05:05 -0400

    Updated gradle dependency in RoomwithContentProviders

--- RoomwithContentProviders/app/src/main/java/com/example/android/contentprovidersample/data/SampleDatabase.kt
+++ RoomwithContentProviders/app/src/main/java/com/example/android/contentprovidersample/data/SampleDatabase.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@file:Suppress("unused", "ReplaceNotNullAssertionWithElvisReturn")
+@file:Suppress("unused")
 
 package com.example.android.contentprovidersample.data
 
