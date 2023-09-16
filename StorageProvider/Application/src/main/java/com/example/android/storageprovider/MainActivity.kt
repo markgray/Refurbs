ce0bd270fc1afdd744e913d422a0ebfd4230d66a
markgray <mark.t.gray@gmail.com>
2023-09-16 06:11:18 -0400

    Updated gradle dependency in StorageProvider

--- StorageProvider/Application/src/main/java/com/example/android/storageprovider/MainActivity.kt
+++ StorageProvider/Application/src/main/java/com/example/android/storageprovider/MainActivity.kt
@@ -95,7 +95,6 @@ class MainActivity : SampleActivityBase() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             logFragment.logView!!.setTextAppearance(R.style.Log)
         } else {
-            @Suppress("DEPRECATION") // Needed for SDK older than M
             logFragment.logView!!.setTextAppearance(this, R.style.Log)
         }
         logFragment.logView!!.setBackgroundColor(Color.WHITE)
