396369aa99e2199fc314c98f0be2d613e23bc14b
Mark Gray <mark.t.gray@gmail.com>
2023-09-16 03:59:08 -0400

    Updated gradle dependency in SlidingFragments

--- SlidingFragments/app/src/main/java/com/example/android/slidingfragments/TextFragment.kt
+++ SlidingFragments/app/src/main/java/com/example/android/slidingfragments/TextFragment.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@file:Suppress("ReplaceNotNullAssertionWithElvisReturn", "MemberVisibilityCanBePrivate")
+@file:Suppress("ReplaceNotNullAssertionWithElvisReturn")
 
 package com.example.android.slidingfragments
 
