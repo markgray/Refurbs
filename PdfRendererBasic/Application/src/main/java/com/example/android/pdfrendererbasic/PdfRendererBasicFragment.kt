4a03b17613a76ff181973144ecb74b8c487e5d19
markgray <mark.t.gray@gmail.com>
2023-10-27 05:21:29 -0400

    Tidied up comments in PdfRendererBasic

--- PdfRendererBasic/Application/src/main/java/com/example/android/pdfrendererbasic/PdfRendererBasicFragment.kt
+++ PdfRendererBasic/Application/src/main/java/com/example/android/pdfrendererbasic/PdfRendererBasicFragment.kt
@@ -18,6 +18,7 @@
 package com.example.android.pdfrendererbasic
 
 import android.content.Context
+import android.content.res.AssetManager
 import android.graphics.Bitmap
 import android.graphics.pdf.PdfRenderer
 import android.os.Bundle
@@ -32,6 +33,7 @@ import androidx.fragment.app.Fragment
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
+import java.io.InputStream
 
 /**
  * This fragment has a big [ImageView] that shows PDF pages, and 2 [Button]s to move between pages.
@@ -79,38 +81,41 @@ class PdfRendererBasicFragment
     private var mPageIndex = 0
 
     /**
-     * Called to have the fragment instantiate its user interface view. We return the `View`
-     * that our parameter `LayoutInflater inflater` inflates from our layout file
-     * R.layout.fragment_pdf_renderer_basic, using our parameter `ViewGroup container` for the
+     * Called to have the fragment instantiate its user interface view. We return the [View] that
+     * our [LayoutInflater] parameter [inflater] inflates from our layout file
+     * [R.layout.fragment_pdf_renderer_basic], using our [ViewGroup] parameter [container] for the
      * LayoutParams of the view without attaching to it.
      *
-     * @param inflater The LayoutInflater object that can be used to inflate
+     * @param inflater The [LayoutInflater] object that can be used to inflate
      * any views in the fragment,
-     * @param container If non-null, this is the parent view that the fragment's
-     * UI should be attached to.  The fragment should not add the view itself,
+     * @param container If non-`null`, this is the parent view that the fragment's
+     * UI will be attached to.  The fragment should not add the view itself,
      * but this can be used to generate the LayoutParams of the view.
-     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * @param savedInstanceState If non-`null`, this fragment is being re-constructed
      * from a previous saved state as given here.
-     * @return Return the View for the fragment's UI.
+     * @return Return the [View] for the fragment's UI.
      */
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_pdf_renderer_basic, container, false)
     }
 
     /**
-     * Called immediately after [.onCreateView]
-     * has returned, but before any saved state has been restored in to the view. First we call our
-     * super's implementation of `onViewCreated` then we initialize our field `ImageView mImageView`
-     * by finding the view with id R.id.image, our field `Button mButtonPrevious` by finding the
-     * view with id R.id.previous, and our field `Button mButtonNext` by finding the view with
-     * id R.id.next. We then set the `OnClickListener` of both `mButtonPrevious` and
-     * `mButtonNext` to this. We initialize our field `int mPageIndex` to 0, then if our
-     * parameter `Bundle savedInstanceState` is not null we set `mPageIndex` to the int
-     * stored under the key STATE_CURRENT_PAGE_INDEX ("current_page_index") in `savedInstanceState`
-     * defaulting to 0.
+     * Called immediately after [onCreateView] has returned, but before any saved state has been
+     * restored in to the view. First we call our super's implementation of `onViewCreated` then we
+     * initialize our [ImageView] field [mImageView] by finding the view with id [R.id.image], our
+     * [Button] field [mButtonPrevious] by finding the view with id [R.id.previous], and our
+     * [Button] field [mButtonNext] by finding the view with id [R.id.next]. We then set the
+     * [View.OnClickListener] of both [mButtonPrevious] and [mButtonNext] to this. We initialize our
+     * [Int] field [mPageIndex] to 0, then if our [Bundle] parameter [savedInstanceState] is not
+     * `null` we set [Int] field [mPageIndex] to the [Int] stored under the key
+     * [STATE_CURRENT_PAGE_INDEX] ("current_page_index") in [savedInstanceState] defaulting to 0.
      *
-     * @param view The View returned by [.onCreateView].
-     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * @param view The [View] returned by [onCreateView].
+     * @param savedInstanceState If non-`null`, this fragment is being re-constructed
      * from a previous saved state as given here.
      */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -131,10 +136,10 @@ class PdfRendererBasicFragment
 
     /**
      * Called when the Fragment is visible to the user. First we call our super's implementation of
-     * `onStart`. Then wrapped in a try block intended to catch and toast IOException to the
-     * user we call our method `openRenderer` to set up our `PdfRenderer` to render our
-     * pdf file. We then call our method `showPage` to show the page of PDF at page index
-     * `mPageIndex` on the screen.
+     * `onStart`. Then wrapped in a try block intended to catch and toast [IOException] to the
+     * user we call our method [openRenderer] to set up our [PdfRenderer] to render our pdf file.
+     * We then call our method [showPage] to show the page of the PDF at page index [Int] field
+     * [mPageIndex] on the screen.
      */
     override fun onStart() {
         super.onStart()
@@ -149,9 +154,9 @@ class PdfRendererBasicFragment
 
     /**
      * Called when the Fragment is no longer started. Wrapped in a try block intended to catch and
-     * log IOException we call our method `closeRenderer` to close the current `Page`
-     * `mCurrentPage`, our `PdfRenderer`, and the file descriptor `mFileDescriptor`.
-     * We then call our super's implementation of `onStop`.
+     * log [IOException] we call our method [closeRenderer] to close the current [PdfRenderer.Page]
+     * in [mCurrentPage], our [PdfRenderer], and the file descriptor [mFileDescriptor]. We then call
+     * our super's implementation of `onStop`.
      */
     override fun onStop() {
         try {
@@ -163,20 +168,17 @@ class PdfRendererBasicFragment
     }
 
     /**
-     * Called to ask the fragment to save its current dynamic state, so it
-     * can later be reconstructed in a new instance of its process is
-     * restarted.  If a new instance of the fragment later needs to be
-     * created, the data you place in the Bundle here will be available
-     * in the Bundle given to [.onCreate],
-     * [.onCreateView], and
-     * [.onActivityCreated].
+     * Called to ask the fragment to save its current dynamic state, so it can later be
+     * reconstructed in a new instance if its process is restarted.  If a new instance of
+     * the fragment later needs to be created, the data you place in the [Bundle] here will
+     * be available in the Bundle given to [onCreate], [onCreateView], and [onActivityCreated].
      *
+     * First we call our super's implementation of `onSaveInstanceState`, then if our
+     * [PdfRenderer.Page] field [mCurrentPage] is not `null` we add the page index of [mCurrentPage]
+     * to our [Bundle] parameter [outState] under the key [STATE_CURRENT_PAGE_INDEX]
+     * ("current_page_index").
      *
-     * First we call our super's implementation of `onSaveInstanceState`, then if our field
-     * `Page mCurrentPage` is not null we add the page index of `mCurrentPage` to our
-     * parameter `Bundle outState` under the key STATE_CURRENT_PAGE_INDEX ("current_page_index").
-     *
-     * @param outState Bundle in which to place your saved state.
+     * @param outState the [Bundle] in which to place your saved state.
      */
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
@@ -186,27 +188,27 @@ class PdfRendererBasicFragment
     }
 
     /**
-     * Sets up a [android.graphics.pdf.PdfRenderer] and related resources. First we initialize
-     * `File file` using the absolute path to the application specific cache directory on the
-     * filesystem for the parent path, and FILENAME ("sample.pdf") as the child pathname. If this file
-     * does not exist (we have not been run before) we initialize `InputStream asset` by using
-     * an AssetManager instance for the application's package to open the asset file named FILENAME,
-     * initialize `FileOutputStream output` with a new instance to write to the file represented
-     * by `File file`. We allocate 1024 bytes for `byte[] buffer`, and declare `int size`.
-     * We then loop, setting `size` to the number of bytes that the `read` method of `asset`
-     * reads into `buffer` and writing `size` bytes from `buffer` to `output`
-     * until `size` is equal to -1 (end of file). Then we close both `asset` and `output`.
+     * Sets up a [PdfRenderer] and related resources. First we initialize [File] variable `val file`
+     * using the absolute path to the application specific cache directory on the filesystem for the
+     * parent path, and [FILENAME] ("sample.pdf") as the child pathname. If this file does not exist
+     * (we have not been run before) we initialize [InputStream] variable `val asset` by using an
+     * [AssetManager] instance for the application's package to open the asset file named [FILENAME],
+     * initialize [FileOutputStream] variable `val output` with a new instance to write to the file
+     * represented by [File] variable `file`. We allocate 1024 bytes for [ByteArray] variable
+     * `val buffer`, and declare [Int] variable `var size`. We then loop, setting `size` to the
+     * number of bytes that the [InputStream.read] method of `asset` reads into `buffer` and writing
+     * `size` bytes from `buffer` to `output` until `size` is equal to -1 (end of file). Then we
+     * close both `asset` and `output`.
      *
+     * Now that we know that [File] variable `file` exists we initialize our [ParcelFileDescriptor]
+     * field [mFileDescriptor] with a new instance to access [File] variable `file` in
+     * [ParcelFileDescriptor.MODE_READ_ONLY] mode. If [mFileDescriptor] is not `null` we initialize
+     * our [PdfRenderer] field [mPdfRenderer] with a new instance constructed to read from
+     * [mFileDescriptor].
      *
-     * Now that we know that `File file` exists we initialize our field `ParcelFileDescriptor mFileDescriptor`
-     * with a new instance to access `File file` in MODE_READ_ONLY mode. If `mFileDescriptor`
-     * is not null we initialize our field `PdfRenderer mPdfRenderer` with a new instance constructed
-     * to read from `mFileDescriptor`.
-     *
-     * @param context `Context` to use to access the application specific cache directory, and
-     * the AssetManager instance for the application's package (in order to read our
-     * example PDF from our assets). It is supplied by the `getActivity` method
-     * in our `onStart` override.
+     * @param context the [Context] to use to access the application specific cache directory, and
+     * the [AssetManager] instance for the application's package (in order to read our example PDF
+     * from our assets). It is supplied by the [getActivity] method in our `onStart` override.
      * @throws IOException if an I/O error occurs.
      */
     @Throws(IOException::class)
@@ -216,7 +218,7 @@ class PdfRendererBasicFragment
         if (!file.exists()) {
             // Since PdfRenderer cannot handle the compressed asset file directly, we copy it into
             // the cache directory.
-            val asset = requireContext().assets.open(FILENAME)
+            val asset: InputStream = requireContext().assets.open(FILENAME)
             val output = FileOutputStream(file)
             val buffer = ByteArray(1024)
             var size: Int
@@ -234,14 +236,14 @@ class PdfRendererBasicFragment
     }
 
     /**
-     * Closes the [android.graphics.pdf.PdfRenderer] and related resources. If our field
-     * `Page mCurrentPage` is not null we call its `close` method to close the `Page`.
-     * We then call the `close` method of our field `PdfRenderer mPdfRenderer` to close
-     * the renderer, and the `close` method of our field `ParcelFileDescriptor mFileDescriptor`
-     * to close the ParcelFileDescriptor as well as the underlying OS resources allocated to represent
-     * the stream.
+     * Closes the [PdfRenderer] and related resources. If our [PdfRenderer.Page] field [mCurrentPage]
+     * is not `null` we call its [PdfRenderer.Page.close] method to close the `Page`.
+     * We then call the [PdfRenderer.close] method of our [PdfRenderer] field [mPdfRenderer] to close
+     * the renderer, and the [ParcelFileDescriptor.close] method of our [ParcelFileDescriptor] field
+     * [mFileDescriptor] to close the [ParcelFileDescriptor] as well as the underlying OS resources
+     * allocated to represent the stream.
      *
-     * @throws java.io.IOException When the PDF file cannot be closed.
+     * @throws [IOException] When the PDF file cannot be closed.
      */
     @Throws(IOException::class)
     private fun closeRenderer() {
@@ -254,16 +256,16 @@ class PdfRendererBasicFragment
 
     /**
      * Shows the specified page of PDF to the screen. If the number of pages in the document is less
-     * than or equal to the request page index `index` we return having done nothing. If the
-     * field `Page mCurrentPage` is not null we call its `close` method to close the page.
-     * We then set `mCurrentPage` to the `Page` that the `openPage` method of our
-     * field `PdfRenderer mPdfRenderer` opens for rendering for the page index `index`.
-     * We initialize `Bitmap bitmap` with a ARGB_8888 bitmap created for the width and height
-     * of `mCurrentPage`. We then use the `render` method of `mCurrentPage` to
-     * render the page's content for display on a screen into `bitmap`, and set that image as
-     * the content of our field `ImageView mImageView`. Finally we call our method `updateUi`
-     * to enable or disable the control buttons based on the current index, and set the activity's
-     * title to reflect the page number.
+     * than or equal to the request page index in [Int] parameter [index] we return having done
+     * nothing. If the [PdfRenderer.Page] field [mCurrentPage] is not `null` we call its
+     * [PdfRenderer.Page.close] method to close the page. We then set [mCurrentPage] to the `Page`
+     * that the [PdfRenderer.openPage] method of our [PdfRenderer] field [mPdfRenderer] opens for
+     * rendering for the page index of [Int] parameter [index]. We initialize [Bitmap] variable
+     * `val bitmap` with a ARGB_8888 bitmap created for the width and height of [mCurrentPage]. We
+     * then use the [PdfRenderer.Page.render] method of [mCurrentPage] to render the page's content
+     * for display on a screen into `bitmap`, and set that image as the content of our [ImageView]
+     * field [mImageView]. Finally we call our method [updateUi] to enable or disable the control
+     * buttons based on the current index, and set the activity's title to reflect the page number.
      *
      * @param index The page index.
      */
@@ -278,13 +280,21 @@ class PdfRendererBasicFragment
         // Use `openPage` to open a specific page in PDF.
         mCurrentPage = mPdfRenderer!!.openPage(index)
         // Important: the destination bitmap must be ARGB (not RGB).
-        val bitmap = Bitmap.createBitmap(mCurrentPage!!.width, mCurrentPage!!.height,
-            Bitmap.Config.ARGB_8888)
+        val bitmap = Bitmap.createBitmap(
+            /* width = */ mCurrentPage!!.width,
+            /* height = */ mCurrentPage!!.height,
+            /* config = */ Bitmap.Config.ARGB_8888
+        )
         // Here, we render the page onto the Bitmap.
         // To render a portion of the page, use the second and third parameter. Pass nulls to get
         // the default result.
         // Pass either RENDER_MODE_FOR_DISPLAY or RENDER_MODE_FOR_PRINT for the last parameter.
-        mCurrentPage!!.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
+        mCurrentPage!!.render(
+            /* destination = */ bitmap,
+            /* destClip = */ null,
+            /* transform = */ null,
+            /* renderMode = */ PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY
+        )
         // We are ready to show the Bitmap to user.
         mImageView!!.setImageBitmap(bitmap)
         updateUi()
@@ -306,7 +316,10 @@ class PdfRendererBasicFragment
         val pageCount = mPdfRenderer!!.pageCount
         mButtonPrevious!!.isEnabled = 0 != index
         mButtonNext!!.isEnabled = index + 1 < pageCount
-        requireActivity().title = getString(R.string.app_name_with_index, index + 1, pageCount)
+        requireActivity().title = getString(
+            /* resId = */ R.string.app_name_with_index,
+            /* ...formatArgs = */ index + 1, pageCount
+        )
     }
 
     /**
