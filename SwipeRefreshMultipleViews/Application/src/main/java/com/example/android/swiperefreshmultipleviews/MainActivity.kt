20f250cda715da70d598561aa32effc5cddb8a29
markgray <mark.t.gray@gmail.com>
2023-12-27 05:15:24 -0500

    Tidied up comments in SwipeRefreshMultipleViews

--- SwipeRefreshMultipleViews/Application/src/main/java/com/example/android/swiperefreshmultipleviews/MainActivity.kt
+++ SwipeRefreshMultipleViews/Application/src/main/java/com/example/android/swiperefreshmultipleviews/MainActivity.kt
@@ -19,10 +19,13 @@ package com.example.android.swiperefreshmultipleviews
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.widget.ViewAnimator
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.example.android.common.activities.SampleActivityBase
 import com.example.android.common.logger.Log
 import com.example.android.common.logger.LogFragment
@@ -31,11 +34,9 @@ import com.example.android.common.logger.MessageOnlyLogFilter
 
 /**
  * A simple launcher activity containing a summary sample description, sample log and a custom
- * [Fragment] which can display a view.
- *
- *
- * For devices with displays with a width of 720dp or greater, the sample log is always visible,
- * on other devices it's visibility is controlled by an item on the Action Bar.
+ * [Fragment] which can display a view. For devices with displays with a width of 720dp or greater,
+ * the sample log is always visible, on other devices it's visibility is controlled by an item on
+ * the Action Bar.
  */
 class MainActivity : SampleActivityBase() {
     /**
@@ -45,22 +46,24 @@ class MainActivity : SampleActivityBase() {
 
     /**
      * Called when the activity is starting. We first call through to our super's implementation of
-     * `onCreate`, then we set our content view to our layout file R.layout.activity_main. If
-     * our parameter `savedInstanceState` is null, this is the first time we were called so we
-     * use the FragmentManager for interacting with fragments associated with this activity to begin
-     * `FragmentTransaction transaction`, initialize `SwipeRefreshMultipleViewsFragment fragment`
-     * with a new instance and use `transaction` to replace (add) `fragment` to the container
-     * view with ID R.id.sample_content_fragment in our layout. We then commit `transaction`.
+     * `onCreate`, then we set our content view to our layout file [R.layout.activity_main]. If our
+     * [Bundle] parameter [savedInstanceState] is `null`, this is the first time we were called so
+     * we use the [FragmentManager] for interacting with fragments associated with this activity to
+     * begin [FragmentTransaction] variable `val transaction`, initialize [SwipeRefreshMultipleViewsFragment]
+     * variable `val fragment` with a new instance and use `transaction` to replace (add) `fragment`
+     * to the container view with ID [R.id.sample_content_fragment] in our layout. We then commit
+     * `transaction`.
      *
-     * @param savedInstanceState If this is null we need to create and add our `SlidingTabsBasicFragment`
-     * fragment, if not null we are being recreated after a configuration
-     * change so the fragment already exists
+     * @param savedInstanceState If this is `null` we need to create and add our
+     * [SwipeRefreshMultipleViewsFragment] fragment, if not `null` we are being
+     * recreated after a configuration change so the system will take care of restoring
+     * the fragment.
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         if (savedInstanceState == null) {
-            val transaction = supportFragmentManager.beginTransaction()
+            val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
             val fragment = SwipeRefreshMultipleViewsFragment()
             transaction.replace(R.id.sample_content_fragment, fragment)
             transaction.commit()
@@ -68,13 +71,13 @@ class MainActivity : SampleActivityBase() {
     }
 
     /**
-     * Initialize the contents of the Activity's standard options menu. We use a `MenuInflater`
-     * with this context to inflate our menu layout file R.menu.main into our parameter `Menu menu`
-     * and return true so that the menu will be displayed.
+     * Initialize the contents of the Activity's standard options menu. We use a [MenuInflater] for
+     * this context to inflate our menu layout file [R.menu.main] into our [Menu] parameter [menu]
+     * and return `true` so that the menu will be displayed.
      *
      * @param menu The options menu in which you place your items.
-     * @return You must return true for the menu to be displayed;
-     * if you return false it will not be shown.
+     * @return You must return `true` for the menu to be displayed;
+     * if you return `false` it will not be shown.
      */
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.main, menu)
@@ -82,29 +85,28 @@ class MainActivity : SampleActivityBase() {
     }
 
     /**
-     * Prepare the Screen's standard options menu to be displayed. We initialize `MenuItem logToggle`
-     * by finding the menu item in our parameter `Menu menu` with id R.id.menu_toggle_log, set
-     * it to visible only if the view in our layout with id R.id.sample_output is an instance of
-     * `ViewAnimator`, and set its title to R.string.sample_hide_log ("Hide Log") if our flag
-     * `mLogShown` is true or to R.string.sample_show_log ("Show Log") if it is false. Finally
-     * we return the value returned by our super's implementation of `onPrepareOptionsMenu` to
-     * the caller.
+     * Prepare the Screen's standard options menu to be displayed. We initialize [MenuItem] variable
+     * `val logToggle` by finding the menu item in our [Menu] parameter [menu] with id
+     * [R.id.menu_toggle_log], set it to visible only if the view in our layout with id
+     * [R.id.sample_output] is an instance of [ViewAnimator], and set its title to [R.string.sample_hide_log]
+     * ("Hide Log") if our [Boolean] flag field [mLogShown] is `true` or to [R.string.sample_show_log]
+     * ("Show Log") if it is `false`. Finally we return the value returned by our super's implementation
+     * of `onPrepareOptionsMenu` to the caller.
      *
-     * @param menu The options menu as last shown or first initialized by
-     * onCreateOptionsMenu().
-     * @return You must return true for the menu to be displayed;
-     * if you return false it will not be shown.
+     * @param menu The options menu as last shown or first initialized by [onCreateOptionsMenu].
+     * @return You must return `true` for the menu to be displayed;
+     * if you return `false` it will not be shown.
      */
     override fun onPrepareOptionsMenu(menu: Menu): Boolean {
-        val logToggle = menu.findItem(R.id.menu_toggle_log)
+        val logToggle: MenuItem = menu.findItem(R.id.menu_toggle_log)
         logToggle.isVisible = findViewById<View>(R.id.sample_output) is ViewAnimator
         logToggle.setTitle(if (mLogShown) R.string.sample_hide_log else R.string.sample_show_log)
         return super.onPrepareOptionsMenu(menu)
     }
 
     /**
-     * This hook is called whenever an item in your options menu is selected. We use a switch to handle
-     * only the `MenuItem` with id R.id.menu_toggle_log. If it is that item we toggle the value
+     * This hook is called whenever an item in your options menu is selected. We use a `when` switch
+     * to handle only the [MenuItem] with id R.id.menu_toggle_log. If it is that item we toggle the value
      * of `mLogShown`, then initialize `ViewAnimator output` by finding the view with
      * ID R.id.sample_output. If `mLogShown` is true we set the displayed child of `output`
      * to 1, if it is false we we set the displayed child of `output` to 0. We then call the
