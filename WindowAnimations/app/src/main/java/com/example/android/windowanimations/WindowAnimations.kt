90a304d1993f501c135f78ea93bf0160a110b29b
markgray <mark.t.gray@gmail.com>
2024-01-15 04:55:57 -0500

    Tidied up comments in WindowAnimations

--- WindowAnimations/app/src/main/java/com/example/android/windowanimations/WindowAnimations.kt
+++ WindowAnimations/app/src/main/java/com/example/android/windowanimations/WindowAnimations.kt
@@ -20,6 +20,7 @@ package com.example.android.windowanimations
 import android.app.Activity
 import android.app.ActivityOptions
 import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.view.View
@@ -36,45 +37,54 @@ import android.widget.ImageView
 class WindowAnimations : Activity() {
     /**
      * Called when the activity is starting. First we call our super's implementation of `onCreate`,
-     * and set our content view to our layout file R.layout.activity_window_animations. We initialize
-     * `Button defaultButton` by finding the view with id R.id.defaultButton ("Default Animations"),
-     * initialize `Button translateButton` by finding the view with id R.id.translateButton ("Translate Animations")
-     * initialize `Button scaleButton` by finding the view with id R.id.scaleButton ("Scale Animations"),
-     * and initialize `ImageView thumbnail` by finding the view with id R.id.thumbnail (displays the thumbnail
-     * drawable-nodpi/thumbnail.png). We set the `OnClickListener` of `defaultButton` to an anonymous
-     * class whose `onClick` override just creates an `Intent` to launch the activity `SubActivity`
-     * and starts it running (relying on the default transition animations). We set the `OnClickListener` of
-     * `translateButton` to an anonymous class whose `onClick` override creates an `Intent` to
-     * launch `AnimatedSubActivity`, then creates a `Bundle translateBundle` from an `ActivityOptions`
-     * instance which specifies the use of the xml animation with resource id R.anim.slide_in_left as the animation to
-     * use for the incoming activity, and R.anim.slide_out_left as the animation to use for the outgoing activity,
-     * and finally launches the `Intent subActivity` with `translateBundle` as the option bundle. We set
-     * the `OnClickListener` of `scaleButton` to an anonymous class whose `onClick` override creates
-     * an `Intent` to launch `AnimatedSubActivity`, then creates a `Bundle scaleBundle` from an
-     * `ActivityOptions` instance which specifies a scale up animation from the `View v` that was clicked
-     * with a start X and Y of 0 to the width and height of `v`, and finally launches the `Intent subActivity`
-     * with `scaleBundle` as the option bundle. We set the `OnClickListener` of `thumbnail` to an
-     * anonymous class whose `onClick` override initializes `BitmapDrawable drawable` with the current
-     * drawable of `ImageView thumbnail`, initializes `Bitmap bm` with the bitmap used to render `drawable`,
-     * creates an `Intent subActivity` to launch `AnimatedSubActivity`, then creates a `Bundle scaleBundle`
-     * from an `ActivityOptions` instance which specifies a thumbnail scale up animation from `thumbnail`, using
-     * `bm` as the bitmap that will be shown as the initial thumbnail of the animation, with the starting point relative
-     * to the source being (0,0), and finally launches the `Intent subActivity` with `scaleBundle` as the option
-     * bundle.
+     * and set our content view to our layout file [R.layout.activity_window_animations]. We
+     * initialize [Button] variable `val defaultButton` by finding the view with id [R.id.defaultButton]
+     * ("Default Animations"), initialize [Button] variable `val translateButton` by finding the view
+     * with id [R.id.translateButton] ("Translate Animations") initialize [Button] variable
+     * `val scaleButton` by finding the view with id [R.id.scaleButton] ("Scale Animations"), and
+     * initialize [ImageView] variable `val thumbnail` by finding the view with id [R.id.thumbnail]
+     * (displays the thumbnail drawable-nodpi/thumbnail.png). We set the [View.OnClickListener] of
+     * `defaultButton` to an anonymous class whose [View.OnClickListener.onClick] override just
+     * creates an [Intent] to launch the activity [SubActivity] and starts it running (relying on
+     * the default transition animations). We set the [View.OnClickListener] of `translateButton` to
+     * an anonymous class whose [View.OnClickListener.onClick] override creates an [Intent] to
+     * launch [AnimatedSubActivity], then creates a [Bundle] variable `val translateBundle` from an
+     * [ActivityOptions] instance which specifies the use of the xml animation with resource id
+     * [R.anim.slide_in_left] as the animation to use for the incoming activity, and
+     * [R.anim.slide_out_left] as the animation to use for the outgoing activity, and finally
+     * launches the [Intent] variable `subActivity` with `translateBundle` as the option bundle.
+     * We set the [View.OnClickListener] of `scaleButton` to an anonymous class whose
+     * [View.OnClickListener.onClick] override creates an `Intent` to launch [AnimatedSubActivity],
+     * then creates a [Bundle] variable `val scaleBundle` from an [ActivityOptions] instance which
+     * specifies a scale up animation from the [View] parameter `v`  that was clicked with a start X
+     * and Y of 0 to the width and height of `v`, and finally launches the [Intent] variable
+     * `subActivity` with `scaleBundle` as the option bundle. We set the [View.OnClickListener] of
+     * `thumbnail` to an anonymous class whose [View.OnClickListener.onClick] override initializes
+     * [BitmapDrawable] variable `val drawable` with the current drawable of [ImageView] variable
+     * `thumbnail`, initializes [Bitmap] variable `val bm` with the bitmap used to render `drawable`,
+     * creates an [Intent] variable `val subActivity` to launch [AnimatedSubActivity], then creates
+     * a [Bundle] variable `val scaleBundle` from an [ActivityOptions] instance which specifies a
+     * thumbnail scale up animation from `thumbnail`, using `bm` as the bitmap that will be shown as
+     * the initial thumbnail of the animation, with the starting point relative to the source being
+     * (0,0), and finally launches the [Intent] variable `subActivity` with `scaleBundle` as the
+     * option bundle.
      *
-     * @param savedInstanceState we do not override `onSaveInstanceState` so do not use.
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_window_animations)
-        val defaultButton = findViewById<Button>(R.id.defaultButton)
-        val translateButton = findViewById<Button>(R.id.translateButton)
-        val scaleButton = findViewById<Button>(R.id.scaleButton)
-        val thumbnail = findViewById<ImageView>(R.id.thumbnail)
+        val defaultButton: Button = findViewById(R.id.defaultButton)
+        val translateButton: Button = findViewById(R.id.translateButton)
+        val scaleButton: Button = findViewById(R.id.scaleButton)
+        val thumbnail: ImageView = findViewById(R.id.thumbnail)
 
         // By default, launching a sub-activity uses the system default for window animations
         defaultButton.setOnClickListener { v: View? ->
-            val subActivity = Intent(this@WindowAnimations, SubActivity::class.java)
+            val subActivity = Intent(
+                /* packageContext = */ this@WindowAnimations,
+                /* cls = */ SubActivity::class.java
+            )
             startActivity(subActivity)
         }
 
@@ -83,24 +93,35 @@ class WindowAnimations : Activity() {
         translateButton.setOnClickListener { v: View? ->
             // Using the AnimatedSubActivity also allows us to animate exiting that
             // activity - see that activity for details
-            val subActivity = Intent(this@WindowAnimations, AnimatedSubActivity::class.java)
+            val subActivity = Intent(
+                /* packageContext = */ this@WindowAnimations,
+                /* cls = */ AnimatedSubActivity::class.java
+            )
             // The enter/exit animations for the two activities are specified by xml resources
-            val translateBundle = ActivityOptions
-                .makeCustomAnimation(this@WindowAnimations,
-                    R.anim.slide_in_left,
-                    R.anim.slide_out_left)
-                .toBundle()
+            val translateBundle: Bundle = ActivityOptions
+                .makeCustomAnimation(
+                    /* context = */ this@WindowAnimations,
+                    /* enterResId = */ R.anim.slide_in_left,
+                    /* exitResId = */ R.anim.slide_out_left
+                ).toBundle()
             startActivity(subActivity, translateBundle)
         }
 
         // Starting in Jellybean, you can provide an animation that scales up the new
         // activity from a given source rectangle
         scaleButton.setOnClickListener { v: View ->
-            val subActivity = Intent(this@WindowAnimations, AnimatedSubActivity::class.java)
-            val scaleBundle = ActivityOptions
-                .makeScaleUpAnimation(v, 0, 0,
-                    v.width, v.height)
-                .toBundle()
+            val subActivity = Intent(
+                /* packageContext = */ this@WindowAnimations,
+                /* cls = */ AnimatedSubActivity::class.java
+            )
+            val scaleBundle: Bundle = ActivityOptions
+                .makeScaleUpAnimation(
+                    /* source = */ v,
+                    /* startX = */ 0,
+                    /* startY = */ 0,
+                    /* width = */ v.width,
+                    /* height = */ v.height
+                ).toBundle()
             startActivity(subActivity, scaleBundle)
         }
 
@@ -109,11 +130,18 @@ class WindowAnimations : Activity() {
         // representations. Here, we scale up from a thumbnail image of the final sub-activity
         thumbnail.setOnClickListener { v: View? ->
             val drawable = thumbnail.drawable as BitmapDrawable
-            val bm = drawable.bitmap
-            val subActivity = Intent(this@WindowAnimations, AnimatedSubActivity::class.java)
-            val scaleBundle = ActivityOptions
-                .makeThumbnailScaleUpAnimation(thumbnail, bm, 0, 0)
-                .toBundle()
+            val bm: Bitmap = drawable.bitmap
+            val subActivity = Intent(
+                /* packageContext = */ this@WindowAnimations,
+                /* cls = */ AnimatedSubActivity::class.java
+            )
+            val scaleBundle: Bundle = ActivityOptions
+                .makeThumbnailScaleUpAnimation(
+                    /* source = */ thumbnail,
+                    /* thumbnail = */ bm,
+                    /* startX = */ 0,
+                    /* startY = */ 0
+                ).toBundle()
             startActivity(subActivity, scaleBundle)
         }
     }
