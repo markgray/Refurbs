aa384721d57a55bfeeb7266cf3fc123d6e4fa7ef
markgray <mark.t.gray@gmail.com>
2024-06-02 06:31:08 -0400

    Partially switched to kts syntax, added TODO, and updated gradle dependency in ClippingBasic

--- ClippingBasic/Application/src/main/java/com/example/android/clippingbasic/ClippingBasicFragment.kt
+++ ClippingBasic/Application/src/main/java/com/example/android/clippingbasic/ClippingBasicFragment.kt
@@ -68,6 +68,7 @@ class ClippingBasicFragment : Fragment() {
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        @Suppress("DEPRECATION") // TODO: Replace with MenuProvider
         setHasOptionsMenu(true)
         mOutlineProvider = ClipOutlineProvider()
         mSampleTexts = resources.getStringArray(R.array.sample_texts)
