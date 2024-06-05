edebf764de494f99d28caba5337865308b54fd18
Mark Gray <mark.t.gray@gmail.com>
2024-06-05 05:42:13 -0400

    Partially switched to kts syntax, added TODO, and updated gradle dependency in RevealEffectBasic

--- RevealEffectBasic/Application/src/main/java/com/example/android/revealeffectbasic/RevealEffectBasicFragment.kt
+++ RevealEffectBasic/Application/src/main/java/com/example/android/revealeffectbasic/RevealEffectBasicFragment.kt
@@ -42,6 +42,7 @@ class RevealEffectBasicFragment : Fragment() {
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        @Suppress("DEPRECATION") // TODO: Use MenuProvider
         setHasOptionsMenu(true)
     }
 
