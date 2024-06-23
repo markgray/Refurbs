bcd72269ffcf0992d67586c84fc226af97c4efbc
markgray <mark.t.gray@gmail.com>
2024-06-23 05:23:00 -0400

    Converted build files to kts, and updated AGP to 8.5.0 in ActiveNotifications

---
+++ ActiveNotifications/app/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+dependencies {
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+}
+
+android {
+    compileSdk = 34
+    defaultConfig {
+        applicationId = "com.example.android.activenotifications"
+        minSdk = 28
+        targetSdk = 34
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android.txt"),
+                "proguard-rules.txt"
+            )
+        }
+    }
+    namespace = "com.example.android.activenotifications"
+}
