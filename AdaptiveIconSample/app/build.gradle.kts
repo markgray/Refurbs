809ee260da413b03570d78453b4bcf97bf462952
markgray <mark.t.gray@gmail.com>
2024-06-23 05:57:05 -0400

    Converted build files to kts, and updated AGP to 8.5.0 in AdaptiveIconSample

---
+++ AdaptiveIconSample/app/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = 34
+    defaultConfig {
+        applicationId = "com.example.android.adaptiveicon"
+        minSdk = 26
+        targetSdk = 34
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android.txt"),
+                "proguard-rules.txt"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    namespace = "com.example.android.adaptiveicon"
+}
+
+dependencies {
+    implementation ("androidx.appcompat:appcompat:1.7.0")
+}
