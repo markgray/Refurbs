70a21216f9d1ce7f548899afd8c433946113e2e2
markgray <mark.t.gray@gmail.com>
2024-06-23 05:33:57 -0400

    Converted build files to kts, and updated AGP to 8.5.0 in ActivityAnimations

---
+++ ActivityAnimations/app/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = 34
+    defaultConfig {
+        applicationId = "com.example.android.activityanim"
+        minSdk = 21
+        targetSdk = 34
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android.txt"),
+                "proguard-rules.txt"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    namespace = "com.example.android.activityanim"
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+}
