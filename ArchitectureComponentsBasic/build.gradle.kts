0e6229f48a6a92d3dd6a6497182381dfd33eefdc
markgray <mark.t.gray@gmail.com>
2024-06-23 06:24:58 -0400

    Converted build files to kts, and updated AGP to 8.5.0 in ArchitectureComponentsBasic

--- ArchitectureComponentsBasic/build.gradle.kts
+++ ArchitectureComponentsBasic/build.gradle.kts
@@ -14,4 +14,10 @@
  * limitations under the License.
  */
 
-include ':app'
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+plugins {
+    id("com.android.application") version "8.5.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
+}
--- ArchitectureComponentsBasic/settings.gradle.kts
+++ ArchitectureComponentsBasic/settings.gradle.kts
@@ -14,33 +14,18 @@
  * limitations under the License.
  */
 
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-
-buildscript {
-    ext {
-        kotlin_version = "1.9.24"
-    }
+pluginManagement {
     repositories {
         google()
         mavenCentral()
-    }
-    dependencies {
-        classpath ("com.android.tools.build:gradle:8.4.1")
-        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
-        classpath ("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.24-1.0.20")
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
+        gradlePluginPortal()
     }
 }
-
-allprojects {
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage") // TODO: Keep an eye on this
     repositories {
         google()
         mavenCentral()
     }
 }
-
-tasks.register("clean", Delete) {
-    delete (rootProject.layout.buildDir)
-}
+include(":app")
