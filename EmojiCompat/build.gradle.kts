0fe16598dd28c0224bd36929f75b21518efec1d7
markgray <mark.t.gray@gmail.com>
2024-06-25 06:42:16 -0400

    Converted build files to kts, and updated AGP to 8.5.0 in EmojiCompat

--- EmojiCompat/build.gradle.kts
+++ EmojiCompat/build.gradle.kts
@@ -15,29 +15,10 @@
  */
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    ext {
-        kotlin_version = "1.9.24"
-    }
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath ("com.android.tools.build:gradle:8.4.1")
-        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
-    }
-}
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-tasks.register("clean", Delete) {
-    delete (rootProject.layout.buildDir)
+plugins {
+    id("com.android.application") version "8.5.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
 
 
