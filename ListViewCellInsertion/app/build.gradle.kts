e4abc94cb40c5d643053f7670670e98db7c82b8f
markgray <mark.t.gray@gmail.com>
2024-06-26 07:04:18 -0400

    Converted build files to kts, and updated AGP to 8.5.0 in ListViewCellInsertion

---
+++ ListViewCellInsertion/app/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = 34
+    defaultConfig {
+        applicationId = "com.example.android.insertingcells"
+        minSdk = 16
+        targetSdk = 34
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android.txt"),
+                "proguard-rules.txt"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    namespace = "com.example.android.insertingcells"
+}
