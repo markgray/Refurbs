dc4e50a6fc9e1246a3fe88c4f0128e76b639ed9a
markgray <mark.t.gray@gmail.com>
2024-06-26 06:22:52 -0400

    Converted build files to kts, and updated AGP to 8.5.0 in JobScheduler

---
+++ JobScheduler/Application/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+dependencies {
+    api("androidx.legacy:legacy-support-v4:1.0.0")
+    api("androidx.legacy:legacy-support-v13:1.0.0")
+    api("androidx.cardview:cardview:1.0.0")
+    api("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+}
+
+android {
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 34
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        buildConfig = true
+    }
+    namespace = "com.example.android.jobscheduler"
+}
