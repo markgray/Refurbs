00ebda0d178876cf22ea983efb58867cf807d2d8
markgray <mark.t.gray@gmail.com>
2024-06-27 05:35:02 -0400

    Converted build files to kts, and updated AGP to 8.5.0 in LunarLander

---
+++ LunarLander/app/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = 34
+    defaultConfig {
+        applicationId = "com.example.android.lunarlander"
+        minSdk = 16
+        targetSdk = 34
+        testApplicationId = "com.example.android.lunarlander.tests"
+        testInstrumentationRunner = "android.test.InstrumentationTestRunner"
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    namespace = "com.example.android.lunarlander"
+}
