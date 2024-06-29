456e18bd2448a5fd8c80d8331c710b3360be5932
markgray <mark.t.gray@gmail.com>
2024-06-29 05:16:41 -0400

    Converted build files to kts, and updated AGP to 8.5.0 in RevealEffectBasic

---
+++ RevealEffectBasic/Application/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+dependencies {
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.legacy:legacy-support-v13:1.0.0")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+}
+
+android {
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 34
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    namespace = "com.example.android.revealeffectbasic"
+}
