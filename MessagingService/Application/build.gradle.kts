2922e5e7a6f47119a3037c5f676fc8b80a55a4e8
Mark Gray <mark.t.gray@gmail.com>
2024-08-03 04:23:06 -0400

    Updated gradle dependencies in MessagingService

--- MessagingService/Application/build.gradle.kts
+++ MessagingService/Application/build.gradle.kts
@@ -8,9 +8,8 @@ dependencies {
     implementation("androidx.legacy:legacy-support-v13:1.0.0")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.activity:activity-ktx:1.9.0")
-    implementation("androidx.activity:activity-ktx:1.9.0")
-    implementation("androidx.fragment:fragment-ktx:1.8.1")
+    implementation("androidx.activity:activity-ktx:1.9.1")
+    implementation("androidx.fragment:fragment-ktx:1.8.2")
 }
 
 android {
