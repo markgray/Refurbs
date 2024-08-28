4939d26e901e60c019d421e2574eb60b83dea72b
markgray <mark.t.gray@gmail.com>
2024-08-28 05:35:59 -0400

    Updated kotlin to 2.0.20, and updated ksp to 2.0.20-1.0.24 in ArchitectureComponentsBasic

--- ArchitectureComponentsBasic/build.gradle.kts
+++ ArchitectureComponentsBasic/build.gradle.kts
@@ -18,6 +18,6 @@
 
 plugins {
     id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-    id("com.google.devtools.ksp") version "2.0.0-1.0.22" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
+    id("com.google.devtools.ksp") version "2.0.20-1.0.24" apply false
 }
