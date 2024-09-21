8523d0051332dadad59c226b943db5d1e2f96642
markgray <mark.t.gray@gmail.com>
2024-09-21 07:01:26 -0400

    Updated AGP to 8.6.1, and added enableEdgeToEdge in ActivityInstrumentation

--- ActivityInstrumentation/app/src/main/java/com.example.android.activityinstrumentation/MainActivity.kt
+++ ActivityInstrumentation/app/src/main/java/com.example.android.activityinstrumentation/MainActivity.kt
@@ -22,11 +22,17 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import android.preference.PreferenceManager
 import android.view.View
+import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
+import android.widget.RelativeLayout
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
 import java.util.Arrays
 
 /**
@@ -47,9 +53,25 @@ class MainActivity : AppCompatActivity() {
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
         // Inflate UI from res/layout/activity_main.xml
         setContentView(R.layout.sample_main)
+        val rootView = findViewById<RelativeLayout>(R.id.root_view)
+        ViewCompat.setOnApplyWindowInsetsListener(rootView!!) { v, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            // Apply the insets as a margin to the view.
+            v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                leftMargin = insets.left
+                rightMargin = insets.right
+                topMargin = insets.top+supportActionBar!!.height
+                bottomMargin = insets.bottom
+            }
+            // Return CONSUMED if you don't want want the window insets to keep passing
+            // down to descendant views.
+            WindowInsetsCompat.CONSUMED
+        }
+
 
         // Get handle to default shared preferences for this activity
         mPrefs = PreferenceManager.getDefaultSharedPreferences(this@MainActivity)
