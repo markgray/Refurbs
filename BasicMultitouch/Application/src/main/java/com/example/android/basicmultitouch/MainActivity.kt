83a07fdda0b2db28a24939d92df83de41aee3335
Mark Gray <mark.t.gray@gmail.com>
2024-09-22 03:40:59 -0400

    Updated AGP to 8.6.1, and added enableEdgeToEdge in BasicMultitouch

--- BasicMultitouch/Application/src/main/java/com/example/android/basicmultitouch/MainActivity.kt
+++ BasicMultitouch/Application/src/main/java/com/example/android/basicmultitouch/MainActivity.kt
@@ -18,9 +18,14 @@ package com.example.android.basicmultitouch
 import android.os.Bundle
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
 
 /**
  * This is an example of keeping track of individual touches across multiple [MotionEvent]s.
@@ -43,6 +48,21 @@ class MainActivity : AppCompatActivity() {
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.layout_mainactivity)
+        val rootView = findViewById<FrameLayout>(R.id.root_view)
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            // Apply the insets as a margin to the view.
+            v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                leftMargin = insets.left
+                rightMargin = insets.right
+                topMargin = insets.top
+                bottomMargin = insets.bottom
+            }
+            // Return CONSUMED if you don't want want the window insets to keep passing
+            // down to descendant views.
+            WindowInsetsCompat.CONSUMED
+        }
     }
 }
