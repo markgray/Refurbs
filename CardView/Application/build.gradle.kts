948343544c5282c80b491ee9bd2cb82ac7ad76a4
markgray <mark.t.gray@gmail.com>
2024-09-22 05:54:52 -0400

    Updated AGP to 8.6.1, and added enableEdgeToEdge in CardView

--- CardView/Application/build.gradle.kts
+++ CardView/Application/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 }
 
 dependencies {
+    implementation("androidx.activity:activity-ktx:1.9.2")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.fragment:fragment-ktx:1.8.3")
--- CardView/Application/src/main/java/com/example/android/cardview/CardViewActivity.kt
+++ CardView/Application/src/main/java/com/example/android/cardview/CardViewActivity.kt
@@ -16,6 +16,12 @@
 package com.example.android.cardview
 
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
@@ -39,7 +45,22 @@ class CardViewActivity : FragmentActivity() {
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_card_view)
+        val rootView = findViewById<FrameLayout>(R.id.container)
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            // Apply the insets as a margin to the view.
+            v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                leftMargin = insets.left
+                rightMargin = insets.right
+                topMargin = insets.top
+                bottomMargin = insets.bottom
+            }
+            // Return CONSUMED if you don't want want the window insets to keep passing
+            // down to descendant views.
+            WindowInsetsCompat.CONSUMED
+        }
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .add(R.id.container, CardViewFragment.newInstance())
