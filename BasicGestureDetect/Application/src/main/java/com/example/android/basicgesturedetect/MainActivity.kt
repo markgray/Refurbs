c53c2f0afccbd4ffa4142824530cea2141a5e337
Mark Gray <mark.t.gray@gmail.com>
2024-09-22 03:26:38 -0400

    Updated AGP to 8.6.1, and added enableEdgeToEdge in BasicGestureDetect

--- BasicGestureDetect/Application/src/main/java/com/example/android/basicgesturedetect/MainActivity.kt
+++ BasicGestureDetect/Application/src/main/java/com/example/android/basicgesturedetect/MainActivity.kt
@@ -22,6 +22,12 @@ import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import com.example.android.common.activities.SampleActivityBase
@@ -52,7 +58,22 @@ class MainActivity : SampleActivityBase() {
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val rootView = findViewById<LinearLayout>(R.id.sample_main_layout)
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            // Apply the insets as a margin to the view.
+            v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                leftMargin = insets.left
+                rightMargin = insets.right
+                topMargin = insets.top
+                bottomMargin = insets.bottom
+            }
+            // Return CONSUMED if you don't want want the window insets to keep passing
+            // down to descendant views.
+            WindowInsetsCompat.CONSUMED
+        }
         if (supportFragmentManager.findFragmentByTag(FRAGTAG) == null) {
             val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
             val fragment = BasicGestureDetectFragment()
