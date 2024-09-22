cadf5ddddf0d096a9cc30e146ba46a7ba8509b1e
Mark Gray <mark.t.gray@gmail.com>
2024-09-22 04:39:58 -0400

    Updated AGP to 8.6.1, and added enableEdgeToEdge in BitmapAllocation

--- BitmapAllocation/app/src/main/java/com/example/android/bitmapallocation/BitmapAllocation.kt
+++ BitmapAllocation/app/src/main/java/com/example/android/bitmapallocation/BitmapAllocation.kt
@@ -22,10 +22,16 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import android.widget.CheckBox
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
 
 /**
  * This example shows how to speed up bitmap loading and reduce garbage collection
@@ -88,7 +94,22 @@ class BitmapAllocation : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_bitmap_allocation)
+        val rootView = findViewById<LinearLayout>(R.id.root_view)
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            // Apply the insets as a margin to the view.
+            v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                leftMargin = insets.left
+                rightMargin = insets.right
+                topMargin = insets.top
+                bottomMargin = insets.bottom
+            }
+            // Return CONSUMED if you don't want want the window insets to keep passing
+            // down to descendant views.
+            WindowInsetsCompat.CONSUMED
+        }
         val imageIDs: IntArray = intArrayOf(R.drawable.a, R.drawable.b, R.drawable.c, R.drawable.d,
             R.drawable.e, R.drawable.f)
         val checkbox: CheckBox = findViewById(R.id.checkbox)
