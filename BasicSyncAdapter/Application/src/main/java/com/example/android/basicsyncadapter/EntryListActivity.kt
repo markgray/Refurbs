adbc06d7589999709855e1da510e7eeb0955b883
Mark Gray <mark.t.gray@gmail.com>
2024-09-22 04:11:49 -0400

    Updated AGP to 8.6.1, and added enableEdgeToEdge in BasicSyncAdapter

--- BasicSyncAdapter/Application/src/main/java/com/example/android/basicsyncadapter/EntryListActivity.kt
+++ BasicSyncAdapter/Application/src/main/java/com/example/android/basicsyncadapter/EntryListActivity.kt
@@ -1,7 +1,13 @@
 package com.example.android.basicsyncadapter
 
 import android.os.Bundle
+import android.view.ViewGroup
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
+import androidx.fragment.app.FragmentContainerView
 
 /**
  * Activity for holding EntryListFragment.
@@ -15,6 +21,21 @@ class EntryListActivity : AppCompatActivity() {
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_entry_list)
+        val rootView = findViewById<FragmentContainerView>(R.id.entry_list)
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            // Apply the insets as a margin to the view.
+            v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                leftMargin = insets.left
+                rightMargin = insets.right
+                topMargin = insets.top
+                bottomMargin = insets.bottom
+            }
+            // Return CONSUMED if you don't want want the window insets to keep passing
+            // down to descendant views.
+            WindowInsetsCompat.CONSUMED
+        }
     }
 }
