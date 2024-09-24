01b9677f605bb52e4a7196e8fbd670c87589e43a
markgray <mark.t.gray@gmail.com>
2024-09-24 05:38:19 -0400

    Updated AGP to 8.6.1, and added enableEdgeToEdge in MessagingService

--- MessagingService/Application/src/main/java/com/example/android/messagingservice/MainActivity.kt
+++ MessagingService/Application/src/main/java/com/example/android/messagingservice/MainActivity.kt
@@ -24,6 +24,12 @@ import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
@@ -71,8 +77,23 @@ class MainActivity : FragmentActivity() {
      * is `null` then, otherwise it contains info that the [FragmentManager] is concerned with.
      */
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rootView = findViewById<FrameLayout>(R.id.container)
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            // Apply the insets as a margin to the view.
+            v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                leftMargin = insets.left
+                rightMargin = insets.right
+                topMargin = insets.top
+                bottomMargin = insets.bottom
+            }
+            // Return CONSUMED if you don't want want the window insets to keep passing
+            // down to descendant views.
+            WindowInsetsCompat.CONSUMED
+        }
         mNM = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
         val chan1 = NotificationChannel(
             /* id = */ PRIMARY_CHANNEL,
