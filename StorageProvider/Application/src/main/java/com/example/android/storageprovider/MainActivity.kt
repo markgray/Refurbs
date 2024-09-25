5336c5e151da7e6ecc8140a776af2e92e9438f44
markgray <mark.t.gray@gmail.com>
2024-09-25 06:48:25 -0400

    Updated AGP to 8.6.1, and added enableEdgeToEdge in StorageProvider

--- StorageProvider/Application/src/main/java/com/example/android/storageprovider/MainActivity.kt
+++ StorageProvider/Application/src/main/java/com/example/android/storageprovider/MainActivity.kt
@@ -22,6 +22,12 @@ import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import com.example.android.common.activities.SampleActivityBase
@@ -50,8 +56,23 @@ class MainActivity : SampleActivityBase() {
      * @param savedInstanceState we do not override `onSaveInstanceState` so do not use
      */
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rootView = findViewById<LinearLayout>(R.id.sample_main_layout)
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            // Apply the insets as a margin to the view.
+            v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                leftMargin = insets.left
+                rightMargin = insets.right
+                topMargin = insets.top+actionBar!!.height
+                bottomMargin = insets.bottom
+            }
+            // Return CONSUMED if you don't want want the window insets to keep passing
+            // down to descendant views.
+            WindowInsetsCompat.CONSUMED
+        }
         if (supportFragmentManager.findFragmentByTag(FRAGTAG) == null) {
             val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
             val fragment = StorageProviderFragment()
