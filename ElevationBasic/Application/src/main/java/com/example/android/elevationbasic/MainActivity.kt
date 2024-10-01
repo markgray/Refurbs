9fb6cd57d3bdd6b13b65ed662a17a36d429912b7
Mark Gray <mark.t.gray@gmail.com>
2024-10-01 04:01:19 -0400

    Moved enableEdgeToEdge in ElevationBasic

--- ElevationBasic/Application/src/main/java/com/example/android/elevationbasic/MainActivity.kt
+++ ElevationBasic/Application/src/main/java/com/example/android/elevationbasic/MainActivity.kt
@@ -66,8 +66,8 @@ class MainActivity : SampleActivityBase() {
      * already exists
      */
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rootView = findViewById<LinearLayout>(R.id.sample_main_layout)
         ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
