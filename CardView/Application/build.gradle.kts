29e31734494b659a8e3a6c22e5ee4e2dd5e1114a
Mark Gray <mark.t.gray@gmail.com>
2024-10-08 04:21:40 -0400

    Updated AGP to 8.7.0, and updated gradle dependency in CardView

--- CardView/Application/build.gradle.kts
+++ CardView/Application/build.gradle.kts
@@ -7,7 +7,7 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.9.2")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.fragment:fragment-ktx:1.8.3")
+    implementation("androidx.fragment:fragment-ktx:1.8.4")
 }
 
 android {
