21850ada2e53e3f106becfb825ffe4f47ceaa65d
Mark Gray <mark.t.gray@gmail.com>
2024-10-10 02:59:10 -0400

    Updated date of gradle-wrapper.properties in PictureViewer

--- PictureViewer/app/src/main/java/com/example/android/pictureviewer/PictureViewer.kt
+++ PictureViewer/app/src/main/java/com/example/android/pictureviewer/PictureViewer.kt
@@ -13,7 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@file:Suppress("UNUSED_ANONYMOUS_PARAMETER", "MemberVisibilityCanBePrivate")
+@file:Suppress("UNUSED_ANONYMOUS_PARAMETER", "MemberVisibilityCanBePrivate",
+    "KDocUnresolvedReference"
+)
 
 package com.example.android.pictureviewer
 
