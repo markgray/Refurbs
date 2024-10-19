a97e4f4e192f8b099458b437d7a12d98ba66b446
Mark Gray <mark.t.gray@gmail.com>
2024-10-19 04:08:50 -0400

    Updated AGP to 8.7.1, and updated gradle dependency in ArchitectureComponentsBasic

--- ArchitectureComponentsBasic/app/build.gradle.kts
+++ ArchitectureComponentsBasic/app/build.gradle.kts
@@ -54,7 +54,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.9.2")
+    implementation("androidx.activity:activity-ktx:1.9.3")
     // Support libraries
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
