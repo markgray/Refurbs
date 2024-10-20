a580154c7b96c19885faeb37014bca91d42c6db2
Mark Gray <mark.t.gray@gmail.com>
2024-10-20 03:06:13 -0400

    Updated AGP to 8.7.1, and updated gradle dependency in BitmapAllocation

--- BitmapAllocation/app/build.gradle.kts
+++ BitmapAllocation/app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.9.2")
+    implementation("androidx.activity:activity-ktx:1.9.3")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.core:core-ktx:1.13.1")
 }
