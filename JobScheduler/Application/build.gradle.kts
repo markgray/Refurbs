ccd809a023b69d37083ead3444d1c07b15e4ca9f
Mark Gray <mark.t.gray@gmail.com>
2024-10-22 03:17:58 -0400

    Updated AGP to 8.7.1, and updated gradle dependency in JobScheduler

--- JobScheduler/Application/build.gradle.kts
+++ JobScheduler/Application/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.9.2")
+    implementation("androidx.activity:activity-ktx:1.9.3")
     api("androidx.legacy:legacy-support-v4:1.0.0")
     api("androidx.legacy:legacy-support-v13:1.0.0")
     api("androidx.cardview:cardview:1.0.0")
