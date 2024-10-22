240b0d77385314761ef03bcdd917aebe542b2221
Mark Gray <mark.t.gray@gmail.com>
2024-10-22 04:09:22 -0400

    Updated AGP to 8.7.1, and updated gradle dependency in MultiWindowPlayground

--- MultiWindowPlayground/Application/build.gradle.kts
+++ MultiWindowPlayground/Application/build.gradle.kts
@@ -33,7 +33,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.9.2")
+    implementation("androidx.activity:activity-ktx:1.9.3")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
 }
