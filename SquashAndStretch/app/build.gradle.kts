e09ebd30e1f61a8ff19239dd7f7275d523185956
Mark Gray <mark.t.gray@gmail.com>
2024-10-23 03:28:46 -0400

    Updated AGP to 8.7.1, and updated gradle dependency in SquashAndStretch

--- SquashAndStretch/app/build.gradle.kts
+++ SquashAndStretch/app/build.gradle.kts
@@ -29,6 +29,6 @@ android {
     }
     namespace = "com.example.squashandstretch"
     dependencies {
-        implementation("androidx.activity:activity-ktx:1.9.2")
+        implementation("androidx.activity:activity-ktx:1.9.3")
     }
 }
