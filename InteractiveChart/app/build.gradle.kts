76bd8136217f283f2af644cbc3b019ac05ef3f34
markgray <mark.t.gray@gmail.com>
2024-10-28 06:08:40 -0400

    Updated kotlin to 2.0.21, updated compileSdk to 35, updated targetSdk to 35 in InteractiveChart

--- InteractiveChart/app/build.gradle.kts
+++ InteractiveChart/app/build.gradle.kts
@@ -10,11 +10,11 @@ dependencies {
 }
 
 android {
-    compileSdk = 34
+    compileSdk = 35
     defaultConfig {
         applicationId = "com.example.android.interactivechart"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
     }
     buildTypes {
         release {
