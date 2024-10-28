fd3d6719283d508152cddebbdb96dacc16f30213
markgray <mark.t.gray@gmail.com>
2024-10-28 05:18:57 -0400

    Updated kotlin to 2.0.21, updated compileSdk to 35, updated targetSdk to 35 in ElevationBasic

--- ElevationBasic/Application/build.gradle.kts
+++ ElevationBasic/Application/build.gradle.kts
@@ -13,11 +13,11 @@ dependencies {
 }
 
 android {
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
