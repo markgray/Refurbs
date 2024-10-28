a2c9ac7ee505c85c1d219b130eb6677fe6e8df58
markgray <mark.t.gray@gmail.com>
2024-10-28 05:39:11 -0400

    Updated kotlin to 2.0.21, updated compileSdk to 35, updated targetSdk to 35 in FoldingLayout

--- FoldingLayout/app/build.gradle.kts
+++ FoldingLayout/app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    compileSdk = 34
+    compileSdk = 35
     defaultConfig {
         applicationId = "com.example.android.foldinglayout"
         minSdk = 23
-        targetSdk = 34
+        targetSdk = 35
     }
     buildTypes {
         release {
