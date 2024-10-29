465a14d146df29af60ee00f3bc3c7d7ed0dc98cd
markgray <mark.t.gray@gmail.com>
2024-10-29 06:25:11 -0400

    Updated kotlin to 2.0.21, updated compileSdk to 35, updated targetSdk to 35 in LunarLander

--- LunarLander/app/build.gradle.kts
+++ LunarLander/app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    compileSdk = 34
+    compileSdk = 35
     defaultConfig {
         applicationId = "com.example.android.lunarlander"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         testApplicationId = "com.example.android.lunarlander.tests"
         testInstrumentationRunner = "android.test.InstrumentationTestRunner"
     }
