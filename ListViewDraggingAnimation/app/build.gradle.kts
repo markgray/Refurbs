6826c103870ea606249489803a3f1607a58565e8
markgray <mark.t.gray@gmail.com>
2024-10-29 05:51:02 -0400

    Updated kotlin to 2.0.21, updated compileSdk to 35, updated targetSdk to 35, and added android:layout_marginTop kludge (will look bad on old phones) in ListViewDraggingAnimation

--- ListViewDraggingAnimation/app/build.gradle.kts
+++ ListViewDraggingAnimation/app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    compileSdk = 34
+    compileSdk = 35
     defaultConfig {
         applicationId = "com.example.android.listviewdragginganimation"
         minSdk = 16
-        targetSdk = 34
+        targetSdk = 35
     }
     buildTypes {
         release {
