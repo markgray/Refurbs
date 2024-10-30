3e52d067427f79db63849d5a803435d93721a0ce
markgray <mark.t.gray@gmail.com>
2024-10-30 05:44:01 -0400

    Updated kotlin to 2.0.21, updated compileSdk to 35, targetSdk to 35 NOTE: edgeToEdge needs lots of work! in NotePad

--- NotePad/app/build.gradle.kts
+++ NotePad/app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    compileSdk = 34
+    compileSdk = 35
     defaultConfig {
         applicationId = "com.example.android.notepad"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         testApplicationId = "com.example.android.notepad.tests"
         testInstrumentationRunner = "android.test.InstrumentationTestRunner"
     }
