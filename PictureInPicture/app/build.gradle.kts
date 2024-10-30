70171e53a6d37cd90cf4801cb9f00c713a24c375
markgray <mark.t.gray@gmail.com>
2024-10-30 06:27:00 -0400

    Updated kotlin to 2.0.21, updated compileSdk to 35, updated targetSdk to 35 in PictureInPicture

--- PictureInPicture/app/build.gradle.kts
+++ PictureInPicture/app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    compileSdk = 34
+    compileSdk = 35
     defaultConfig {
         applicationId = "com.example.android.pictureinpicture"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
