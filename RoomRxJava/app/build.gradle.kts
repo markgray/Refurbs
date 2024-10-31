15fedf77da57d5488e7e089202017f343662750b
markgray <mark.t.gray@gmail.com>
2024-10-31 05:55:04 -0400

    Updated kotlin to 2.0.21, updated compileSdk to 35, updated targetSdk to 35 and updated gradle dependencies in RoomRxJava

--- RoomRxJava/app/build.gradle.kts
+++ RoomRxJava/app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    compileSdk = 34
+    compileSdk = 35
     defaultConfig {
         applicationId = "com.example.android.persistence"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
@@ -35,7 +35,7 @@ android {
 dependencies {
     implementation("androidx.activity:activity-ktx:1.9.3")
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.6")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
 
     // Support libraries
     implementation("androidx.appcompat:appcompat:1.7.0")
@@ -43,14 +43,14 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
 
     // Architecture components
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    annotationProcessor("androidx.lifecycle:lifecycle-common:2.8.6")
+    annotationProcessor("androidx.lifecycle:lifecycle-common:2.8.7")
     implementation("androidx.room:room-runtime:2.6.1")
     annotationProcessor("androidx.room:room-compiler:2.6.1")
     ksp("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-rxjava2:2.6.1")
-    implementation("androidx.annotation:annotation:1.9.0")
+    implementation("androidx.annotation:annotation:1.9.1")
 
     // RxJava
     implementation("io.reactivex.rxjava2:rxandroid:2.0.1")
