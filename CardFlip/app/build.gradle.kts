a3d1fadb29cd51719142d209afdd3ba5e0ad37d9
Mark Gray <mark.t.gray@gmail.com>
2024-11-05 02:59:55 -0500

    Updated AGP to 8.7.2, and updated gradle dependency in CardFlip

--- CardFlip/app/build.gradle.kts
+++ CardFlip/app/build.gradle.kts
@@ -30,6 +30,6 @@ android {
     dependencies {
         implementation("androidx.activity:activity-ktx:1.9.3")
         implementation("androidx.appcompat:appcompat:1.7.0")
-        implementation("androidx.core:core-ktx:1.13.1")
+        implementation("androidx.core:core-ktx:1.15.0")
     }
 }
