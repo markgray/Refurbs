6ec263ba613cea721f37eeac01388b1c5874e878
Mark Gray <mark.t.gray@gmail.com>
2024-11-06 02:55:20 -0500

    Updated AGP to 8.7.2, and updated gradle dependency in EmojiCompat

--- EmojiCompat/app/build.gradle.kts
+++ EmojiCompat/app/build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
     implementation("androidx.emoji:emoji:1.1.0")
     implementation("androidx.emoji:emoji-appcompat:1.1.0")
     implementation("androidx.emoji:emoji-bundled:1.1.0")
-    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.core:core-ktx:1.15.0")
 
     // Test
     testImplementation("junit:junit:4.13.2")
