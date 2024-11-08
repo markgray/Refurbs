f573b2e46b7dab4fa57259b2bafe87e8b98fb639
Mark Gray <mark.t.gray@gmail.com>
2024-11-08 03:46:33 -0500

    Updated AGP to 8.7.2 in RoomwithContentProviders

--- RoomwithContentProviders/build.gradle.kts
+++ RoomwithContentProviders/build.gradle.kts
@@ -15,7 +15,7 @@
  */
 
 plugins {
-    id("com.android.application") version "8.7.1" apply false
+    id("com.android.application") version "8.7.2" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("com.google.devtools.ksp") version "2.0.21-1.0.25" apply false
 }
