95ebd250bdfbd60f64cd0d4ad7e5a5ee780c9db4
markgray <mark.t.gray@gmail.com>
2025-01-17 07:05:37 -0500

    Updated AGP to 8.8.0 and updated gradle dependency in ActiveNotifications

--- ActiveNotifications/app/build.gradle.kts
+++ ActiveNotifications/app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.9.3")
+    implementation("androidx.activity:activity-ktx:1.10.0")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.core:core-ktx:1.15.0")
