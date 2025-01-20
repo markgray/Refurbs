4167725d38019fffdc642f95cd365a26318eebcf
Mark Gray <mark.t.gray@gmail.com>
2025-01-20 05:02:00 -0500

    Updated AGP to 8.8.0 and updated gradle dependency in NotePad

--- NotePad/app/build.gradle.kts
+++ NotePad/app/build.gradle.kts
@@ -32,6 +32,6 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.9.3")
+    implementation("androidx.activity:activity-ktx:1.10.0")
     implementation("androidx.core:core-ktx:1.15.0")
 }
