619044146c6c3489814b381278c07f9c326bba57
markgray <mark.t.gray@gmail.com>
2025-02-01 07:25:01 -0500

    Updated kotlin to 2.1.10 in ActivityAnimations

--- ActivityAnimations/app/src/main/java/com/example/android/activityanim/ActivityAnimations.kt
+++ ActivityAnimations/app/src/main/java/com/example/android/activityanim/ActivityAnimations.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@file:Suppress("KDocUnresolvedReference", "ReplaceNotNullAssertionWithElvisReturn", "MemberVisibilityCanBePrivate")
+@file:Suppress("ReplaceNotNullAssertionWithElvisReturn", "MemberVisibilityCanBePrivate")
 
 package com.example.android.activityanim
 
