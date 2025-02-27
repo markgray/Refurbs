e5faf3638f64a6d5d547fc63d9b2fb4e18e63703
markgray <mark.t.gray@gmail.com>
2025-02-27 07:20:00 -0500

    Updated AGP to 8.8.2 and updated gradle dependency in ActionBarCompat-ShareActionProvider

--- ActionBarCompat-ShareActionProvider/Application/build.gradle.kts
+++ ActionBarCompat-ShareActionProvider/Application/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.10.0")
+    implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.annotation:annotation:1.9.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
