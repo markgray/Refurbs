ef1f2c780d3b22f86f4cab3a1a8b1efe9d327b7a
markgray <mark.t.gray@gmail.com>
2025-02-28 08:11:52 -0500

    Updated AGP to 8.8.2 and updated gradle dependency in CurvedMotion

--- CurvedMotion/app/build.gradle.kts
+++ CurvedMotion/app/build.gradle.kts
@@ -28,6 +28,6 @@ android {
         }
     }
     dependencies {
-        implementation("androidx.activity:activity-ktx:1.10.0")
+        implementation("androidx.activity:activity-ktx:1.10.1")
     }
 }
