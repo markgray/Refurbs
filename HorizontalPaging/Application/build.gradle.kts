cacae8dca9960c305fa112c3c91a133f5817b6b4
markgray <mark.t.gray@gmail.com>
2025-03-01 06:56:59 -0500

    Updated AGP to 8.8.2 and updated gradle dependency in HorizontalPaging

--- HorizontalPaging/Application/build.gradle.kts
+++ HorizontalPaging/Application/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.10.0")
+    implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.gridlayout:gridlayout:1.0.0")
     implementation("androidx.cardview:cardview:1.0.0")
