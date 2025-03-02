6c1fc6900f68a6ef20528c78cf32ce3b4c951fbc
markgray <mark.t.gray@gmail.com>
2025-03-02 06:38:15 -0500

    Updated AGP to 8.8.2 and updated gradle dependency in PictureInPicture

--- PictureInPicture/app/build.gradle.kts
+++ PictureInPicture/app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.10.0")
+    implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.media:media:1.7.0")
