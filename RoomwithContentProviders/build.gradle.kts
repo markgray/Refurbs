b8fd78721371d4742547ad68f0c2a80ae875c3f8
Mark Gray <mark.t.gray@gmail.com>
2025-03-27 04:04:21 -0400

    Updated AGP to 8.9.1 and updated kotlin to 2.1.20 in RoomwithContentProviders

--- RoomwithContentProviders/build.gradle.kts
+++ RoomwithContentProviders/build.gradle.kts
@@ -15,7 +15,7 @@
  */
 
 plugins {
-    id("com.android.application") version "8.9.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.10" apply false
-    id("com.google.devtools.ksp") version "2.1.10-1.0.31" apply false
+    id("com.android.application") version "8.9.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.20" apply false
+    id("com.google.devtools.ksp") version "2.1.20-1.0.32" apply false
 }
