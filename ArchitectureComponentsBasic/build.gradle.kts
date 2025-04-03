378780697450bfbdc69e3d2e3e174c855a1e0b6f
Mark Gray <mark.t.gray@gmail.com>
2025-04-03 04:02:06 -0400

    Updated gradle dependency in ArchitectureComponentsBasic

--- ArchitectureComponentsBasic/build.gradle.kts
+++ ArchitectureComponentsBasic/build.gradle.kts
@@ -19,5 +19,5 @@
 plugins {
     id("com.android.application") version "8.9.1" apply false
     id("org.jetbrains.kotlin.android") version "2.1.20" apply false
-    id("com.google.devtools.ksp") version "2.1.20-1.0.31" apply false
+    id("com.google.devtools.ksp") version "2.1.20-1.0.32" apply false
 }
