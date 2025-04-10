cc7d96bd1e08e4ef674bc1dcd6ff12fb74bc24ff
Mark Gray <mark.t.gray@gmail.com>
2025-04-10 03:34:45 -0400

    Updated gradle dependency in BasicSyncAdapter

--- BasicSyncAdapter/Application/build.gradle.kts
+++ BasicSyncAdapter/Application/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 dependencies {
     implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.gridlayout:gridlayout:1.0.0")
+    implementation("androidx.gridlayout:gridlayout:1.1.0")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.core:core-ktx:1.15.0")
