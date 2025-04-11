319b13b54ac4c86391dea71a58bd10fbea142bef
Mark Gray <mark.t.gray@gmail.com>
2025-04-11 04:03:09 -0400

    Updated gradle dependency in NotePad

--- NotePad/app/build.gradle.kts
+++ NotePad/app/build.gradle.kts
@@ -33,5 +33,5 @@ android {
 
 dependencies {
     implementation("androidx.activity:activity-ktx:1.10.1")
-    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.core:core-ktx:1.16.0")
 }
