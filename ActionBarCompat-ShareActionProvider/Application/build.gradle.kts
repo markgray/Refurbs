523413c3bb8b591ff768a779ec4c7f865d6d1363
Mark Gray <mark.t.gray@gmail.com>
2025-04-17 04:05:19 -0400

    Updated gradle dependencies in ActionBarCompat-ShareActionProvider

--- ActionBarCompat-ShareActionProvider/Application/build.gradle.kts
+++ ActionBarCompat-ShareActionProvider/Application/build.gradle.kts
@@ -8,11 +8,11 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.annotation:annotation:1.9.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.gridlayout:gridlayout:1.0.0")
+    implementation("androidx.gridlayout:gridlayout:1.1.0")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.recyclerview:recyclerview:1.4.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.core:core-ktx:1.16.0")
 }
 
 android {
