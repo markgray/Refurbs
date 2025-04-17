67cc8b0fe40278d0f0efd5264241d895393cdee4
Mark Gray <mark.t.gray@gmail.com>
2025-04-17 04:22:52 -0400

    Updated gradle dependency in ActiveNotifications

--- ActiveNotifications/app/build.gradle.kts
+++ ActiveNotifications/app/build.gradle.kts
@@ -7,7 +7,7 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.core:core-ktx:1.16.0")
 }
 
 android {
