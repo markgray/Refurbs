53b1dd8f3e230a4fc833a5b872509d13dd9654d8
Mark Gray <mark.t.gray@gmail.com>
2025-04-18 04:50:39 -0400

    Updated gradle dependency in EmojiCompat

--- EmojiCompat/app/build.gradle.kts
+++ EmojiCompat/app/build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
     implementation("androidx.emoji:emoji:1.1.0")
     implementation("androidx.emoji:emoji-appcompat:1.1.0")
     implementation("androidx.emoji:emoji-bundled:1.1.0")
-    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.core:core-ktx:1.16.0")
 
     // Test
     testImplementation("junit:junit:4.13.2")
