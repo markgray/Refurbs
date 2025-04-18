d28453a9aaf6484169c7376dee8e4e3b5ed23cae
Mark Gray <mark.t.gray@gmail.com>
2025-04-18 03:32:44 -0400

    Updated gradle dependency in BitmapScaling

--- BitmapScaling/app/build.gradle.kts
+++ BitmapScaling/app/build.gradle.kts
@@ -32,5 +32,5 @@ android {
 dependencies {
     implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.core:core-ktx:1.16.0")
 }
