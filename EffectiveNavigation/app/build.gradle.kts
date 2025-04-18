f7a52165d4a8b7c87244f7831de15df92abec231
Mark Gray <mark.t.gray@gmail.com>
2025-04-18 04:41:41 -0400

    Updated gradle dependency in EffectiveNavigation

--- EffectiveNavigation/app/build.gradle.kts
+++ EffectiveNavigation/app/build.gradle.kts
@@ -31,5 +31,5 @@ android {
 
 dependencies {
     implementation ("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation ("androidx.core:core-ktx:1.15.0")
+    implementation ("androidx.core:core-ktx:1.16.0")
 }
