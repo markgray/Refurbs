ec36b5c4d891669080f2278e37fbe5104275eab7
Mark Gray <mark.t.gray@gmail.com>
2025-05-20 04:03:16 -0400

    Updated AGP to 8.10.0, updated gradle to 8.14, updated kotlin to 2.1.21 and updated gradle dependencies in ArchitectureComponentsBasic

--- ArchitectureComponentsBasic/app/build.gradle.kts
+++ ArchitectureComponentsBasic/app/build.gradle.kts
@@ -62,11 +62,11 @@ dependencies {
     implementation("androidx.cardview:cardview:1.0.0")
 
     // Architecture components
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.0")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
     implementation("androidx.core:core-ktx:1.16.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
-    ksp("androidx.lifecycle:lifecycle-common:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.0")
+    ksp("androidx.lifecycle:lifecycle-common:2.9.0")
     implementation("androidx.room:room-runtime:2.7.1")
     ksp("androidx.room:room-compiler:2.7.1")
 
