bd26b93dc6537c36572530078ba32ba0a58b1d68
markgray <mark.t.gray@gmail.com>
2025-06-16 06:38:12 -0400

    Updated gradle to 8.14.2, and updated gradle dependency in ActionBarCompat-ShareActionProvider

--- ActionBarCompat-ShareActionProvider/Application/build.gradle.kts
+++ ActionBarCompat-ShareActionProvider/Application/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 dependencies {
     implementation("androidx.activity:activity-ktx:1.10.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.annotation:annotation:1.9.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.gridlayout:gridlayout:1.1.0")
