dfe734229e929f47fda8db96949888a409168fe5
markgray <mark.t.gray@gmail.com>
2025-06-17 06:03:18 -0400

    Updated gradle to 8.14.2, and updated gradle dependency in BitmapAllocation

--- BitmapAllocation/app/build.gradle.kts
+++ BitmapAllocation/app/build.gradle.kts
@@ -32,6 +32,6 @@ android {
 
 dependencies {
     implementation("androidx.activity:activity-ktx:1.10.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.core:core-ktx:1.16.0")
 }
