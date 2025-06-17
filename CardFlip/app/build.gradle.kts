17759989d31538a72ccda1fd5692097e54cc7700
markgray <mark.t.gray@gmail.com>
2025-06-17 07:46:25 -0400

    Updated gradle to 8.14.2, and updated gradle dependency in CardFlip

--- CardFlip/app/build.gradle.kts
+++ CardFlip/app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
     namespace = "com.example.android.cardflip"
     dependencies {
         implementation("androidx.activity:activity-ktx:1.10.1")
-        implementation("androidx.appcompat:appcompat:1.7.0")
+        implementation("androidx.appcompat:appcompat:1.7.1")
         implementation("androidx.core:core-ktx:1.16.0")
     }
 }
