6f3cf8e5d7f6483518c4bd0aa4f260a8d2067a6a
markgray <mark.t.gray@gmail.com>
2025-06-19 06:58:31 -0400

    Updated gradle to 8.14.2, and updated gradle dependency in MessagingService

--- MessagingService/Application/build.gradle.kts
+++ MessagingService/Application/build.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
     implementation("androidx.legacy:legacy-support-v13:1.0.0")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.core:core-ktx:1.16.0")
-    implementation("androidx.fragment:fragment-ktx:1.8.7")
+    implementation("androidx.fragment:fragment-ktx:1.8.8")
 }
 
 android {
