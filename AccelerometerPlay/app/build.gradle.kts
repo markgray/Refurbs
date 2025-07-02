886da62ab5068dab071baca51f4564d54dabb45e
markgray <mark.t.gray@gmail.com>
2025-07-02 07:01:55 -0400

    Updated targetSdk to 36, updated AGP to 8.11.0, updated kotlin to 2.2.0, switched to "kotlin compilerOptions" and updated gradle dependencies in AccelerometerPlay

--- AccelerometerPlay/app/build.gradle.kts
+++ AccelerometerPlay/app/build.gradle.kts
@@ -1,14 +1,16 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
 
 android {
-    compileSdk = 35
+    compileSdk = 36
     defaultConfig {
         applicationId = "com.example.android.accelerometerplay"
         minSdk = 21
-        targetSdk = 35
+        targetSdk = 36
     }
     buildTypes {
         release {
@@ -23,8 +25,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_17
+        }
     }
     namespace = "com.example.android.accelerometerplay"
 }
@@ -32,4 +36,3 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.7.1")
 }
-
