cf52c68d20490d31e827336018315839249e506b
markgray <mark.t.gray@gmail.com>
2025-07-03 04:55:44 -0400

    Updated targetSdk to 36, updated AGP to 8.11.0, updated kotlin to 2.2.0, switched to "kotlin compilerOptions" and updated gradle dependencies in BitmapAllocation

--- BitmapAllocation/app/build.gradle.kts
+++ BitmapAllocation/app/build.gradle.kts
@@ -1,15 +1,17 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
 
 android {
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.android.bitmapallocation"
         minSdk = 21
-        targetSdk = 35
+        targetSdk = 36
     }
     buildTypes {
         release {
@@ -24,8 +26,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_17
+        }
     }
     namespace = "com.example.android.bitmapallocation"
 }
