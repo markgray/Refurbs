ff092f337c2482ce1bd9776b638cd015add2ce7b
Mark Gray <mark.t.gray@gmail.com>
2025-07-03 02:40:29 -0400

    Updated targetSdk to 36, updated AGP to 8.11.0, updated kotlin to 2.2.0, switched to "kotlin compilerOptions" and updated gradle dependencies in AdaptiveIconSample

--- AdaptiveIconSample/app/build.gradle.kts
+++ AdaptiveIconSample/app/build.gradle.kts
@@ -1,14 +1,16 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
 
 android {
-    compileSdk = 35
+    compileSdk = 36
     defaultConfig {
         applicationId = "com.example.android.adaptiveicon"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
     }
     buildTypes {
         release {
@@ -23,8 +25,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_17
+        }
     }
     namespace = "com.example.android.adaptiveicon"
 }
