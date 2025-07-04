96b00694c5fe0ccbbb727b381f298c58813ae9a2
Mark Gray <mark.t.gray@gmail.com>
2025-07-04 03:39:37 -0400

    Switched to "kotlin compilerOptions" in CustomChoiceList

--- CustomChoiceList/Application/build.gradle.kts
+++ CustomChoiceList/Application/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -22,8 +24,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_17
+        }
     }
     namespace = "com.example.android.customchoicelist"
 }
