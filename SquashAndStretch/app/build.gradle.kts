e71cf0a4d1a95d52e57da5c130ac7d1fe5d49de1
Mark Gray <mark.t.gray@gmail.com>
2025-07-07 03:52:13 -0400

    Updated targetSdk to 36, updated AGP to 8.11.0, updated kotlin to 2.2.0, switched to "kotlin compilerOptions" and updated gradle dependencies

--- SquashAndStretch/app/build.gradle.kts
+++ SquashAndStretch/app/build.gradle.kts
@@ -1,14 +1,16 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
 
 android {
-    compileSdk = 35
+    compileSdk = 36
     defaultConfig {
         applicationId = "com.example.squashandstretch"
         minSdk = 21
-        targetSdk = 35
+        targetSdk = 36
     }
 
     buildTypes {
@@ -24,8 +26,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_17
+        }
     }
     namespace = "com.example.squashandstretch"
     dependencies {
