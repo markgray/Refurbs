5cd738420aafb198d3168e865ad946b1660f26f9
Mark Gray <mark.t.gray@gmail.com>
2025-08-13 03:51:52 -0400

    Updated AGP to 8.12.0, updated gradle to 9.0.0, and updated gradle dependencies in EffectiveNavigation

--- EffectiveNavigation/app/src/main/java/com/example/android/effectivenavigation/CollectionDemoActivity.kt
+++ EffectiveNavigation/app/src/main/java/com/example/android/effectivenavigation/CollectionDemoActivity.kt
@@ -13,7 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@file:Suppress("ReplaceNotNullAssertionWithElvisReturn", "ReplaceJavaStaticMethodWithKotlinAnalog", "MemberVisibilityCanBePrivate")
+@file:Suppress("ReplaceNotNullAssertionWithElvisReturn", "ReplaceJavaStaticMethodWithKotlinAnalog", "MemberVisibilityCanBePrivate",
+    "UnusedImport"
+)
 
 package com.example.android.effectivenavigation
 
