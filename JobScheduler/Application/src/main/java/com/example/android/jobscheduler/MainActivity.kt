e7a53955e30cb30363cd3993c97831b0ce200a8a
markgray <mark.t.gray@gmail.com>
2025-08-13 06:52:06 -0400

    Updated date of gradle-wrapper.properties and fixed warnings in JobScheduler

--- JobScheduler/Application/src/main/java/com/example/android/jobscheduler/MainActivity.kt
+++ JobScheduler/Application/src/main/java/com/example/android/jobscheduler/MainActivity.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@file:Suppress("UNUSED_PARAMETER", "ReplaceNotNullAssertionWithElvisReturn")
+@file:Suppress("UNUSED_PARAMETER", "ReplaceNotNullAssertionWithElvisReturn", "RedundantSuppression")
 
 package com.example.android.jobscheduler
 
