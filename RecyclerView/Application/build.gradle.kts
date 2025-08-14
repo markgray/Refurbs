36b69e5de86fd996ccc69528580bca5cfdc82d06
markgray <mark.t.gray@gmail.com>
2025-08-14 07:38:17 -0400

    Updated date of gradle-wrapper.properties and fixed warnings in RecyclerView

--- RecyclerView/Application/build.gradle.kts
+++ RecyclerView/Application/build.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.recyclerview:recyclerview:1.4.0")
-    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.core:core-ktx:1.17.0")
 }
 
 android {
