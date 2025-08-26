1e4532c01b92d44790ccef6a95bec410ad367cce
markgray <mark.t.gray@gmail.com>
2025-08-26 08:22:48 -0400

    Updated AGP to 8.12.1, updated kotlin to 2.2.10, fixed warnings, and updated gradle dependencies in ActiveNotifications

--- ActiveNotifications/app/build.gradle.kts
+++ ActiveNotifications/app/build.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.core:core-ktx:1.17.0")
 }
 
 android {
