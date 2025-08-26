7c0a87977a65f70ea52476e24b42a7699fa5b923
markgray <mark.t.gray@gmail.com>
2025-08-26 09:51:49 -0400

    Updated AGP to 8.12.1, updated kotlin to 2.2.10, fixed warnings, and updated gradle dependencies in Anticipation

--- Anticipation/app/build.gradle.kts
+++ Anticipation/app/build.gradle.kts
@@ -36,5 +36,5 @@ android {
 dependencies {
     implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.core:core-ktx:1.17.0")
 }
