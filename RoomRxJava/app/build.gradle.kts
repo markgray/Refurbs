76d3f6229d85122ec965cbc00ae6852182a6a7dc
markgray <mark.t.gray@gmail.com>
2025-08-30 08:31:19 -0400

    Updated AGP to 8.12.2, updated kotlin to 2.2.10, fixed warnings, and updated gradle dependencies in RoomRxJava

--- RoomRxJava/app/build.gradle.kts
+++ RoomRxJava/app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
 dependencies {
     implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.core:core-ktx:1.17.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.3")
 
     // Support libraries
     implementation("androidx.appcompat:appcompat:1.7.1")
@@ -47,9 +47,9 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
 
     // Architecture components
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.3")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    annotationProcessor("androidx.lifecycle:lifecycle-common:2.9.2")
+    annotationProcessor("androidx.lifecycle:lifecycle-common:2.9.3")
     implementation("androidx.room:room-runtime:2.7.2")
     annotationProcessor("androidx.room:room-compiler:2.7.2")
     ksp("androidx.room:room-compiler:2.7.2")
