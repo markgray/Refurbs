678b9a9468508ea60830f7b942258fada1b05358
markgray <mark.t.gray@gmail.com>
2025-09-15 05:56:30 -0400

    Updated AGP to 8.13.0, updated kotlin to 2.2.20, fixed warnings, and updated gradle dependencies in ActionBarCompat-ShareActionProvider

--- ActionBarCompat-ShareActionProvider/Application/build.gradle.kts
+++ ActionBarCompat-ShareActionProvider/Application/build.gradle.kts
@@ -6,15 +6,15 @@ plugins {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.10.1")
+    implementation("androidx.activity:activity-ktx:1.11.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.annotation:annotation:1.9.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.gridlayout:gridlayout:1.1.0")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.recyclerview:recyclerview:1.4.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("com.google.android.material:material:1.13.0")
+    implementation("androidx.core:core-ktx:1.17.0")
 }
 
 android {
