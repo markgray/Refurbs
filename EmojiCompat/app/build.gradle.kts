4b1342038260b97e5fc03b4105f563ff147e27b9
markgray <mark.t.gray@gmail.com>
2025-09-16 07:47:11 -0400

    Updated AGP to 8.13.0, updated kotlin to 2.2.20, fixed warnings, and updated gradle dependencies in EmojiCompat

--- EmojiCompat/app/build.gradle.kts
+++ EmojiCompat/app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.10.1")
+    implementation("androidx.activity:activity-ktx:1.11.0")
     // Support Libraries
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.emoji:emoji:1.1.0")
