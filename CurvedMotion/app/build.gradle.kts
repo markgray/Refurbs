06311e4cf871b3c876f718108d1bb41fb3dbf447
markgray <mark.t.gray@gmail.com>
2025-10-12 06:53:26 -0400

    Switched from "proguard-android.txt" to "proguard-android-optimize.txt" and updated gradle dependencies in CurvedMotion

--- CurvedMotion/app/build.gradle.kts
+++ CurvedMotion/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         release {
             isMinifyEnabled = false
             proguardFiles(
-                getDefaultProguardFile("proguard-android.txt"),
+                getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.txt"
             )
         }
