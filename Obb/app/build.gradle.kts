6a6ab721125bd6155d112c21584e58dd8c56c807
Mark Gray <mark.t.gray@gmail.com>
2025-10-15 04:15:31 -0400

    Updated date of gradle-wrapper.properties, and added signingConfig for release variant in Obb

--- Obb/app/build.gradle.kts
+++ Obb/app/build.gradle.kts
@@ -32,6 +32,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
