9b2390b32d0981869956977d14daee4ad87341d1
Mark Gray <mark.t.gray@gmail.com>
2025-10-16 03:46:22 -0400

    Updated date of gradle-wrapper.properties, and added signingConfig for release variant in ViewAnimations

--- ViewAnimations/app/build.gradle.kts
+++ ViewAnimations/app/build.gradle.kts
@@ -19,6 +19,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
     compileOptions {
