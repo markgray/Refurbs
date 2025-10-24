6d4200f7ff496306ef3d794238430168ea0c74fd
Mark Gray <mark.t.gray@gmail.com>
2025-10-24 03:25:05 -0400

    Updated kotlin to 2.2.21, and added signingConfig to release variant in ActivityInstrumentation

--- ActivityInstrumentation/app/build.gradle.kts
+++ ActivityInstrumentation/app/build.gradle.kts
@@ -27,6 +27,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.txt"
             )
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
     compileOptions {
