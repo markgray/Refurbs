0a5b198ccd89917ea8c14fce02c21b3d7c99525a
Mark Gray <mark.t.gray@gmail.com>
2025-10-24 04:13:29 -0400

    Updated kotlin to 2.2.21, and added signingConfig to release variant in BitmapAllocation

--- BitmapAllocation/app/build.gradle.kts
+++ BitmapAllocation/app/build.gradle.kts
@@ -20,6 +20,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.txt"
             )
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
     compileOptions {
