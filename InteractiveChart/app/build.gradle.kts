222e9ae4aa91a8310c33e02447ab01117989b2e2
Mark Gray <mark.t.gray@gmail.com>
2025-10-25 03:20:29 -0400

    Updated kotlin to 2.2.21, and added signingConfig to release variant in InteractiveChart

--- InteractiveChart/app/build.gradle.kts
+++ InteractiveChart/app/build.gradle.kts
@@ -25,6 +25,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.txt"
             )
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
     compileOptions {
