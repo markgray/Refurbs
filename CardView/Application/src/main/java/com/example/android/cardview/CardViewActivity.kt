5ac9222e7895ff575ee631a425c47ccaad8cdcb4
markgray <mark.t.gray@gmail.com>
2025-11-28 07:34:25 -0500

    Added comments in CardView

--- CardView/Application/src/main/java/com/example/android/cardview/CardViewActivity.kt
+++ CardView/Application/src/main/java/com/example/android/cardview/CardViewActivity.kt
@@ -16,9 +16,11 @@
 package com.example.android.cardview
 
 import android.os.Bundle
+import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.activity.enableEdgeToEdge
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
@@ -31,8 +33,27 @@ import androidx.fragment.app.FragmentTransaction
  */
 class CardViewActivity : FragmentActivity() {
     /**
-     * Called when the activity is starting. First we call through to our super's implementation of
-     * `onCreate`. Then we set our content view to our layout file `R.layout.activity_card_view`.
+     * Called when the activity is starting. First we call [enableEdgeToEdge]
+     * to enable edge to edge display, then we call our super's implementation
+     * of `onCreate`, and set our content view to our layout file
+     * `R.layout.activity_card_view
+     *
+     * We initialize our [FrameLayout] variable `rootView`
+     * to the view with ID `R.id.container` then call
+     * [ViewCompat.setOnApplyWindowInsetsListener] to take over the policy
+     * for applying window insets to `rootView`, with the `listener`
+     * argument a lambda that accepts the [View] passed the lambda
+     * in variable `v` and the [WindowInsetsCompat] passed the lambda
+     * in variable `windowInsets`. It initializes its [Insets] variable
+     * `insets` to the [WindowInsetsCompat.getInsets] of `windowInsets` with
+     * [WindowInsetsCompat.Type.systemBars] as the argument, then it updates
+     * the layout parameters of `v` to be a [ViewGroup.MarginLayoutParams]
+     * with the left margin set to `insets.left`, the right margin set to
+     * `insets.right`, the top margin set to `insets.top`, and the bottom margin
+     * set to `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED]
+     * to the caller (so that the window insets will not keep passing down to
+     * descendant views).`.
+     *
      * If our [Bundle] parameter [savedInstanceState] is `null` this is the first time we have been
      * called so we use the [FragmentManager] for interacting with fragments associated with this
      * activity to begin a new [FragmentTransaction] to which we chain a command to add a new
@@ -48,8 +69,8 @@ class CardViewActivity : FragmentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card_view)
         val rootView = findViewById<FrameLayout>(R.id.container)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
