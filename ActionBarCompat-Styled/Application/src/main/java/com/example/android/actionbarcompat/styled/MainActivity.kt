73dc8264084fab9ae8882c70ff1633bfff421142
markgray <mark.t.gray@gmail.com>
2025-11-28 04:38:38 -0500

    Added comments in ActionBarCompat-Styled

--- ActionBarCompat-Styled/Application/src/main/java/com/example/android/actionbarcompat/styled/MainActivity.kt
+++ ActionBarCompat-Styled/Application/src/main/java/com/example/android/actionbarcompat/styled/MainActivity.kt
@@ -19,11 +19,13 @@ package com.example.android.actionbarcompat.styled
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
 import android.view.ViewGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.ContentFrameLayout
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
@@ -44,11 +46,30 @@ import androidx.fragment.app.FragmentTransaction
  */
 class MainActivity : AppCompatActivity(), ActionBar.TabListener {
     /**
-     * Called when the activity is starting. First we call through to our super's implementation of
-     * `onCreate`, then we set our content view to our layout file `R.layout.sample_main`. We
-     * initialize our [ActionBar] variable `val ab` with a reference to this activity's [ActionBar],
-     * set its navigation mode to [ActionBar.NAVIGATION_MODE_TABS], then add three tabs to it with
-     * their [ActionBar.TabListener] set to this.
+     * Called when the activity is starting. First we call [enableEdgeToEdge]
+     * to enable edge to edge display, then we call our super's implementation
+     * of `onCreate`, and set our content view to our layout file
+     * `R.layout.sample_main`.
+     *
+     * We initialize our [ContentFrameLayout] variable `rootView`
+     * to the view with ID `android.R.id.content` then call
+     * [ViewCompat.setOnApplyWindowInsetsListener] to take over over
+     * the policy for applying window insets to `rootView`, with the
+     * `listener` argument a lambda that accepts the [View] passed the
+     * lambda in variable `v` and the [WindowInsetsCompat] passed the lambda
+     * in variable `windowInsets`. It initializes its [Insets] variable
+     * `insets` to the [WindowInsetsCompat.getInsets] of `windowInsets` with
+     * [WindowInsetsCompat.Type.systemBars] as the argument, then it updates
+     * the layout parameters of `v` to be a [ViewGroup.MarginLayoutParams]
+     * with the left margin set to `insets.left`, the right margin set to
+     * `insets.right`, the top margin set to `insets.top`, and the bottom margin
+     * set to `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED]
+     * to the caller (so that the window insets will not keep passing down to
+     * descendant views).
+     *
+     * Next we initialize our [ActionBar] variable `val ab` with a reference to this activity's
+     * [ActionBar], set its navigation mode to [ActionBar.NAVIGATION_MODE_TABS], then add three
+     * tabs to it with their [ActionBar.TabListener] set to `this`.
      *
      * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
@@ -57,8 +78,8 @@ class MainActivity : AppCompatActivity(), ActionBar.TabListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sample_main)
         val rootView = window.decorView.findViewById<ContentFrameLayout>(android.R.id.content)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
