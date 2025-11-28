ed0b86ee5f87854d536c2ab18ca518161172b8e7
Mark Gray <mark.t.gray@gmail.com>
2025-11-28 04:28:22 -0500

    Updated gradle to 9.2.1, updated gradle dependencies and increased minSdk to 23 in EmojiCompat

--- EmojiCompat/app/build.gradle.kts
+++ EmojiCompat/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 36
     defaultConfig {
         applicationId = "com.example.android.emojicompat"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -41,7 +41,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.11.0")
+    implementation("androidx.activity:activity-ktx:1.12.0")
     // Support Libraries
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.emoji:emoji:1.1.0")
