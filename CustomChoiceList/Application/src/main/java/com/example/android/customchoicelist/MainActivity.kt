2264f622909f117bef4474a6b70fe59ac252aa84
markgray <mark.t.gray@gmail.com>
2025-11-28 08:07:25 -0500

    Added comments in CustomChoiceList

--- CustomChoiceList/Application/src/main/java/com/example/android/customchoicelist/MainActivity.kt
+++ CustomChoiceList/Application/src/main/java/com/example/android/customchoicelist/MainActivity.kt
@@ -26,23 +26,44 @@ import android.widget.ListView
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
 
 /**
  * This sample demonstrates how to create custom single- or multi-choice
- * [android.widget.ListView] UIs. The most interesting bits are in
+ * [ListView] UIs. The most interesting bits are in
  * the `res/layout/` directory of this sample.
  */
 class MainActivity : ComponentActivity() {
     /**
-     * Called when the activity is starting. First we call our super's implementation of `onCreate`,
-     * then we set our content view to our layout file `R.layout.sample_main` (which consists of a
-     * [LinearLayout] containing a [TextView] which explains the example and a [ListView] with id
-     * "@android:id/list" which this [Activity] will use to display its list o Cheeses. Finally we
-     * call the [ListView.setAdapter] method of the [ListView] (aka kotlin `adapter` property) with
-     * a new instance of [MyAdapter].
+     * Called when the activity is starting. First we call [enableEdgeToEdge]
+     * to enable edge to edge display, then we call our super's implementation
+     * of `onCreate`, and set our content view to our layout file
+     * `R.layout.sample_main` (which consists of a [LinearLayout] containing a [TextView] which
+     * explains the example and a [ListView] with id "@android:id/list" which this [Activity]
+     * will use to display its list o Cheeses).
+     *
+     * We initialize our [LinearLayout] variable `rootView`
+     * to the view with ID `R.id.root_view` then call
+     * [ViewCompat.setOnApplyWindowInsetsListener] to take over the policy
+     * for applying window insets to `rootView`, with the `listener`
+     * argument a lambda that accepts the [View] passed the lambda
+     * in variable `v` and the [WindowInsetsCompat] passed the lambda
+     * in variable `windowInsets`. It initializes its [Insets] variable
+     * `insets` to the [WindowInsetsCompat.getInsets] of `windowInsets` with
+     * [WindowInsetsCompat.Type.systemBars] as the argument, then it updates
+     * the layout parameters of `v` to be a [ViewGroup.MarginLayoutParams]
+     * with the left margin set to `insets.left`, the right margin set to
+     * `insets.right`, the top margin set to `insets.top`, and the bottom margin
+     * set to `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED]
+     * to the caller (so that the window insets will not keep passing down to
+     * descendant views).
+     *
+     * Finally we use [findViewById] to find the [ListView] with ID [android.R.id.list] and call
+     * its [ListView.setAdapter] method (aka kotlin `adapter` property) with a new instance of
+     * [MyAdapter].
      *
      * @param savedInstanceState We do not override `onSaveInstanceState` so do not use.
      */
@@ -52,8 +73,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sample_main)
         val rootView = findViewById<LinearLayout>(R.id.root_view)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
