0c35e4fa45524165cad5e8872b79299333e57690
markgray <mark.t.gray@gmail.com>
2025-11-28 04:26:39 -0500

    Added comments in ActionBarCompat-ShareActionProvider

--- ActionBarCompat-ShareActionProvider/Application/src/main/java/com/example/android/actionbarcompat/shareactionprovider/MainActivity.kt
+++ ActionBarCompat-ShareActionProvider/Application/src/main/java/com/example/android/actionbarcompat/shareactionprovider/MainActivity.kt
@@ -33,6 +33,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.ContentFrameLayout
 import androidx.appcompat.widget.ShareActionProvider
+import androidx.core.graphics.Insets
 import androidx.core.view.ActionProvider
 import androidx.core.view.MenuItemCompat
 import androidx.core.view.ViewCompat
@@ -66,11 +67,28 @@ class MainActivity : AppCompatActivity() {
     private var mShareActionProvider: ShareActionProvider? = null
 
     /**
-     * Called when the activity is starting. First we call through to our super's implementation of
-     * `onCreate`, then we set our content view to our layout file `R.layout.sample_main`. We find
-     * the view with id `R.id.viewpager` to set our [ViewPager] variable `val vp`, and add our
-     * [OnPageChangeListener] field [mOnPageChangeListener] as an [OnPageChangeListener].
-     * Finally we set the adapter of `vp` to our [PagerAdapter] field [mPagerAdapter].
+     * Called when the activity is starting. First we call [enableEdgeToEdge]
+     * to enable edge to edge display, then we call our super's implementation
+     * of `onCreate`, and set our content view to our layout file `R.layout.sample_main`.
+     *
+     * We initialize our ContentFrameLayout variable `rootView` to the view with ID
+     * `android.R.id.content` then call [ViewCompat.setOnApplyWindowInsetsListener]
+     * to take over the policy for applying window insets to `rootView`, with the
+     * `listener` argument a lambda that accepts the [View] passed the lambda in
+     * variable `v` and the [WindowInsetsCompat] passed the lambda in variable
+     * `windowInsets`. It initializes its [Insets] variable `insets` to the
+     * [WindowInsetsCompat.getInsets] of `windowInsets` with [WindowInsetsCompat.Type.systemBars]
+     * as the argument, then it updates the layout parameters of `v` to be a
+     * [ViewGroup.MarginLayoutParams] with the left margin set to `insets.left`,
+     * the right margin set to `insets.right`, the top margin set to `insets.top`,
+     * and the bottom margin set to `insets.bottom`. Finally it returns
+     * [WindowInsetsCompat.CONSUMED] to the caller (so that the window insets will
+     * not keep passing down to descendant views).
+     *
+     * We find the view with id `R.id.viewpager` to set our [ViewPager] variable `val vp`,
+     * and add our [OnPageChangeListener] field [mOnPageChangeListener] as an
+     * [OnPageChangeListener]. Finally we set the adapter of `vp` to our [PagerAdapter]
+     * field [mPagerAdapter].
      *
      * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
@@ -81,8 +99,8 @@ class MainActivity : AppCompatActivity() {
         // Set content view (which contains a CheeseListFragment)
         setContentView(R.layout.sample_main)
         val rootView = window.decorView.findViewById<ContentFrameLayout>(android.R.id.content)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
