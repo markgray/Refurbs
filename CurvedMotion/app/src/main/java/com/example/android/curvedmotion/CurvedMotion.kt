7e80b6e5f5cf669906ed703622deabfbb439f438
markgray <mark.t.gray@gmail.com>
2025-11-28 07:58:03 -0500

    Added comments in CurvedMotion

--- CurvedMotion/app/src/main/java/com/example/android/curvedmotion/CurvedMotion.kt
+++ CurvedMotion/app/src/main/java/com/example/android/curvedmotion/CurvedMotion.kt
@@ -20,6 +20,7 @@ package com.example.android.curvedmotion
 import android.animation.ObjectAnimator
 import android.animation.TimeInterpolator
 import android.os.Bundle
+import android.view.View
 import android.view.View.OnClickListener
 import android.view.ViewGroup
 import android.view.ViewTreeObserver
@@ -29,6 +30,7 @@ import android.widget.Button
 import android.widget.RelativeLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
@@ -57,12 +59,31 @@ class CurvedMotion : ComponentActivity() {
     var mButton: Button? = null
 
     /**
-     * Called when the activity is starting. First we call our super's implementation of `onCreate`.
-     * then we set our content view to our layout file `R.layout.activity_curved_motion`. We initialize
-     * our [Button] field [mButton] by finding the view with id `R.id.button` ("Click Me!"), and set
-     * its [OnClickListener] to an anonymous class whose [OnClickListener.onClick] override causes
-     * the button to be moved from the top left of the screen to the bottom right (or back again)
-     * using a curved animation.
+     * Called when the activity is starting. First we call [enableEdgeToEdge]
+     * to enable edge to edge display, then we call our super's implementation
+     * of `onCreate`, and set our content view to our layout file
+     * `R.layout.activity_curved_motion`.
+     *
+     * We initialize our [RelativeLayout] variable `rootView`
+     * to the view with ID `R.id.root_view` then call
+     * [ViewCompat.setOnApplyWindowInsetsListener] to take over the policy
+     * for applying window insets to `rootView`, with the `listener`
+     * argument a lambda that accepts the [View] passed the lambda
+     * in variable `v` and the [WindowInsetsCompat] passed the lambda
+     * in variable `windowInsets`. It initializes its [Insets] variable
+     * `insets` to the [WindowInsetsCompat.getInsets] of `windowInsets` with
+     * [WindowInsetsCompat.Type.systemBars] as the argument, then it updates
+     * the layout parameters of `v` to be a [ViewGroup.MarginLayoutParams]
+     * with the left margin set to `insets.left`, the right margin set to
+     * `insets.right`, the top margin set to `insets.top`, and the bottom margin
+     * set to `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED]
+     * to the caller (so that the window insets will not keep passing down to
+     * descendant views).
+     *
+     * We initialize our [Button] field [mButton] by finding the view with id `R.id.button`
+     * ("Click Me!"), and set its [OnClickListener] to an anonymous class whose
+     * [OnClickListener.onClick] override causes the button to be moved from the top left of
+     * the screen to the bottom right (or back again) using a curved animation.
      *
      * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
@@ -71,8 +92,8 @@ class CurvedMotion : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_curved_motion)
         val rootView = findViewById<RelativeLayout>(R.id.root_view)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
