a4ce62377432b17c2e12bbd27e50e3ab07b4964b
Mark Gray <mark.t.gray@gmail.com>
2025-11-28 05:14:14 -0500

    Updated gradle to 9.2.1, updated gradle dependencies and increased minSdk to 23 in InteractiveChart

--- InteractiveChart/app/build.gradle.kts
+++ InteractiveChart/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.11.0")
+    implementation("androidx.activity:activity-ktx:1.12.0")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.core:core-ktx:1.17.0")
 }
@@ -15,7 +15,7 @@ android {
     compileSdk = 36
     defaultConfig {
         applicationId = "com.example.android.interactivechart"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 36
     }
     buildTypes {
