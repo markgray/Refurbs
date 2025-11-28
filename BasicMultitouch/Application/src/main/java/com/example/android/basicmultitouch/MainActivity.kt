2e0a106ecf5e05cb76d7218865491f8fff378cb7
markgray <mark.t.gray@gmail.com>
2025-11-28 05:19:56 -0500

    Added comments in BasicMultitouch

--- BasicMultitouch/Application/src/main/java/com/example/android/basicmultitouch/MainActivity.kt
+++ BasicMultitouch/Application/src/main/java/com/example/android/basicmultitouch/MainActivity.kt
@@ -23,6 +23,7 @@ import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
@@ -35,14 +36,31 @@ import androidx.core.view.updateLayoutParams
  * positions of this pointer and draws them. This example shows the relationship
  * between [MotionEvent] indices, pointer identifiers and actions.
  *
- * @see android.view.MotionEvent
+ * @see MotionEvent
  */
 class MainActivity : AppCompatActivity() {
     /**
-     * Called when the activity is starting. First we call through to our super's implementation of
-     * `onCreate`, then we set our content view to our layout file `R.layout.layout_mainactivity`
-     * (this layout file contains a [TouchDisplayView] widget overlayed over a [TextView] in a
-     * shared [FrameLayout]).
+     * Called when the activity is starting. First we call [enableEdgeToEdge]
+     * to enable edge to edge display, then we call our super's implementation
+     * of `onCreate`, and set our content view to our layout file
+     * `R.layout.layout_mainactivity` (this layout file contains a [TouchDisplayView]
+     * widget overlayed over a [TextView] in a shared [FrameLayout]).
+     *
+     * We initialize our [FrameLayout] variable `rootView`
+     * to the view with ID `R.id.root_view` then call
+     * [ViewCompat.setOnApplyWindowInsetsListener] to take over the policy
+     * for applying window insets to `rootView`, with the `listener`
+     * argument a lambda that accepts the [View] passed the lambda
+     * in variable `v` and the [WindowInsetsCompat] passed the lambda
+     * in variable `windowInsets`. It initializes its [Insets] variable
+     * `insets` to the [WindowInsetsCompat.getInsets] of `windowInsets` with
+     * [WindowInsetsCompat.Type.systemBars] as the argument, then it updates
+     * the layout parameters of `v` to be a [ViewGroup.MarginLayoutParams]
+     * with the left margin set to `insets.left`, the right margin set to
+     * `insets.right`, the top margin set to `insets.top`, and the bottom margin
+     * set to `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED]
+     * to the caller (so that the window insets will not keep passing down to
+     * descendant views).
      *
      * @param savedInstanceState we do not override [onSaveInstanceState] so do not use
      */
@@ -51,8 +69,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_mainactivity)
         val rootView = findViewById<FrameLayout>(R.id.root_view)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
