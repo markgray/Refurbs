c79c6a4165857ca2141356e278feba32f8eb9b2b
Mark Gray <mark.t.gray@gmail.com>
2025-11-28 03:44:49 -0500

    Updated gradle to 9.2.1, updated gradle dependencies in DirectShare

--- DirectShare/Application/build.gradle.kts
+++ DirectShare/Application/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.11.0")
+    implementation("androidx.activity:activity-ktx:1.12.0")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.legacy:legacy-support-v13:1.0.0")
     implementation("androidx.cardview:cardview:1.0.0")
