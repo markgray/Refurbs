1ac0ead214e4bbebd7d9319c770ab320f3c302ac
markgray <mark.t.gray@gmail.com>
2025-11-29 01:18:46 -0500

    Added comments in EmojiCompat

--- EmojiCompat/app/src/main/java/com/example/android/emojicompat/MainActivity.kt
+++ EmojiCompat/app/src/main/java/com/example/android/emojicompat/MainActivity.kt
@@ -17,11 +17,13 @@ package com.example.android.emojicompat
 
 import android.content.Context
 import android.os.Bundle
+import android.view.View
 import android.view.ViewGroup
 import android.widget.ScrollView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
@@ -38,25 +40,44 @@ import java.lang.ref.WeakReference
  */
 class MainActivity : AppCompatActivity() {
     /**
-     * Called when the activity is starting. First we call through to our super's implementation of
-     * `onCreate`, then we set our content view to our layout file `R.layout.activity_main`.
+     * Called when the activity is starting. First we call [enableEdgeToEdge]
+     * to enable edge to edge display, then we call our super's implementation
+     * of `onCreate`, and set our content view to our layout file
+     * `R.layout.activity_main`.
+     *
+     * We initialize our [ScrollView] variable `rootView`
+     * to the view with ID `R.id.scroll` then call
+     * [ViewCompat.setOnApplyWindowInsetsListener] to take over the policy
+     * for applying window insets to `rootView`, with the `listener`
+     * argument a lambda that accepts the [View] passed the lambda
+     * in variable `v` and the [WindowInsetsCompat] passed the lambda
+     * in variable `windowInsets`. It initializes its [Insets] variable
+     * `insets` to the [WindowInsetsCompat.getInsets] of `windowInsets` with
+     * [WindowInsetsCompat.Type.systemBars] as the argument, then it updates
+     * the layout parameters of `v` to be a [ViewGroup.MarginLayoutParams]
+     * with the left margin set to `insets.left`, the right margin set to
+     * `insets.right`, the top margin set to `insets.top`, and the bottom margin
+     * set to `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED]
+     * to the caller (so that the window insets will not keep passing down to
+     * descendant views).
+     *
      * We initialize [TextView] variable `val emojiTextView` by finding the view with id
      * `R.id.emoji_text_view` (this is a [EmojiAppCompatTextView]), then set its text to the
      * formatted string created using `R.string.emoji_text_view` as the format and [EMOJI] as the
      * format argument. We initialize [TextView] variable `val emojiEditText` by finding the view
-     * with id `R.id.emoji_edit_text` (this is a [EmojiAppCompatEditText]), then set its text to the
-     * formatted string created using `R.string.emoji_edit_text` as the format and [EMOJI] as the
-     * format argument. We initialize [TextView] variable `val emojiButton` by finding the view with
-     * id `R.id.emoji_button` (this is a [EmojiAppCompatButton]), then set its text to the formatted
-     * string created using `R.string.emoji_button` as the format and [EMOJI] as the format argument.
-     * We initialize our regular [TextView] variable `val regularTextView` by finding the view with
-     * id `R.id.regular_text_view`, then because it is not supplied by the [EmojiCompat] library we
-     * must get the singleton [EmojiCompat] instance and register a new instance of our class
-     * [InitCallback] subclass with `regularTextView` in order to set its text using the [EmojiCompat]
-     * library. We initialize [TextView] variable `val customTextView` by finding the view with id
-     * `R.id.emoji_custom_text_view` (a  [com.example.android.emojicompat.CustomTextView]), then set
-     * its text to the formatted string created using `R.string.emoji_edit_text` as the format and
-     * [EMOJI] as the format argument.
+     * with id `R.id.emoji_edit_text` (this is a [EmojiAppCompatEditText]), then set its text to
+     * the formatted string created using `R.string.emoji_edit_text` as the format and [EMOJI] as
+     * the format argument. We initialize [TextView] variable `val emojiButton` by finding the
+     * view with id `R.id.emoji_button` (this is a [EmojiAppCompatButton]), then set its text
+     * to the formatted string created using `R.string.emoji_button` as the format and [EMOJI]
+     * as the format argument. We initialize our regular [TextView] variable `val regularTextView`
+     * by finding the view with id `R.id.regular_text_view`, then because it is not supplied by
+     * the [EmojiCompat] library we must get the singleton [EmojiCompat] instance and register
+     * a new instance of our class [InitCallback] subclass with `regularTextView` in order to set
+     * its text using the [EmojiCompat] library. We initialize [TextView] variable
+     * `val customTextView` by finding the view with id `R.id.emoji_custom_text_view`
+     * (a [CustomTextView]), then set its text to the formatted string created using
+     * `R.string.emoji_edit_text` as the format and [EMOJI] as the format argument.
      *
      * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
@@ -65,8 +86,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rootView = findViewById<ScrollView>(R.id.scroll)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
