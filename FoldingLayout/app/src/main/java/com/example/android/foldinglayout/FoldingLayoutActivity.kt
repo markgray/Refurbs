410f6aaa4663e6cc10236ca588b40138e6ec7fd6
markgray <mark.t.gray@gmail.com>
2025-11-29 03:44:15 -0500

    Added comments in FoldingLayout

--- FoldingLayout/app/src/main/java/com/example/android/foldinglayout/FoldingLayoutActivity.kt
+++ FoldingLayout/app/src/main/java/com/example/android/foldinglayout/FoldingLayoutActivity.kt
@@ -53,6 +53,7 @@ import android.widget.SeekBar.OnSeekBarChangeListener
 import android.widget.Spinner
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
@@ -197,33 +198,49 @@ class FoldingLayoutActivity : ComponentActivity() {
     private var mDefaultPaint: Paint? = null
 
     /**
-     * Called when the activity is starting. First we call our super's implementation of `onCreate`,
-     * then we set our content view to our layout file `R.layout.activity_fold`. We initialize our
-     * [ImageView] field [mImageView] by finding the view with id `R.id.image_view`, set its padding
-     * to [ANTIALIAS_PADDING] on all sides, set its scale type (how the image should be resized or
-     * moved to match the size of the [ImageView]) to [ImageView.ScaleType.FIT_XY] (Scale the image
-     * using [Matrix.ScaleToFit.FILL] (scale in X and Y independently, so that src matches dst
-     * exactly)), and then set its content to the jpg with resource id `R.drawable.image`. We then
-     * initialize our [TextureView] field [mTextureView] with a new instance, and set its
-     * [SurfaceTextureListener] to our [SurfaceTextureListener] field [mSurfaceTextureListener]
-     * (its [SurfaceTextureListener.onSurfaceTextureAvailable] override opens the [Camera] and sets
+     * Called when the activity is starting. First we call [enableEdgeToEdge] to enable edge
+     * to edge display, then we call our super's implementation of `onCreate`, and set our
+     * content view to our layout file `R.layout.activity_fold`.
+     *
+     * We initialize our [LinearLayout] variable `rootView` to the view with ID `R.id.root_view`
+     * then call [ViewCompat.setOnApplyWindowInsetsListener] to take over the policy for applying
+     * window insets to `rootView`, with the `listener` argument a lambda that accepts the [View]
+     * passed the lambda in variable `v` and the [WindowInsetsCompat] passed the lambda in variable
+     * `windowInsets`. It initializes its [Insets] variable `insets` to the
+     * [WindowInsetsCompat.getInsets] of `windowInsets` with [WindowInsetsCompat.Type.systemBars]
+     * as the argument, then it updates the layout parameters of `v` to be a
+     * [ViewGroup.MarginLayoutParams] with the left margin set to `insets.left`,
+     * the right margin set to `insets.right`, the top margin set to `insets.top`,
+     * and the bottom margin set to `insets.bottom`. Finally it returns
+     * [WindowInsetsCompat.CONSUMED] to the caller (so that the window insets will not keep
+     * passing down to descendant views).
+     *
+     * We initialize our [ImageView] property [mImageView] by finding the view with id
+     * `R.id.image_view`, set its padding to [ANTIALIAS_PADDING] on all sides, set its scale type
+     * (how the image should be resized or moved to match the size of the [ImageView]) to
+     * [ImageView.ScaleType.FIT_XY] (Scale the image using [Matrix.ScaleToFit.FILL] (scale in X
+     * and Y independently, so that src matches dst exactly), and then set its content to the jpg
+     * with resource id `R.drawable.image`. We then initialize our [TextureView] field
+     * [mTextureView] with a new instance, and set its [SurfaceTextureListener] to our
+     * [SurfaceTextureListener] field [mSurfaceTextureListener] (its
+     * [SurfaceTextureListener.onSurfaceTextureAvailable] override opens the [Camera] and sets
      * the [SurfaceTexture] to be used for live preview of the camera). We then initialize our
      * [SeekBar] field [mAnchorSeekBar] by finding the view with id `R.id.anchor_seek_bar`. We
-     * initialize our [FoldingLayout] field [mFoldLayout] by finding the view with id `R.id.fold_view`,
-     * set its background color to [Color.BLACK], and set its [OnFoldListener] to our [OnFoldListener]
-     * field [mOnFoldListener]. We initialize our [Int] field [mTouchSlop] to the standard constant
-     * for the UI on this device for the distance in pixels a touch can wander before we think the
-     * user is scrolling. We set the [OnSeekBarChangeListener] of our [SeekBar] field [mAnchorSeekBar]
-     * to our [OnSeekBarChangeListener] field [mSeekBarChangeListener] (its
-     * [OnSeekBarChangeListener.onStopTrackingTouch] override sets the anchor factor (relative
-     * location of the anchor point for the folding) of our [FoldingLayout] field [mFoldLayout] to
-     * its current progress divided by 100). We initialize our [Paint] fields [mDefaultPaint] and
-     * [mSepiaPaint] with new instances. We then initialize our [ColorMatrix] variables `var m1`
-     * and `val m2` with new instances. We set the saturation of `m1` to 0 (gray scale) and set
-     * `m2` to scale RED by 1.0, GREEN by .95, BLUE by .82 and ALPHA by 1.0, then set `m1` to the
-     * concatenation of `m2` and `m1` (which will have the same effect as applying `m1` and then
-     * applying `m2`). We then set the color filter of [Paint] field [mSepiaPaint] to a
-     * [ColorMatrixColorFilter] created from `m1`
+     * initialize our [FoldingLayout] field [mFoldLayout] by finding the view with id
+     * `R.id.fold_view`, set its background color to [Color.BLACK], and set its [OnFoldListener]
+     * to our [OnFoldListener] field [mOnFoldListener]. We initialize our [Int] field [mTouchSlop]
+     * to the standard constant for the UI on this device for the distance in pixels a touch can
+     * wander before we think the user is scrolling. We set the [OnSeekBarChangeListener] of our
+     * [SeekBar] field [mAnchorSeekBar] to our [OnSeekBarChangeListener] field
+     * [mSeekBarChangeListener] (its [OnSeekBarChangeListener.onStopTrackingTouch] override sets
+     * the anchor factor (relative location of the anchor point for the folding) of our
+     * [FoldingLayout] field [mFoldLayout] to its current progress divided by 100). We initialize
+     * our [Paint] fields [mDefaultPaint] and [mSepiaPaint] with new instances. We then initialize
+     * our [ColorMatrix] variables `var m1` and `val m2` with new instances. We set the saturation
+     * of `m1` to 0 (gray scale) and set `m2` to scale RED by 1.0, GREEN by .95, BLUE by .82 and
+     * ALPHA by 1.0, then set `m1` to the concatenation of `m2` and `m1` (which will have the same
+     * effect as applying `m1` and then applying `m2`). We then set the color filter of [Paint]
+     * field [mSepiaPaint] to a [ColorMatrixColorFilter] created from `m1`
      *
      * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
@@ -232,8 +249,8 @@ class FoldingLayoutActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fold)
         val rootView = findViewById<LinearLayout>(R.id.root_view)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
