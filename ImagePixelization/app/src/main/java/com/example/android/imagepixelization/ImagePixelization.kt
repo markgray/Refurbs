e0df80cc21bb88576c80a052c020dbb16ed81b4e
markgray <mark.t.gray@gmail.com>
2025-11-29 04:09:59 -0500

    Added comments in ImagePixelization

--- ImagePixelization/app/src/main/java/com/example/android/imagepixelization/ImagePixelization.kt
+++ ImagePixelization/app/src/main/java/com/example/android/imagepixelization/ImagePixelization.kt
@@ -30,6 +30,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
 import android.view.ViewGroup
 import android.view.animation.LinearInterpolator
 import android.widget.CheckBox
@@ -39,6 +40,7 @@ import android.widget.SeekBar
 import android.widget.SeekBar.OnSeekBarChangeListener
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
@@ -109,9 +111,23 @@ class ImagePixelization : ComponentActivity() {
     var mPixelatedBitmap: Bitmap? = null
 
     /**
-     * Called when the activity is starting. First we call our super's implementation of `onCreate`,
-     * then we set our content view to our layout file `R.layout.activity_image_pixelization`. We
-     * initialize our [ImageView] field [mImageView] by finding the view with id `R.id.pixelView`,
+     * Called when the activity is starting. First we call [enableEdgeToEdge] to enable edge to
+     * edge display, then we call our super's implementation of `onCreate`, and set our content
+     * view to our layout file `R.layout.activity_image_pixelization`.
+     *
+     * We initialize our [RelativeLayout] variable `rootView` to the view with ID `R.id.root_view`
+     * then call [ViewCompat.setOnApplyWindowInsetsListener] to take over the policy for applying
+     * window insets to `rootView`, with the `listener` argument a lambda that accepts the [View]
+     * passed the lambda in variable `v` and the [WindowInsetsCompat] passed the lambda in variable
+     * `windowInsets`. It initializes its [Insets] variable `insets` to the
+     * [WindowInsetsCompat.getInsets] of `windowInsets` with
+     * [WindowInsetsCompat.Type.systemBars] as the argument, then it updates the layout parameters
+     * of `v` to be a [ViewGroup.MarginLayoutParams] with the left margin set to `insets.left`,
+     * the right margin set to `insets.right`, the top margin set to `insets.top`, and the bottom
+     * margin set to `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED] to the caller
+     * (so that the window insets will not keep passing down to descendant views).
+     *
+     * We initialize our [ImageView] field [mImageView] by finding the view with id `R.id.pixelView`,
      * and our [SeekBar] field [mSeekBar] by finding the view with id `R.id.seekbar`. We initialize
      * our [Bitmap] field [mImageBitmap] by decoding the jpg with resource id `R.drawable.image`,
      * and set it to be the content of [mImageView]. Finally we set the [OnSeekBarChangeListener] of
@@ -124,8 +140,8 @@ class ImagePixelization : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image_pixelization)
         val rootView = findViewById<RelativeLayout>(R.id.root_view)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
