3ee7cb4437db6243572672d2e559e6a8fab7af82
markgray <mark.t.gray@gmail.com>
2025-11-29 07:37:41 -0500

    Added comments in ListViewExpandingCells

--- ListViewExpandingCells/app/src/main/java/com/example/android/expandingcells/ExpandingCells.kt
+++ ListViewExpandingCells/app/src/main/java/com/example/android/expandingcells/ExpandingCells.kt
@@ -18,10 +18,12 @@
 package com.example.android.expandingcells
 
 import android.os.Bundle
+import android.view.View
 import android.view.ViewGroup
 import android.widget.ListView
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
@@ -47,13 +49,32 @@ class ExpandingCells : ComponentActivity() {
     private var mListView: ExpandingListView? = null
 
     /**
-     * Called when the activity is starting. First we call our super's implementation of `onCreate`,
-     * then we set our content view to our layout file `R.layout.activity_main`. We initialize our
-     * [Array] of [ExpandableListItem] variable `val values` with three different instances. We
-     * initialize our [MutableList] of [ExpandableListItem] variable `val mData` with a new instance
-     * of [ArrayList]. We then loop over [Int] variable `var i` adding [NUM_OF_CELLS] of
-     * [ExpandableListItem] objects constructed by copying the contents of entries of `values` in a
-     * round robin order. We initialize [CustomArrayAdapter] variable `val adapter` with a new
+     * Called when the activity is starting. First we call [enableEdgeToEdge]
+     * to enable edge to edge display, then we call our super's implementation
+     * of `onCreate`, and set our content view to our layout file
+     * `R.layout.activity_main`.
+     *
+     * We initialize our [ExpandingListView] variable `rootView`
+     * to the view with ID `R.id.main_list_view` then call
+     * [ViewCompat.setOnApplyWindowInsetsListener] to take over the policy
+     * for applying window insets to `rootView`, with the `listener`
+     * argument a lambda that accepts the [View] passed the lambda
+     * in variable `v` and the [WindowInsetsCompat] passed the lambda
+     * in variable `windowInsets`. It initializes its [Insets] variable
+     * `insets` to the [WindowInsetsCompat.getInsets] of `windowInsets` with
+     * [WindowInsetsCompat.Type.systemBars] as the argument, then it updates
+     * the layout parameters of `v` to be a [ViewGroup.MarginLayoutParams]
+     * with the left margin set to `insets.left`, the right margin set to
+     * `insets.right`, the top margin set to `insets.top`, and the bottom margin
+     * set to `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED]
+     * to the caller (so that the window insets will not keep passing down to
+     * descendant views).
+     *
+     * We initialize our [Array] of [ExpandableListItem] variable `val values` with three different
+     * instances. We initialize our [MutableList] of [ExpandableListItem] variable `val mData` with
+     * a new instance of [ArrayList]. We then loop over [Int] variable `var i` adding [NUM_OF_CELLS]
+     * of [ExpandableListItem] objects constructed by copying the contents of entries of `values`
+     * in a round robin order. We initialize [CustomArrayAdapter] variable `val adapter` with a new
      * instance which uses `mData` as its dataset, and the layout file `R.layout.list_view_item` to
      * display each item in that dataset. We initialize [ExpandingListView] field [mListView] by
      * finding the view with id `R.id.main_list_view`, set its adapter to be `adapter` and call its
@@ -66,8 +87,8 @@ class ExpandingCells : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rootView = findViewById<ExpandingListView>(R.id.main_list_view)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
