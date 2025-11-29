db2926b5033206a4d79463d05d6401c88a6d45c4
markgray <mark.t.gray@gmail.com>
2025-11-29 04:26:04 -0500

    Added comments in InteractiveChart

--- InteractiveChart/app/src/main/java/com/example/android/interactivechart/MainActivity.kt
+++ InteractiveChart/app/src/main/java/com/example/android/interactivechart/MainActivity.kt
@@ -19,9 +19,11 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
 import android.view.ViewGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.ComponentActivity
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
@@ -36,9 +38,24 @@ class MainActivity : ComponentActivity() {
     private lateinit var mGraphView: InteractiveLineGraphView
 
     /**
-     * Called when the activity is starting. First we call our super's implementation of `onCreate`,
-     * then we set our content view to our layout file `R.layout.activity_main`, and initialize our
-     * [InteractiveLineGraphView] field [mGraphView] by finding the view with ID `R.id.chart`
+     * Called when the activity is starting. First we call [enableEdgeToEdge] to enable edge
+     * to edge display, then we call our super's implementation of `onCreate`, set our
+     * content view to our layout file `R.layout.activity_main`, and initialize our
+     * [InteractiveLineGraphView] field [mGraphView] by finding the view with ID `R.id.chart`.
+     *
+     *We call [ViewCompat.setOnApplyWindowInsetsListener] to take over the policy
+     * for applying window insets to [mGraphView], with the `listener`
+     * argument a lambda that accepts the [View] passed the lambda
+     * in variable `v` and the [WindowInsetsCompat] passed the lambda
+     * in variable `windowInsets`. It initializes its [Insets] variable
+     * `insets` to the [WindowInsetsCompat.getInsets] of `windowInsets` with
+     * [WindowInsetsCompat.Type.systemBars] as the argument, then it updates
+     * the layout parameters of `v` to be a [ViewGroup.MarginLayoutParams]
+     * with the left margin set to `insets.left`, the right margin set to
+     * `insets.right`, the top margin set to `insets.top`, and the bottom margin
+     * set to `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED]
+     * to the caller (so that the window insets will not keep passing down to
+     * descendant views).
      *
      * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
@@ -48,8 +65,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mGraphView = findViewById(R.id.chart)
-        ViewCompat.setOnApplyWindowInsetsListener(mGraphView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(mGraphView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
