1176321911225bcc90da931e1b4cd1f71d69be09
markgray <mark.t.gray@gmail.com>
2025-11-29 07:21:03 -0500

    Added comments in ListViewCellInsertion

--- ListViewCellInsertion/app/src/main/java/com/example/android/insertingcells/InsertingCells.kt
+++ ListViewCellInsertion/app/src/main/java/com/example/android/insertingcells/InsertingCells.kt
@@ -31,6 +31,7 @@ import android.widget.ListView
 import android.widget.RelativeLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
@@ -84,13 +85,32 @@ class InsertingCells : ComponentActivity(), OnRowAdditionAnimationListener {
     private var mCellHeight: Int = 0
 
     /**
-     * Called when the activity is starting. First we call our super's implementation of `onCreate`,
-     * then we set our content view to our layout file `R.layout.activity_main`. We initialize our
-     * [Array] of [ListItemObject] field [mValues] with a new instance, filled with three
-     * [ListItemObject] instances constructed to contain "Chameleon", "Rock", and "Flower" text
-     * views as well as the resource id's for jpg images to use for the associated [ImageView]
-     * (`R.drawable.chameleon`, `R.drawable.rock`, and `R.drawable.flower` respectively). We
-     * initialize our [Int] field [mCellHeight] by retrieving the dimensional for the resource
+     * Called when the activity is starting. First we call [enableEdgeToEdge]
+     * to enable edge to edge display, then we call our super's implementation
+     * of `onCreate`, and set our content view to our layout file
+     * `R.layout.activity_main`.
+     *
+     * We initialize our [RelativeLayout] variable `rootView`
+     * to the view with ID `R.id.relative_layout` then call
+     * [ViewCompat.setOnApplyWindowInsetsListener] to take over the policy
+     * for applying window insets to `rootView`, with the `listener`
+     * argument a lambda that accepts the [View] passed the lambda
+     * in variable `v` and the [WindowInsetsCompat] passed the lambda
+     * in variable `windowInsets`. It initializes its [Insets] variable
+     * `insets` to the [WindowInsetsCompat.getInsets] of `windowInsets` with
+     * [WindowInsetsCompat.Type.systemBars] as the argument, then it updates
+     * the layout parameters of `v` to be a [ViewGroup.MarginLayoutParams]
+     * with the left margin set to `insets.left`, the right margin set to
+     * `insets.right`, the top margin set to `insets.top`, and the bottom margin
+     * set to `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED]
+     * to the caller (so that the window insets will not keep passing down to
+     * descendant views).
+     *
+     * We initialize our [Array] of [ListItemObject] field [mValues] with a new instance, filled
+     * with three [ListItemObject] instances constructed to contain "Chameleon", "Rock", and
+     * "Flower" text views as well as the resource id's for jpg images to use for the associated
+     * [ImageView] (`R.drawable.chameleon`, `R.drawable.rock`, and `R.drawable.flower` respectively).
+     * We initialize our [Int] field [mCellHeight] by retrieving the dimensional for the resource
      * id `R.dimen.cell_height` from a [Resources] instance for our application's package. We
      * initialize [List] of [ListItemObject] variable `val mData` with a new instance, and
      * [CustomArrayAdapter] variable `val mAdapter` with an instance which will display the
@@ -110,8 +130,8 @@ class InsertingCells : ComponentActivity(), OnRowAdditionAnimationListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rootView = findViewById<RelativeLayout>(R.id.relative_layout)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
