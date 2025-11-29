f042f185b5df81dcdf4be0be10ce04c9d7647a22
Mark Gray <mark.t.gray@gmail.com>
2025-11-29 04:22:23 -0500

    Updated gradle to 9.2.1, updated gradle dependencies and increased minSdk to 23 in Snake

--- Snake/app/build.gradle.kts
+++ Snake/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 36
     defaultConfig {
         applicationId = "com.example.android.snake"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 36
         testApplicationId = "com.example.android.snake.tests"
         testInstrumentationRunner = "android.test.InstrumentationTestRunner"
@@ -35,6 +35,6 @@ android {
     }
     namespace = "com.example.android.snake"
     dependencies {
-        implementation("androidx.activity:activity-ktx:1.11.0")
+        implementation("androidx.activity:activity-ktx:1.12.0")
     }
 }
