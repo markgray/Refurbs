44b33c640c00df695cdf2a564740e8859147a2ab
markgray <mark.t.gray@gmail.com>
2025-11-29 07:13:44 -0500

    Added comments in ListViewAnimations

--- ListViewAnimations/app/src/main/java/com/example/android/listviewanimations/ListViewAnimations.kt
+++ ListViewAnimations/app/src/main/java/com/example/android/listviewanimations/ListViewAnimations.kt
@@ -34,6 +34,7 @@ import android.widget.ListView
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
@@ -48,9 +49,28 @@ import java.util.Collections
  */
 class ListViewAnimations : ComponentActivity() {
     /**
-     * Called when the activity is starting. First we call our super's implementation of `onCreate`,
-     * then we set our content view to our layout file `R.layout.activity_list_view_animations`. We
-     * then initialize [CheckBox] variable `val vpaCB` by finding the view with id `R.id.vpaCB`
+     * Called when the activity is starting. First we call [enableEdgeToEdge]
+     * to enable edge to edge display, then we call our super's implementation
+     * of `onCreate`, and set our content view to our layout file
+     * `R.layout.activity_list_view_animations`.
+     *
+     * We initialize our [LinearLayout] variable `rootView`
+     * to the view with ID `R.id.root_view` then call
+     * [ViewCompat.setOnApplyWindowInsetsListener] to take over the policy
+     * for applying window insets to `rootView`, with the `listener`
+     * argument a lambda that accepts the [View] passed the lambda
+     * in variable `v` and the [WindowInsetsCompat] passed the lambda
+     * in variable `windowInsets`. It initializes its [Insets] variable
+     * `insets` to the [WindowInsetsCompat.getInsets] of `windowInsets` with
+     * [WindowInsetsCompat.Type.systemBars] as the argument, then it updates
+     * the layout parameters of `v` to be a [ViewGroup.MarginLayoutParams]
+     * with the left margin set to `insets.left`, the right margin set to
+     * `insets.right`, the top margin set to `insets.top`, and the bottom margin
+     * set to `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED]
+     * to the caller (so that the window insets will not keep passing down to
+     * descendant views).
+     *
+     * We then initialize [CheckBox] variable `val vpaCB` by finding the view with id `R.id.vpaCB`
      * ("ViewPropertyAnimator") initialize [CheckBox] variable `val setTransientStateCB` by finding
      * the view with id `R.id.setTransientStateCB` ("Transient State") and initialize [ListView]
      * variable `val listView` by finding the view with id `R.id.list_view`. We initialize
@@ -73,8 +93,8 @@ class ListViewAnimations : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_view_animations)
         val rootView = findViewById<LinearLayout>(R.id.root_view)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
@@ -98,7 +118,7 @@ class ListViewAnimations : ComponentActivity() {
             objects = cheeseList
         )
         listView.adapter = adapter
-        listView.onItemClickListener = OnItemClickListener { parent, view, position, id ->
+        listView.onItemClickListener = OnItemClickListener { parent, view, position, _ ->
             /**
              * Callback method to be invoked when an item in this [ListView] has been clicked. We
              * initialize [String] variable `val item` with the string returned by the
