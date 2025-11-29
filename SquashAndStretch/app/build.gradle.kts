a4a14191505bf5a6281fa9fff4727473b9d283de
Mark Gray <mark.t.gray@gmail.com>
2025-11-29 04:27:34 -0500

    Updated gradle to 9.2.1, updated gradle dependencies and increased minSdk to 23 in SquashAndStretch

--- SquashAndStretch/app/build.gradle.kts
+++ SquashAndStretch/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 36
     defaultConfig {
         applicationId = "com.example.squashandstretch"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 36
     }
 
@@ -34,6 +34,6 @@ android {
     }
     namespace = "com.example.squashandstretch"
     dependencies {
-        implementation("androidx.activity:activity-ktx:1.11.0")
+        implementation("androidx.activity:activity-ktx:1.12.0")
     }
 }
