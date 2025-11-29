a93e54a11883c3f702bf15eefc0ab6e8e0f0e49e
markgray <mark.t.gray@gmail.com>
2025-11-29 07:27:54 -0500

    Added comments in ListViewDeletion

--- ListViewDeletion/app/src/main/java/com/example/android/listviewdeletion/ListViewDeletion.kt
+++ ListViewDeletion/app/src/main/java/com/example/android/listviewdeletion/ListViewDeletion.kt
@@ -33,6 +33,7 @@ import android.widget.LinearLayout
 import android.widget.ListView
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
@@ -53,13 +54,32 @@ class ListViewDeletion : ComponentActivity() {
     val mCheckedViews: ArrayList<View> = ArrayList()
 
     /**
-     * Called when the activity is starting. First we call our super's implementation of `onCreate`,
-     * then we set our content view to our layout file `R.layout.activity_list_view_deletion`. We
-     * initialize [Button] variable `val deleteButton` by finding the view with id `R.id.deleteButton`
-     * ("Delete Selected"), initialize [CheckBox] variable `val usePositionsCB` by finding the view
-     * with id `R.id.usePositionsCB` ("Use Positions"), and initialize [ListView] variable
-     * `val listView` by finding the view with id `R.id.list_view`. We initialize [ArrayList] of
-     * [String] variable `val cheeseList` with a new instance, and add all the strings in
+     * Called when the activity is starting. First we call [enableEdgeToEdge]
+     * to enable edge to edge display, then we call our super's implementation
+     * of `onCreate`, and set our content view to our layout file
+     * `R.layout.activity_list_view_deletion`.
+     *
+     * We initialize our [LinearLayout] variable `rootView`
+     * to the view with ID `R.id.root_view` then call
+     * [ViewCompat.setOnApplyWindowInsetsListener] to take over the policy
+     * for applying window insets to `rootView`, with the `listener`
+     * argument a lambda that accepts the [View] passed the lambda
+     * in variable `v` and the [WindowInsetsCompat] passed the lambda
+     * in variable `windowInsets`. It initializes its [Insets] variable
+     * `insets` to the [WindowInsetsCompat.getInsets] of `windowInsets` with
+     * [WindowInsetsCompat.Type.systemBars] as the argument, then it updates
+     * the layout parameters of `v` to be a [ViewGroup.MarginLayoutParams]
+     * with the left margin set to `insets.left`, the right margin set to
+     * `insets.right`, the top margin set to `insets.top`, and the bottom margin
+     * set to `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED]
+     * to the caller (so that the window insets will not keep passing down to
+     * descendant views).
+     *
+     * We initialize [Button] variable `val deleteButton` by finding the view with id
+     * `R.id.deleteButton` ("Delete Selected"), initialize [CheckBox] variable `val usePositionsCB`
+     * by finding the view with id `R.id.usePositionsCB` ("Use Positions"), and initialize [ListView]
+     * variable `val listView` by finding the view with id `R.id.list_view`. We initialize [ArrayList]
+     * of [String] variable `val cheeseList` with a new instance, and add all the strings in
      * [Cheeses.sCheeseStrings] to it. We initialize our [StableArrayAdapter] variable `val adapter`
      * with a new instance which uses the layout file with resource id
      * [android.R.layout.simple_list_item_multiple_choice] to instantiate item views (it consists of
@@ -88,8 +108,8 @@ class ListViewDeletion : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_view_deletion)
         val rootView = findViewById<LinearLayout>(R.id.root_view)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
@@ -218,7 +238,7 @@ class ListViewDeletion : ComponentActivity() {
             mCheckedViews.clear()
             adapter.notifyDataSetChanged()
         }
-        listView.onItemClickListener = OnItemClickListener { parent, view, position, id ->
+        listView.onItemClickListener = OnItemClickListener { _, view, position, _ ->
 
             /**
              * Callback method to be invoked when an item in the [ListView] has been clicked.
