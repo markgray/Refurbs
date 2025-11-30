9257426de92b8704408c65d3a00ac8cf353c1c02
markgray <mark.t.gray@gmail.com>
2025-11-30 07:12:56 -0500

    Added comments in Snake

--- Snake/app/src/main/java/com/example/android/snake/Snake.kt
+++ Snake/app/src/main/java/com/example/android/snake/Snake.kt
@@ -25,6 +25,7 @@ import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
@@ -44,24 +45,38 @@ class Snake : ComponentActivity() {
     private lateinit var mSnakeView: SnakeView
 
     /**
-     * Called when Activity is first created. Turns off the title bar, sets up the content views,
-     * and fires up the [SnakeView]. First we call our super's implementation of `onCreate`, then
-     * we set our content view to our layout file `R.layout.snake_layout`. We initialize our
-     * [SnakeView] field  [mSnakeView] by finding the view with id `R.id.snake`, then call its
-     * method [SnakeView.setDependentViews] to have it set its [TextView] field [SnakeView.mStatusText]
-     * to the view with id `R.id.text`, its [View] field [SnakeView.mArrowsView] to the view with id
-     * `R.id.arrowContainer`, and its [View] field [SnakeView.mBackgroundView] to the view with id
-     * `R.id.background`. If our [Bundle] parameter [savedInstanceState] is `null` we were just
-     * launched so we call the [SnakeView.setMode] method of our field [mSnakeView] to have it set
-     * its mode to `READY`. If it is not `null` we are being restored so we initialize [Bundle]
-     * variable `val map` with the bundle stored in [savedInstanceState] under the key [ICICLE_KEY],
-     * and if that is not `null` we call the [SnakeView.restoreState] method of [mSnakeView] to have
-     * it restore its state from `map`, and if it is `null` we call the [SnakeView.setMode] method
-     * of [mSnakeView] to have it set its mode to `PAUSE`. Finally we set the [View.OnTouchListener]
-     * of [mSnakeView] to an anonymous class whose `onTouch` override changes the direction the snake
-     * is moving based on the location of the touch if the game state of [mSnakeView] is `RUNNING`,
-     * or starts the game running if it was not running by having it move the snake in the `MOVE_UP`
-     * direction.
+     * Called when the activity is starting. First we call [enableEdgeToEdge] to enable
+     * edge to edge display, then we call our super's implementation of `onCreate`, and
+     * set our content view to our layout file `R.layout.snake_layout`.
+     *
+     * We initialize our [FrameLayout] variable `rootView` to the view with ID `R.id.root_view`
+     * then call [ViewCompat.setOnApplyWindowInsetsListener] to take over the policy for
+     * applying window insets to `rootView`, with the `listener` argument a lambda that accepts
+     * the [View] passed the lambda in variable `v` and the [WindowInsetsCompat] passed the lambda
+     * in variable `windowInsets`. It initializes its [Insets] variable `insets` to the
+     * [WindowInsetsCompat.getInsets] of `windowInsets` with [WindowInsetsCompat.Type.systemBars]
+     * as the argument, then it updates the layout parameters of `v` to be a
+     * [ViewGroup.MarginLayoutParams] with the left margin set to `insets.left`, the right margin
+     * set to `insets.right`, the top margin set to `insets.top`, and the bottom margin
+     * set to `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED]
+     * to the caller (so that the window insets will not keep passing down to
+     * descendant views).
+     *
+     * We initialize our [SnakeView] field  [mSnakeView] by finding the view with id `R.id.snake`,
+     * then call its method [SnakeView.setDependentViews] to have it set its [TextView] field
+     * [SnakeView.mStatusText] to the view with id `R.id.text`, its [View] field
+     * [SnakeView.mArrowsView] to the view with id `R.id.arrowContainer`, and its [View] field
+     * [SnakeView.mBackgroundView] to the view with id `R.id.background`. If our [Bundle] parameter
+     * [savedInstanceState] is `null` we were just launched so we call the [SnakeView.setMode]
+     * method of our field [mSnakeView] to have it set its mode to `READY`. If it is not `null` we
+     * are being restored after a configuration change so we initialize [Bundle] variable `val map`
+     * with the bundle stored in [savedInstanceState] under the key [ICICLE_KEY], and if that is not
+     * `null` we call the [SnakeView.restoreState] method of [mSnakeView] to have it restore its
+     * state from `map`, and if it is `null` we call the [SnakeView.setMode] method of [mSnakeView]
+     * to have it set its mode to `PAUSE`. Finally we set the [View.OnTouchListener] of [mSnakeView]
+     * to an anonymous class whose `onTouch` override changes the direction the snake is moving
+     * based on the location of the touch if the game state of [mSnakeView] is `RUNNING`, or starts
+     * the game running if it was not running by having it move the snake in the `MOVE_UP` direction.
      *
      * @param savedInstanceState If the activity is being re-initialized after previously being shut
      * down then this Bundle contains the data it most recently supplied in [onSaveInstanceState].
@@ -72,8 +87,8 @@ class Snake : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.snake_layout)
         val rootView = findViewById<FrameLayout>(R.id.root_view)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
