b4c72a29cb6cd17df07f450ffdf3340627822f35
markgray <mark.t.gray@gmail.com>
2025-11-29 23:03:03 -0500

    Added comments in MultiPropertyAnimations

--- MultiPropertyAnimations/app/src/main/java/com/example/android/multipropertyanimations/MultiPropertyAnimations.kt
+++ MultiPropertyAnimations/app/src/main/java/com/example/android/multipropertyanimations/MultiPropertyAnimations.kt
@@ -26,6 +26,7 @@ import android.view.ViewPropertyAnimator
 import android.widget.LinearLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
@@ -40,8 +41,21 @@ import androidx.core.view.updateLayoutParams
  */
 class MultiPropertyAnimations : ComponentActivity() {
     /**
-     * Called when the activity is starting. First we call our super's implementation of `onCreate`,
-     * then we set our content view to our layout file `R.layout.activity_multi_property_animations`
+     * Called when the activity is starting. First we call [enableEdgeToEdge] to enable edge
+     * to edge display, then we call our super's implementation of `onCreate`, and set our
+     * content view to our layout file `R.layout.activity_main`.
+     *
+     * We initialize our [LinearLayout] variable `rootView` to the view with ID `R.id.root_view`
+     * then call [ViewCompat.setOnApplyWindowInsetsListener] to take over the policy for applying
+     * window insets to `rootView`, with the `listener` argument a lambda that accepts the [View]
+     * passed the lambda in variable `v` and the [WindowInsetsCompat] passed the lambda in variable
+     * `windowInsets`. It initializes its [Insets] variable `insets` to the
+     * [WindowInsetsCompat.getInsets] of `windowInsets` with [WindowInsetsCompat.Type.systemBars]
+     * as the argument, then it updates the layout parameters of `v` to be a
+     * [ViewGroup.MarginLayoutParams] with the left margin set to `insets.left`, the right margin
+     * set to `insets.right`, the top margin set to `insets.top`, and the bottom margin set to
+     * `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED] to the caller (so that
+     * the window insets will not keep passing down to descendant views).
      *
      * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
@@ -52,8 +66,8 @@ class MultiPropertyAnimations : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_multi_property_animations)
         val rootView = findViewById<LinearLayout>(R.id.root_view)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
