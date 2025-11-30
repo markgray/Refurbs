c01830e8b9809e970ad49e81c848c5ff5291d20e
markgray <mark.t.gray@gmail.com>
2025-11-30 03:11:05 -0500

    Added comments in PdfRendererBasic

--- PdfRendererBasic/Application/src/main/java/com/example/android/pdfrendererbasic/MainActivity.kt
+++ PdfRendererBasic/Application/src/main/java/com/example/android/pdfrendererbasic/MainActivity.kt
@@ -21,10 +21,12 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
@@ -36,14 +38,29 @@ import androidx.fragment.app.FragmentTransaction
  */
 class MainActivity : AppCompatActivity() {
     /**
-     * Called when the activity is starting. First we call our super's implementation of `onCreate`,
-     * then we set our content view to our layout file `R.layout.activity_main_real`. If our [Bundle]
-     * parameter [savedInstanceState] is `null` this is the first time we were called, so we fetch
-     * the [FragmentManager] for interacting with fragments associated with this activity and use it
-     * to begin a [FragmentTransaction] to which we chain an [FragmentTransaction.add] command to
-     * add a new instance of [PdfRendererBasicFragment] to the view in our layout with ID `R.id.container`
-     * using [FRAGMENT_PDF_RENDERER_BASIC] ("pdf_renderer_basic") as its fragment tag, and to this
-     * command we chain a command to `commit` the [FragmentTransaction].
+     * Called when the activity is starting. First we call [enableEdgeToEdge] to enable
+     * edge to edge display, then we call our super's implementation of `onCreate`, and
+     * set our content view to our layout file `R.layout.activity_main_real`.
+     *
+     * We initialize our [FrameLayout] variable `rootView` to the view with ID `R.id.container`
+     * then call [ViewCompat.setOnApplyWindowInsetsListener] to take over the policy for applying
+     * window insets to `rootView`, with the `listener` argument a lambda that accepts the [View]
+     * passed the lambda in variable `v` and the [WindowInsetsCompat] passed the lambda in variable
+     * `windowInsets`. It initializes its [Insets] variable `insets` to the
+     * [WindowInsetsCompat.getInsets] of `windowInsets` with [WindowInsetsCompat.Type.systemBars]
+     * as the argument, then it updates the layout parameters of `v` to be a
+     * [ViewGroup.MarginLayoutParams] with the left margin set to `insets.left`, the right margin
+     * set to `insets.right`, the top margin set to `insets.top`, and the bottom margin set to
+     * `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED] to the caller (so that the
+     * window insets will not keep passing down to descendant views).
+     *
+     * If our [Bundle] parameter [savedInstanceState] is `null` this is the first time we were
+     * called, so we fetch the [FragmentManager] for interacting with fragments associated with this
+     * activity and use it to begin a [FragmentTransaction] to which we chain an
+     * [FragmentTransaction.add] command to add a new instance of [PdfRendererBasicFragment] to the
+     * view in our layout with ID `R.id.container` using [FRAGMENT_PDF_RENDERER_BASIC]
+     * ("pdf_renderer_basic") as its fragment tag, and to this command we chain a command to
+     * `commit` the [FragmentTransaction].
      *
      * @param savedInstanceState if this is `null`, this is the first time we were called so we need
      * to construct and add our fragment [PdfRendererBasicFragment]. If it is not `null` we are
@@ -55,8 +72,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_real)
         val rootView = findViewById<FrameLayout>(R.id.container)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
