8647a1e8f8afbedb4a29bf63ade03a868c3772a5
markgray <mark.t.gray@gmail.com>
2025-11-30 05:04:19 -0500

    Added comments in RoomRxJava

--- RoomRxJava/app/src/main/java/com/example/android/observability/ui/UserActivity.kt
+++ RoomRxJava/app/src/main/java/com/example/android/observability/ui/UserActivity.kt
@@ -27,6 +27,7 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
@@ -83,9 +84,26 @@ class UserActivity : AppCompatActivity() {
     private val mDisposable = CompositeDisposable()
 
     /**
-     * Called when the activity is starting. First we call through to our super's implementation of
-     * `onCreate`, then we set our content view to our layout file `R.layout.activity_user`. We
-     * initialize our [TextView] field [mUserName] by finding the view with id `R.id.user_name`,
+     * Called when the activity is starting. First we call [enableEdgeToEdge] to enable
+     * edge to edge display, then we call our super's implementation of `onCreate`, and
+     * set our content view to our layout file `R.layout.activity_user`.
+     *
+     * We initialize our [LinearLayout] variable `rootView` to the view with ID
+     * `R.id.root_view` then call [ViewCompat.setOnApplyWindowInsetsListener] to
+     * take over the policy for applying window insets to `rootView`, with the
+     * `listener` argument a lambda that accepts the [View] passed the lambda
+     * in variable `v` and the [WindowInsetsCompat] passed the lambda
+     * in variable `windowInsets`. It initializes its [Insets] variable
+     * `insets` to the [WindowInsetsCompat.getInsets] of `windowInsets` with
+     * [WindowInsetsCompat.Type.systemBars] as the argument, then it updates
+     * the layout parameters of `v` to be a [ViewGroup.MarginLayoutParams]
+     * with the left margin set to `insets.left`, the right margin set to
+     * `insets.right`, the top margin set to `insets.top`, and the bottom margin
+     * set to `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED]
+     * to the caller (so that the window insets will not keep passing down to
+     * descendant views).
+     *
+     * We initialize our [TextView] field [mUserName] by finding the view with id `R.id.user_name`,
      * [EditText] field [mUserNameInput] by finding the view with id `R.id.user_name_input`, and our
      * [Button] field [mUpdateButton] by finding the view with id `R.id.update_user`. We initialize
      * our [ViewModelFactory] field [mViewModelFactory] to the [ViewModelFactory] returned by the
@@ -101,8 +119,8 @@ class UserActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
         val rootView = findViewById<LinearLayout>(R.id.root_view)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
@@ -119,7 +137,7 @@ class UserActivity : AppCompatActivity() {
         mUpdateButton = findViewById(R.id.update_user)
         mViewModelFactory = Injection.provideViewModelFactory(this)
         mViewModel = ViewModelProvider(this, mViewModelFactory!!)[UserViewModel::class.java]
-        mUpdateButton!!.setOnClickListener { v: View? -> updateUserName() }
+        mUpdateButton!!.setOnClickListener { _: View? -> updateUserName() }
     }
 
     /**
