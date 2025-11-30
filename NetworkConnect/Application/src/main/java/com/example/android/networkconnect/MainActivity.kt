a8932c792f2f3423dfa10ec9c97d288f7a9541e7
markgray <mark.t.gray@gmail.com>
2025-11-29 23:40:19 -0500

    Added comments in NetworkConnect

--- NetworkConnect/Application/src/main/java/com/example/android/networkconnect/MainActivity.kt
+++ NetworkConnect/Application/src/main/java/com/example/android/networkconnect/MainActivity.kt
@@ -25,11 +25,13 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
@@ -59,23 +61,40 @@ class MainActivity : AppCompatActivity(), DownloadCallback {
     private var mDownloading = false
 
     /**
-     * Called when the activity is starting. First we call through to our super's implementation of
-     * `onCreate`, then we set our content view to our layout file `R.layout.sample_main`. We
-     * initialize our [TextView] field  [mDataText] by finding the view with id `R.id.data_text`,
-     * and initialize our [NetworkFragment] field [mNetworkFragment] with a handle to an instance
-     * constructed to retrieve the url "[Google](https://www.google.com)" (creating it if
-     * need be, or using the fragment manager to fetch a handle to an already running instance).
+     * Called when the activity is starting. First we call [enableEdgeToEdge] to enable
+     * edge to edge display, then we call our super's implementation of `onCreate`, and
+     * set our content view to our layout file `R.layout.sample_main`.
+     *
+     * We initialize our [LinearLayout] variable `rootView` to the view with ID
+     * `R.id.root_view` then call [ViewCompat.setOnApplyWindowInsetsListener] to
+     * take over the policy for applying window insets to `rootView`, with the
+     * `listener` argument a lambda that accepts the [View] passed the lambda
+     * in variable `v` and the [WindowInsetsCompat] passed the lambda
+     * in variable `windowInsets`. It initializes its [Insets] variable
+     * `insets` to the [WindowInsetsCompat.getInsets] of `windowInsets` with
+     * [WindowInsetsCompat.Type.systemBars] as the argument, then it updates
+     * the layout parameters of `v` to be a [ViewGroup.MarginLayoutParams]
+     * with the left margin set to `insets.left`, the right margin set to
+     * `insets.right`, the top margin set to `insets.top`, and the bottom margin
+     * set to `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED]
+     * to the caller (so that the window insets will not keep passing down to
+     * descendant views).
+     *
+     * We initialize our [TextView] field  [mDataText] by finding the view with id
+     * `R.id.data_text`, and initialize our [NetworkFragment] field [mNetworkFragment]
+     * with a handle to an instance constructed to retrieve the url
+     * "[Google](https://www.google.com)" (creating it if need be, or using the
+     * fragment manager to fetch a handle to an already running instance).
      *
      * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
-        // TODO: Make text move down automatically
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sample_main)
         val rootView = findViewById<LinearLayout>(R.id.root_view)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
