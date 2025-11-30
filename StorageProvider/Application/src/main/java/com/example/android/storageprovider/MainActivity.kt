c63aab4eb436bbe4506a1a33669b3a9316ec2d52
markgray <mark.t.gray@gmail.com>
2025-11-30 07:45:30 -0500

    Added comments in StorageProvider

--- StorageProvider/Application/src/main/java/com/example/android/storageprovider/MainActivity.kt
+++ StorageProvider/Application/src/main/java/com/example/android/storageprovider/MainActivity.kt
@@ -22,9 +22,11 @@ import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
@@ -44,14 +46,31 @@ import com.example.android.common.logger.MessageOnlyLogFilter
  */
 class MainActivity : SampleActivityBase() {
     /**
-     * Called when the activity is starting. First we call through to our super's implementation of
-     * `onCreate`, then we set our content view to our layout file `R.layout.activity_main`. If the
-     * [FragmentManager] for interacting with fragments associated with this activity is unable to
-     * find a fragment with the tag [FRAGTAG], we initialize [FragmentTransaction] variable
-     * `val transaction` by using the [FragmentManager.beginTransaction] method to begin it, create
-     * a new instance for [StorageProviderFragment] variable `val fragment`, use `transaction`'s
-     * [FragmentTransaction.add] method to add `fragment` to `transaction` with the tag [FRAGTAG]
-     * and then use `transaction`'s [FragmentTransaction.commit] method to commit `transaction`.
+     * Called when the activity is starting. First we call [enableEdgeToEdge] to enable
+     * edge to edge display, then we call our super's implementation of `onCreate`, and
+     * set our content view to our layout file `R.layout.activity_main`.
+     *
+     * We initialize our [LinearLayout] variable `rootView` to the view with ID
+     * `R.id.sample_main_layout` then call [ViewCompat.setOnApplyWindowInsetsListener]
+     * to take over the policy for applying window insets to `rootView`, with the
+     * `listener` argument a lambda that accepts the [View] passed the lambda in
+     * variable `v` and the [WindowInsetsCompat] passed the lambda in variable
+     * `windowInsets`. It initializes its [Insets] variable `insets` to the
+     * [WindowInsetsCompat.getInsets] of `windowInsets` with [WindowInsetsCompat.Type.systemBars]
+     * as the argument, then it updates the layout parameters of `v` to be a
+     * [ViewGroup.MarginLayoutParams] with the left margin set to `insets.left`,
+     * the right margin set to `insets.right`, the top margin set to `insets.top`,
+     * and the bottom margin set to `insets.bottom`. Finally it returns
+     * [WindowInsetsCompat.CONSUMED] to the caller (so that the window insets
+     * will not keep passing down to descendant views).
+     *
+     * If the [FragmentManager] for interacting with fragments associated with this activity is
+     * unable to find a fragment with the tag [FRAGTAG], we initialize [FragmentTransaction]
+     * variable `val transaction` by using the [FragmentManager.beginTransaction] method to begin
+     * it, create a new instance for [StorageProviderFragment] variable `val fragment`, use
+     * `transaction`'s [FragmentTransaction.add] method to add `fragment` to `transaction` with
+     * the tag [FRAGTAG] and then use `transaction`'s [FragmentTransaction.commit] method to commit
+     * `transaction`.
      *
      * @param savedInstanceState we do not override `onSaveInstanceState` so do not use
      */
@@ -61,8 +80,8 @@ class MainActivity : SampleActivityBase() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rootView = findViewById<LinearLayout>(R.id.sample_main_layout)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
