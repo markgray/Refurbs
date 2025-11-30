1c133afa6f498eda660384787c1a69892e9bb4c8
markgray <mark.t.gray@gmail.com>
2025-11-30 04:44:51 -0500

    Add comments in RequestDuringLayout

--- RequestDuringLayout/app/src/main/java/com/example/android/requestduringlayout/RequestDuringLayout.kt
+++ RequestDuringLayout/app/src/main/java/com/example/android/requestduringlayout/RequestDuringLayout.kt
@@ -23,6 +23,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
@@ -36,19 +37,35 @@ import androidx.core.view.updateLayoutParams
  */
 class RequestDuringLayout : ComponentActivity() {
     /**
-     * Called when the activity is starting. First we call our super's implementation of `onCreate`,
-     * then we set our content view to our layout file `R.layout.activity_request_during_layout`. We
-     * initialize our [MyLayout] variable `val myLayout` by finding the view with id `R.id.container`,
-     * initialize [Button] variable `val addViewButton` by finding the view with id `R.id.addView`
-     * ("Add"), initialize [Button] variable `val removeViewButton` by finding the view with id
-     * `R.id.removeView` ("Remove") and initialize [Button] variable `val forceLayoutButton` by
-     * finding the view with id `R.id.forceLayout` ("Layout"). We set the [View.OnClickListener] of
-     * `addViewButton` to an anonymous class which sets the [MyLayout.mAddRequestPending] field of
-     * `myLayout` to `true`, then calls its `MyLayout.requestLayout` method, and we set the
-     * [View.OnClickListener] of `removeViewButton` to an anonymous class which sets the
-     * [MyLayout.mRemoveRequestPending] field of `myLayout` to true, then calls its
-     * `MyLayout.requestLayout` method. Finally we set the [View.OnClickListener] of `removeViewButton`
-     * to an anonymous class which calls the `MyLayout.requestLayout` method of `myLayout`.
+     * Called when the activity is starting. First we call [enableEdgeToEdge] to enable
+     * edge to edge display, then we call our super's implementation of `onCreate`, and
+     * set our content view to our layout file `R.layout.activity_request_during_layout`.
+     *
+     * We initialize our [MyLayout] variable `myLayout` to the view with ID `R.id.container`
+     * then call [ViewCompat.setOnApplyWindowInsetsListener] to take over the policy for
+     * applying window insets to `myLayout`, with the `listener` argument a lambda that
+     * accepts the [View] passed the lambda in variable `v` and the [WindowInsetsCompat]
+     * passed the lambda in variable `windowInsets`. It initializes its [Insets] variable
+     * `insets` to the [WindowInsetsCompat.getInsets] of `windowInsets` with
+     * [WindowInsetsCompat.Type.systemBars] as the argument, then it updates
+     * the layout parameters of `v` to be a [ViewGroup.MarginLayoutParams]
+     * with the left margin set to `insets.left`, the right margin set to
+     * `insets.right`, the top margin set to `insets.top`, and the bottom margin
+     * set to `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED]
+     * to the caller (so that the window insets will not keep passing down to
+     * descendant views).
+     *
+     * We initialize [Button] variable `val addViewButton` by finding the view with id
+     * `R.id.addView` ("Add"), initialize [Button] variable `val removeViewButton` by finding
+     * the view with id `R.id.removeView` ("Remove") and initialize [Button] variable
+     * `val forceLayoutButton` by finding the view with id `R.id.forceLayout` ("Layout").
+     * We set the [View.OnClickListener] of `addViewButton` to an anonymous class which sets the
+     * [MyLayout.mAddRequestPending] field of `myLayout` to `true`, then calls its
+     * `MyLayout.requestLayout` method, and we set the [View.OnClickListener] of `removeViewButton`
+     * to an anonymous class which sets the [MyLayout.mRemoveRequestPending] field of `myLayout` to
+     * `true`, then calls its [MyLayout.requestLayout] method. Finally we set the
+     * [View.OnClickListener] of `forceLayoutButton` to an anonymous class which calls the
+     *[ MyLayout.requestLayout] method of `myLayout`.
      *
      * @param savedInstanceState we do not override [onSaveInstanceState] so do not use
      */
@@ -57,8 +74,8 @@ class RequestDuringLayout : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_request_during_layout)
         val myLayout: MyLayout = findViewById(R.id.container)
-        ViewCompat.setOnApplyWindowInsetsListener(myLayout) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(myLayout) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
@@ -73,14 +90,14 @@ class RequestDuringLayout : ComponentActivity() {
         val addViewButton: Button = findViewById(R.id.addView)
         val removeViewButton: Button = findViewById(R.id.removeView)
         val forceLayoutButton: Button = findViewById(R.id.forceLayout)
-        addViewButton.setOnClickListener { v: View? ->
+        addViewButton.setOnClickListener { _: View? ->
             myLayout.mAddRequestPending = true
             myLayout.requestLayout()
         }
-        removeViewButton.setOnClickListener { v: View? ->
+        removeViewButton.setOnClickListener { _: View? ->
             myLayout.mRemoveRequestPending = true
             myLayout.requestLayout()
         }
-        forceLayoutButton.setOnClickListener { v: View? -> myLayout.requestLayout() }
+        forceLayoutButton.setOnClickListener { _: View? -> myLayout.requestLayout() }
     }
 }
