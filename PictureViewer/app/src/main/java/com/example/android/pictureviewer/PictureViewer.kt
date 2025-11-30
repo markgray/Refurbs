b13419de6cc7a01ac505747785b40622020525f5
markgray <mark.t.gray@gmail.com>
2025-11-30 03:56:12 -0500

    Added comments in PictureViewer

--- PictureViewer/app/src/main/java/com/example/android/pictureviewer/PictureViewer.kt
+++ PictureViewer/app/src/main/java/com/example/android/pictureviewer/PictureViewer.kt
@@ -29,6 +29,7 @@ import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
@@ -60,25 +61,44 @@ class PictureViewer : ComponentActivity() {
     )
 
     /**
-     * Called when the activity is starting. First we call our super's implementation of `onCreate`,
-     * then we set our content view to our layout file `R.layout.activity_picture_viewer` (this is a
-     * [FrameLayout] containing two [ImageView] widgets occupying the same space). We initialize
-     * [ImageView] variable `val prevImageView` by finding the view with id `R.id.prevImageView`,
-     * and [ImageView] variable `val nextImageView` by finding the view with id `R.id.nextImageView`,
-     * and set the background color of both to [Color.TRANSPARENT]. We also set the duration of both
-     * view's [ViewPropertyAnimator] to 1000ms. We initialize the [BitmapDrawable] variable
-     * `val drawables` array with an instance which will hold as many [BitmapDrawable] objects as
-     * there are entries in our [IntArray] field [drawableIDs]. We then loop over [Int] variable
-     * `var i` for these entries creating [Bitmap] variable `val bitmap` by decoding the jpg whose
-     * resource id is at index `i` in [drawableIDs] and setting the contents of the `i`th entry in
-     * `drawables` to the [BitmapDrawable] created from `bitmap`. When done loading up `drawables`
-     * we set the content of `prevImageView` to `drawables` at index 0 and the content of
-     * `nextImageView` to `drawables` at index 1. Finally we set the [View.OnClickListener]
-     * of `prevImageView` to an anonymous instance whose [View.OnClickListener.onClick] override
-     * animates the alpha of `prevImageView` to 0 and the alpha of `nextImageView` to 1 with an end
-     * action consisting of an anonymous [Runnable] whose [Runnable.run] override sets the content
-     * of `prevImageView` and `nextImageView` to the next round robin [BitmapDrawable] they should
-     * display, and sets the alpha of `nextImageView` to 0 and the alpha of `prevImageView` to 1.
+     * Called when the activity is starting. First we call [enableEdgeToEdge]
+     * to enable edge to edge display, then we call our super's implementation
+     * of `onCreate`, and set our content view to our layout file
+     * `R.layout.activity_picture_viewer` (this is a [FrameLayout] containing two
+     * [ImageView] widgets occupying the same space).
+     *
+     * We initialize our [FrameLayout] variable `rootView` to the view with ID
+     * `R.id.root_view` then call [ViewCompat.setOnApplyWindowInsetsListener] to
+     * take over the policy for applying window insets to `rootView`, with the
+     * `listener` argument a lambda that accepts the [View] passed the lambda
+     * in variable `v` and the [WindowInsetsCompat] passed the lambda
+     * in variable `windowInsets`. It initializes its [Insets] variable
+     * `insets` to the [WindowInsetsCompat.getInsets] of `windowInsets` with
+     * [WindowInsetsCompat.Type.systemBars] as the argument, then it updates
+     * the layout parameters of `v` to be a [ViewGroup.MarginLayoutParams]
+     * with the left margin set to `insets.left`, the right margin set to
+     * `insets.right`, the top margin set to `insets.top`, and the bottom margin
+     * set to `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED]
+     * to the caller (so that the window insets will not keep passing down to
+     * descendant views).
+     *
+     * We initialize [ImageView] variable `val prevImageView` by finding the view with id
+     * `R.id.prevImageView`, and [ImageView] variable `val nextImageView` by finding the view
+     * with id `R.id.nextImageView`, and set the background color of both to [Color.TRANSPARENT].
+     * We also set the duration of both view's [ViewPropertyAnimator] to 1000ms. We initialize the
+     * [BitmapDrawable] variable `val drawables` array with an instance which will hold as many
+     * [BitmapDrawable] objects as there are entries in our [IntArray] field [drawableIDs]. We then
+     * loop over [Int] variable `var i` for these entries creating [Bitmap] variable `val bitmap`
+     * by decoding the jpg whose resource id is at index `i` in [drawableIDs] and setting the
+     * contents of the `i`th entry in `drawables` to the [BitmapDrawable] created from `bitmap`.
+     * When done loading up `drawables` we set the content of `prevImageView` to `drawables` at
+     * index 0 and the content of `nextImageView` to `drawables` at index 1. Finally we set the
+     * [View.OnClickListener] of `prevImageView` to an anonymous instance whose
+     * [View.OnClickListener.onClick] override animates the alpha of `prevImageView` to 0 and the
+     * alpha of `nextImageView` to 1 with an end action consisting of an anonymous [Runnable] whose
+     * [Runnable.run] override sets the content of `prevImageView` and `nextImageView` to the next
+     * round robin [BitmapDrawable] they should display, and sets the alpha of `nextImageView` to 0
+     * and the alpha of `prevImageView` to 1.
      *
      * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
@@ -87,8 +107,8 @@ class PictureViewer : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_picture_viewer)
         val rootView = findViewById<FrameLayout>(R.id.root_view)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
@@ -120,7 +140,7 @@ class PictureViewer : ComponentActivity() {
         }
         prevImageView.setImageDrawable(drawables[0])
         nextImageView.setImageDrawable(drawables[1])
-        prevImageView.setOnClickListener { v: View? ->
+        prevImageView.setOnClickListener { _: View? ->
             // Use ViewPropertyAnimator to fade the previous imageView out and the next one in
             prevImageView.animate().alpha(0f).withLayer()
             // When the animation ends, set up references to change the prev/next
