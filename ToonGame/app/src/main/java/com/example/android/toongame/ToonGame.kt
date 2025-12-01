57d450e6d67984c6053fb023f21f153b6143520f
markgray <mark.t.gray@gmail.com>
2025-11-30 23:36:37 -0500

    Added comments in ToonGame

--- ToonGame/app/src/main/java/com/example/android/toongame/ToonGame.kt
+++ ToonGame/app/src/main/java/com/example/android/toongame/ToonGame.kt
@@ -45,6 +45,7 @@ import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
@@ -78,14 +79,34 @@ class ToonGame : ComponentActivity() {
     var mContainer: ViewGroup? = null
 
     /**
-     * Called when the activity is starting. First we call our super's implementation of `onCreate`,
-     * then we call the [overridePendingTransition] method to cancel any pending incoming or outgoing
-     * animation (by using 0 as the resource id for both `enterAnim` and `exitAnim`). Then
-     * we set our content view to our layout file `R.layout.activity_toon_game`. We initialize our
-     * [Button] field [mStarter] by finding the view with id `R.id.startButton` ("Play!"), initialize
-     * our [ViewGroup] field [mContainer] by finding the [RelativeLayout] in our layout file with
-     * id `R.id.container`, set the [OnTouchListener] of [mStarter] to [funButtonListener] and set
-     * the duration of its [ViewPropertyAnimator] to 100ms.
+     * Called when the activity is starting. First we call [enableEdgeToEdge]
+     * to enable edge to edge display, then we call our super's implementation
+     * of `onCreate`.
+     *
+     * If [Build.VERSION.SDK_INT] is greater than or equal to 34 we call the modern version of
+     * [overrideActivityTransition] with its `overrideType` argument `OVERRIDE_TRANSITION_OPEN`, and
+     * both its `enterAnim` and `exitAnim` arguments 0, otherwise we call the deprecated 2 argument
+     * overload with both its `enterAnim` and `exitAnim` arguments 0 to cancel any pending incoming
+     * or outgoing animation (by using 0 as the resource id for both `enterAnim` and `exitAnim`).
+     * Then we set our content view to our layout file `R.layout.activity_toon_game`.
+     *
+     * We initialize our [RelativeLayout] variable `rootView` to the view with ID
+     * `R.id.container` then call [ViewCompat.setOnApplyWindowInsetsListener] to take
+     * over the policy for applying window insets to `rootView`, with the `listener`
+     * argument a lambda that accepts the [View] passed the lambda in variable `v`
+     * and the [WindowInsetsCompat] passed the lambda in variable `windowInsets`.
+     * It initializes its [Insets] variable `insets` to the [WindowInsetsCompat.getInsets]
+     * of `windowInsets` with [WindowInsetsCompat.Type.systemBars] as the argument, then
+     * it updates the layout parameters of `v` to be a [ViewGroup.MarginLayoutParams] with
+     * the left margin set to `insets.left`, the right margin set to `insets.right`, the
+     * top margin set to `insets.top`, and the bottom margin set to `insets.bottom`.
+     * Finally it returns [WindowInsetsCompat.CONSUMED] to the caller (so that the window
+     * insets will not keep passing down to descendant views).
+     *
+     * We initialize our [Button] field [mStarter] by finding the view with id `R.id.startButton`
+     * ("Play!"), initialize our [ViewGroup] field [mContainer] to `rootView` (the view with ID
+     * `R.id.container` recall), set the [OnTouchListener] of [mStarter] to [funButtonListener] and
+     * set the duration of its [ViewPropertyAnimator] to 100ms.
      *
      * @param savedInstanceState we do not override [onSaveInstanceState] so do not use
      */
@@ -94,15 +115,19 @@ class ToonGame : ComponentActivity() {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         if (Build.VERSION.SDK_INT >= 34) {
-            overrideActivityTransition(OVERRIDE_TRANSITION_OPEN, 0, 0)
+            overrideActivityTransition(
+                /* overrideType = */ OVERRIDE_TRANSITION_OPEN,
+                /* enterAnim = */ 0,
+                /* exitAnim = */ 0
+            )
         } else {
             @Suppress("DEPRECATION") // Needed for SDK older than 34
-            overridePendingTransition(0, 0)
+            overridePendingTransition(/* enterAnim = */ 0, /* exitAnim = */ 0)
         }
         setContentView(R.layout.activity_toon_game)
         val rootView = findViewById<RelativeLayout>(R.id.container)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
