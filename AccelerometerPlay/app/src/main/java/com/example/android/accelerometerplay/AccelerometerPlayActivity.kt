c390e037a549d637652a0b497d0f33c75d4483b5
markgray <mark.t.gray@gmail.com>
2025-12-04 23:26:02 -0500

    Updated gradle dependencies and added ime padding in AccelerometerPlay

--- AccelerometerPlay/app/src/main/java/com/example/android/accelerometerplay/AccelerometerPlayActivity.kt
+++ AccelerometerPlay/app/src/main/java/com/example/android/accelerometerplay/AccelerometerPlayActivity.kt
@@ -39,11 +39,11 @@ import android.widget.FrameLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.core.graphics.Insets
+import androidx.core.graphics.scale
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
 import kotlin.math.sqrt
-import androidx.core.graphics.scale
 
 /**
  * This is an example of using the accelerometer to integrate the device's
@@ -131,18 +131,23 @@ class AccelerometerPlayActivity : ComponentActivity() {
      *  * FLAG_ALLOW_LOCK_WHILE_SCREEN_ON: as long as this window is visible to the user, allow
      *  the lock screen to activate while the screen is on.
      *
-     * We initialize our [FrameLayout] variable `rootView` to the view with ID `android.R.id.content`
-     * then call [ViewCompat.setOnApplyWindowInsetsListener] to take over the policy for applying
-     * window insets to `rootView`, with the listener argument a lambda that accepts the [View]
-     * passed the lambda in variable `v` and the [WindowInsetsCompat] passed the lambda in variable
-     * `windowInsets`. It initializes its [Insets] variable `insets` to the
-     * [WindowInsetsCompat.getInsets] of `windowInsets` with [WindowInsetsCompat.Type.systemBars]
-     * as the argument, then it updates the layout parameters of `v` to be a
-     * [ViewGroup.MarginLayoutParams] with the left margin set to `insets.left`,
-     * the right margin set to `insets.right`, the top margin set to `insets.top`,
-     * and the bottom margin set to `insets.bottom`. Finally it returns
-     * [WindowInsetsCompat.CONSUMED] to the caller (so that the window insets
-     * will not keep passing down to descendant views).
+     * We initialize our [FrameLayout] variable `rootView`
+     * to the view with ID `android.R.id.content` then call
+     * [ViewCompat.setOnApplyWindowInsetsListener] to take over the policy
+     * for applying window insets to `rootView`, with the `listener`
+     * argument a lambda that accepts the [View] passed the lambda
+     * in variable `v` and the [WindowInsetsCompat] passed the lambda
+     * in variable `windowInsets`. It initializes its [Insets] variable
+     * `systemBars` to the [WindowInsetsCompat.getInsets] of `windowInsets` with
+     * [WindowInsetsCompat.Type.systemBars] as the argument. It then gets the insets for the
+     * IME (keyboard) using `WindowInsetsCompat.Type.ime()`. It then updates
+     * the layout parameters of `v` to be a [ViewGroup.MarginLayoutParams]
+     * with the left margin set to `systemBars.left`, the right margin set to
+     * `systemBars.right`, the top margin set to `systemBars.top`, and the bottom margin
+     * set to the maximum of the system bars bottom inset and the IME bottom inset.
+     * Finally it returns [WindowInsetsCompat.CONSUMED]
+     * to the caller (so that the window insets will not keep passing down to
+     * descendant views).
      *
      * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
@@ -179,19 +184,20 @@ class AccelerometerPlayActivity : ComponentActivity() {
             or WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON)
         val rootView = window.decorView.findViewById<FrameLayout>(android.R.id.content)
         ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
-            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val systemBars = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val ime = windowInsets.getInsets(WindowInsetsCompat.Type.ime())
+
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
-                leftMargin = insets.left
-                rightMargin = insets.right
-                topMargin = insets.top
-                bottomMargin = insets.bottom
+                leftMargin = systemBars.left
+                rightMargin = systemBars.right
+                topMargin = systemBars.top
+                bottomMargin = systemBars.bottom.coerceAtLeast(ime.bottom)
             }
             // Return CONSUMED if you don't want want the window insets to keep passing
             // down to descendant views.
             WindowInsetsCompat.CONSUMED
-        }
-    }
+        }    }
 
     /**
      * Called after [onRestoreInstanceState], [onRestart], or [onPause], for
