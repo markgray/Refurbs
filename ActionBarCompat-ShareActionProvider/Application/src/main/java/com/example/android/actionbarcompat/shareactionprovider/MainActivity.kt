198c344d070a93e700570d0a7c789aa002a6ad45
markgray <mark.t.gray@gmail.com>
2025-12-05 00:49:32 -0500

    Added ime padding in ActionBarCompat-ShareActionProvider

--- ActionBarCompat-ShareActionProvider/Application/src/main/java/com/example/android/actionbarcompat/shareactionprovider/MainActivity.kt
+++ ActionBarCompat-ShareActionProvider/Application/src/main/java/com/example/android/actionbarcompat/shareactionprovider/MainActivity.kt
@@ -71,19 +71,23 @@ class MainActivity : AppCompatActivity() {
      * to enable edge to edge display, then we call our super's implementation
      * of `onCreate`, and set our content view to our layout file `R.layout.sample_main`.
      *
-     * We initialize our ContentFrameLayout variable `rootView` to the view with ID
-     * `android.R.id.content` then call [ViewCompat.setOnApplyWindowInsetsListener]
-     * to take over the policy for applying window insets to `rootView`, with the
-     * `listener` argument a lambda that accepts the [View] passed the lambda in
-     * variable `v` and the [WindowInsetsCompat] passed the lambda in variable
-     * `windowInsets`. It initializes its [Insets] variable `insets` to the
-     * [WindowInsetsCompat.getInsets] of `windowInsets` with [WindowInsetsCompat.Type.systemBars]
-     * as the argument, then it updates the layout parameters of `v` to be a
-     * [ViewGroup.MarginLayoutParams] with the left margin set to `insets.left`,
-     * the right margin set to `insets.right`, the top margin set to `insets.top`,
-     * and the bottom margin set to `insets.bottom`. Finally it returns
-     * [WindowInsetsCompat.CONSUMED] to the caller (so that the window insets will
-     * not keep passing down to descendant views).
+     * We initialize our [ContentFrameLayout] variable `rootView`
+     * to the view with ID `android.R.id.content` then call
+     * [ViewCompat.setOnApplyWindowInsetsListener] to take over the policy
+     * for applying window insets to `rootView`, with the `listener`
+     * argument a lambda that accepts the [View] passed the lambda
+     * in variable `v` and the [WindowInsetsCompat] passed the lambda
+     * in variable `windowInsets`. It initializes its [Insets] variable
+     * `systemBars` to the [WindowInsetsCompat.getInsets] of `windowInsets` with
+     * [WindowInsetsCompat.Type.systemBars] as the argument. It then gets the insets for the
+     * IME (keyboard) using `WindowInsetsCompat.Type.ime()`. It then updates
+     * the layout parameters of `v` to be a [ViewGroup.MarginLayoutParams]
+     * with the left margin set to `systemBars.left`, the right margin set to
+     * `systemBars.right`, the top margin set to `systemBars.top`, and the bottom margin
+     * set to the maximum of the system bars bottom inset and the IME bottom inset.
+     * Finally it returns [WindowInsetsCompat.CONSUMED]
+     * to the caller (so that the window insets will not keep passing down to
+     * descendant views).
      *
      * We find the view with id `R.id.viewpager` to set our [ViewPager] variable `val vp`,
      * and add our [OnPageChangeListener] field [mOnPageChangeListener] as an
@@ -100,13 +104,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.sample_main)
         val rootView = window.decorView.findViewById<ContentFrameLayout>(android.R.id.content)
         ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
-            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val systemBars = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val ime = windowInsets.getInsets(WindowInsetsCompat.Type.ime())
+
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
-                leftMargin = insets.left
-                rightMargin = insets.right
-                topMargin = insets.top
-                bottomMargin = insets.bottom
+                leftMargin = systemBars.left
+                rightMargin = systemBars.right
+                topMargin = systemBars.top
+                bottomMargin = systemBars.bottom.coerceAtLeast(ime.bottom)
             }
             // Return CONSUMED if you don't want want the window insets to keep passing
             // down to descendant views.
