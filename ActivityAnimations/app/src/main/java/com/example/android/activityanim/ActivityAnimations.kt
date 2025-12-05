0b684b63cd75557feaf6cbe9f3f97bfdd18ab7ec
markgray <mark.t.gray@gmail.com>
2025-12-05 04:40:17 -0500

    Added ime padding in ActivityAnimations

--- ActivityAnimations/app/src/main/java/com/example/android/activityanim/ActivityAnimations.kt
+++ ActivityAnimations/app/src/main/java/com/example/android/activityanim/ActivityAnimations.kt
@@ -36,14 +36,15 @@ import android.view.View.OnClickListener
 import android.view.ViewGroup
 import android.widget.GridLayout
 import android.widget.ImageView
-import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.graphics.Insets
-import androidx.core.view.OnApplyWindowInsetsListener
+import androidx.core.graphics.drawable.toDrawable
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
-import androidx.core.graphics.drawable.toDrawable
+import com.example.android.activityanim.ActivityAnimations.Companion.PACKAGE
+import com.example.android.activityanim.ActivityAnimations.Companion.sAnimatorScale
 
 /**
  * This example shows how to create a custom activity animation when you want something more
@@ -79,17 +80,23 @@ class ActivityAnimations : AppCompatActivity() {
      * to our layout file `R.layout.activity_animations`. We initialize our [ColorMatrix] variable
      * `grayMatrix` to a new instance, set its saturation to 0 (0 maps the color to gray-scale), and
      * create [ColorMatrixColorFilter] variable `grayscaleFilter` from it. We initialize [GridLayout]
-     * property [mGridLayout] by finding the view with id `R.id.gridLayout`. Then we call
-     * [ViewCompat.setOnApplyWindowInsetsListener] to set an [OnApplyWindowInsetsListener] on
-     * [mGridLayout] to take over the policy for applying window insets to [mGridLayout], with the
-     * `listener` argument a lambda that accepts the [View] passed the lambda in variable `v` and
-     * the [WindowInsetsCompat] passed the lambda in variable `windowInsets`. It initializes its
-     * [Insets] variable `insets` to the [WindowInsetsCompat.getInsets] of `windowInsets` with
-     * [WindowInsetsCompat.Type.systemBars] as the argument, then it updates the layout parameters
-     * of `v` to be a [ViewGroup.MarginLayoutParams] with the left margin set to `insets.left`, the
-     * right margin set to `insets.right`, the top margin set to `insets.top`, and the bottom margin
-     * set to `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED] to the caller (so
-     * that the window insets will not be passed down to descendant views).
+     * property [mGridLayout] by finding the view with id `R.id.gridLayout`.
+     *
+     * Then we call [ViewCompat.setOnApplyWindowInsetsListener] to take over
+     * the policy for applying window insets to [mGridLayout], with the `listener`
+     * argument a lambda that accepts the [View] passed the lambda
+     * in variable `v` and the [WindowInsetsCompat] passed the lambda
+     * in variable `windowInsets`. It initializes its [Insets] variable
+     * `systemBars` to the [WindowInsetsCompat.getInsets] of `windowInsets` with
+     * [WindowInsetsCompat.Type.systemBars] as the argument. It then gets the insets for the
+     * IME (keyboard) using `WindowInsetsCompat.Type.ime()`. It then updates
+     * the layout parameters of `v` to be a [ViewGroup.MarginLayoutParams]
+     * with the left margin set to `systemBars.left`, the right margin set to
+     * `systemBars.right`, the top margin set to `systemBars.top`, and the bottom margin
+     * set to the maximum of the system bars bottom inset and the IME bottom inset.
+     * Finally it returns [WindowInsetsCompat.CONSUMED]
+     * to the caller (so that the window insets will not keep passing down to
+     * descendant views).
      *
      * We set the column count of [mGridLayout] to 3, and enable its use of default margins around
      * children based on the child's visual characteristics. We initialize [Resources] variable
@@ -127,13 +134,15 @@ class ActivityAnimations : AppCompatActivity() {
         val grayscaleFilter = ColorMatrixColorFilter(grayMatrix)
         mGridLayout = findViewById(R.id.gridLayout)
         ViewCompat.setOnApplyWindowInsetsListener(mGridLayout!!) { v: View, windowInsets: WindowInsetsCompat ->
-            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val systemBars = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val ime = windowInsets.getInsets(WindowInsetsCompat.Type.ime())
+
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
-                leftMargin = insets.left
-                rightMargin = insets.right
-                topMargin = insets.top + supportActionBar!!.height
-                bottomMargin = insets.bottom
+                leftMargin = systemBars.left
+                rightMargin = systemBars.right
+                topMargin = systemBars.top
+                bottomMargin = systemBars.bottom.coerceAtLeast(ime.bottom)
             }
             // Return CONSUMED if you don't want want the window insets to keep passing
             // down to descendant views.
--- ActivityAnimations/app/src/main/java/com/example/android/activityanim/PictureDetailsActivity.kt
+++ ActivityAnimations/app/src/main/java/com/example/android/activityanim/PictureDetailsActivity.kt
@@ -140,17 +140,24 @@ class PictureDetailsActivity : AppCompatActivity() {
      * edge display then we call our super's implementation of `onCreate` and set our content view
      * to our layout file `R.layout.picture_info`. We initialize our [ImageView] property [mImageView]
      * by finding the view with id `R.id.imageView`, and our [FrameLayout] property [mTopLevelLayout]
-     * by finding the view with id `R.id.topLevelLayout`. Next we use the method
-     * [ViewCompat.setOnApplyWindowInsetsListener] to set an [OnApplyWindowInsetsListener] on
-     * [mTopLevelLayout] to take over the policy for applying window insets to [mTopLevelLayout],
-     * with the `listener` argument a lambda that accepts the [View] passed the lambda in variable
-     * `v` and the [WindowInsetsCompat] passed the lambda in variable `windowInsets`. It initializes
-     * its [Insets] variable `insets` to the [WindowInsetsCompat.getInsets] of `windowInsets` with
-     * [WindowInsetsCompat.Type.systemBars] as the argument, then it updates the layout parameters
-     * of `v` to be a [ViewGroup.MarginLayoutParams] with the left margin set to `insets.left`, the
-     * right margin set to `insets.right`, the top margin set to `insets.top`, and the bottom margin
-     * set to `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED] to the caller (so
-     * that the window insets will not keep passing down to descendant views).
+     * by finding the view with id `R.id.topLevelLayout`.
+     *
+     * Next we use the method [ViewCompat.setOnApplyWindowInsetsListener] to set an
+     * [OnApplyWindowInsetsListener] on [mTopLevelLayout] to take over the policy for
+     * applying window insets to [mTopLevelLayout], with the `listener`
+     * argument a lambda that accepts the [View] passed the lambda
+     * in variable `v` and the [WindowInsetsCompat] passed the lambda
+     * in variable `windowInsets`. It initializes its [Insets] variable
+     * `systemBars` to the [WindowInsetsCompat.getInsets] of `windowInsets` with
+     * [WindowInsetsCompat.Type.systemBars] as the argument. It then gets the insets for the
+     * IME (keyboard) using `WindowInsetsCompat.Type.ime()`. It then updates
+     * the layout parameters of `v` to be a [ViewGroup.MarginLayoutParams]
+     * with the left margin set to `systemBars.left`, the right margin set to
+     * `systemBars.right`, the top margin set to `systemBars.top`, and the bottom margin
+     * set to the maximum of the system bars bottom inset and the IME bottom inset.
+     * Finally it returns [WindowInsetsCompat.CONSUMED]
+     * to the caller (so that the window insets will not keep passing down to
+     * descendant views).
      *
      * We initialize our [ShadowLayout] property [mShadowLayout] by finding the view with id
      * `R.id.shadowLayout`, and our [TextView] property [mTextView] by finding the view with id
@@ -205,13 +212,15 @@ class PictureDetailsActivity : AppCompatActivity() {
         mImageView = findViewById(R.id.imageView)
         mTopLevelLayout = findViewById(R.id.topLevelLayout)
         ViewCompat.setOnApplyWindowInsetsListener(mTopLevelLayout!!) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val systemBars = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val ime = windowInsets.getInsets(WindowInsetsCompat.Type.ime())
+
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
-                leftMargin = insets.left
-                rightMargin = insets.right
-                topMargin = insets.top
-                bottomMargin = insets.bottom
+                leftMargin = systemBars.left
+                rightMargin = systemBars.right
+                topMargin = systemBars.top
+                bottomMargin = systemBars.bottom.coerceAtLeast(ime.bottom)
             }
             // Return CONSUMED if you don't want want the window insets to keep passing
             // down to descendant views.
