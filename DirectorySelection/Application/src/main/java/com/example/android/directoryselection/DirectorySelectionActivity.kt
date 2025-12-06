034f0369f742c56a322203b7fcd1cab69c90a5be
markgray <mark.t.gray@gmail.com>
2025-12-06 03:57:17 -0500

    Added ime padding in DirectorySelection

--- DirectorySelection/Application/src/main/java/com/example/android/directoryselection/DirectorySelectionActivity.kt
+++ DirectorySelection/Application/src/main/java/com/example/android/directoryselection/DirectorySelectionActivity.kt
@@ -37,17 +37,22 @@ class DirectorySelectionActivity : FragmentActivity() {
      * edge display, then we call our super's implementation of `onCreate`, and set our content
      * view to our layout file `R.layout.activity_directory_selection`.
      *
-     * We initialize our [FrameLayout] variable `rootView` to the view with ID `R.id.container`
-     * then call [ViewCompat.setOnApplyWindowInsetsListener] to take over the policy for applying
-     * window insets to `rootView`, with the `listener` argument a lambda that accepts the [View]
-     * passed the lambda in variable `v` and the [WindowInsetsCompat] passed the lambda in variable
-     * `windowInsets`. It initializes its [Insets] variable `insets` to the
-     * [WindowInsetsCompat.getInsets] of `windowInsets` with [WindowInsetsCompat.Type.systemBars]
-     * as the argument, then it updates the layout parameters of `v` to be a
-     * [ViewGroup.MarginLayoutParams] with the left margin set to `insets.left`, the right margin
-     * set to `insets.right`, the top margin set to `insets.top`, and the bottom margin
-     * set to `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED] to the caller
-     * (so that the window insets will not keep passing down to descendant views).
+     * We initialize our [FrameLayout] variable `rootView` to the view with ID
+     * `R.id.container` then call [ViewCompat.setOnApplyWindowInsetsListener] to take
+     * over the policy for applying window insets to `rootView`, with the `listener`
+     * argument a lambda that accepts the [View] passed the lambda
+     * in variable `v` and the [WindowInsetsCompat] passed the lambda
+     * in variable `windowInsets`. It initializes its [Insets] variable
+     * `systemBars` to the [WindowInsetsCompat.getInsets] of `windowInsets` with
+     * [WindowInsetsCompat.Type.systemBars] as the argument. It then gets the insets for the
+     * IME (keyboard) using [WindowInsetsCompat.Type.ime]. It then updates
+     * the layout parameters of `v` to be a [ViewGroup.MarginLayoutParams]
+     * with the left margin set to `systemBars.left`, the right margin set to
+     * `systemBars.right`, the top margin set to `systemBars.top`, and the bottom margin
+     * set to the maximum of the system bars bottom inset and the IME bottom inset.
+     * Finally it returns [WindowInsetsCompat.CONSUMED]
+     * to the caller (so that the window insets will not keep passing down to
+     * descendant views).
      *
      * If our [Bundle] parameter [savedInstanceState] is `null` this is the first time that we were
      * called so we need to construct and add our [DirectorySelectionFragment]. To do this we use
@@ -59,20 +64,21 @@ class DirectorySelectionActivity : FragmentActivity() {
      * (`null`) in which case we need to create and add our fragment. On a
      * configuration change our fragment will be restored by the system.
      */
-    @Suppress("ReplaceNotNullAssertionWithElvisReturn")
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_directory_selection)
         val rootView = findViewById<FrameLayout>(R.id.container)
         ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
-            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val systemBars = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val ime = windowInsets.getInsets(WindowInsetsCompat.Type.ime())
+
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
-                leftMargin = insets.left
-                rightMargin = insets.right
-                topMargin = insets.top+actionBar!!.height/2
-                bottomMargin = insets.bottom
+                leftMargin = systemBars.left
+                rightMargin = systemBars.right
+                topMargin = systemBars.top
+                bottomMargin = systemBars.bottom.coerceAtLeast(ime.bottom)
             }
             // Return CONSUMED if you don't want want the window insets to keep passing
             // down to descendant views.
