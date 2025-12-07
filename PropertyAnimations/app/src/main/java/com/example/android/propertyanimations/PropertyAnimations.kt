96a92e5467613b45cfdd16028ab8c45028bf4eca
markgray <mark.t.gray@gmail.com>
2025-12-07 06:42:51 -0500

    Added ime padding in PropertyAnimations

--- PropertyAnimations/app/src/main/java/com/example/android/propertyanimations/PropertyAnimations.kt
+++ PropertyAnimations/app/src/main/java/com/example/android/propertyanimations/PropertyAnimations.kt
@@ -60,14 +60,16 @@ class PropertyAnimations : ComponentActivity() {
      * `R.id.container` then call [ViewCompat.setOnApplyWindowInsetsListener] to
      * take over the policy for applying window insets to `rootView`, with the
      * `listener` argument a lambda that accepts the [View] passed the lambda
-     * in variable `v` and the [WindowInsetsCompat] passed the lambda in variable
-     * `windowInsets`. It initializes its [Insets] variable `insets` to the
-     * [WindowInsetsCompat.getInsets] of `windowInsets` with
-     * [WindowInsetsCompat.Type.systemBars] as the argument, then it updates
+     * in variable `v` and the [WindowInsetsCompat] passed the lambda
+     * in variable `windowInsets`. It initializes its [Insets] variable
+     * `systemBars` to the [WindowInsetsCompat.getInsets] of `windowInsets` with
+     * [WindowInsetsCompat.Type.systemBars] as the argument. It then gets the insets for the
+     * IME (keyboard) using [WindowInsetsCompat.Type.ime]. It then updates
      * the layout parameters of `v` to be a [ViewGroup.MarginLayoutParams]
-     * with the left margin set to `insets.left`, the right margin set to
-     * `insets.right`, the top margin set to `insets.top`, and the bottom margin
-     * set to `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED]
+     * with the left margin set to `systemBars.left`, the right margin set to
+     * `systemBars.right`, the top margin set to `systemBars.top`, and the bottom margin
+     * set to the maximum of the system bars bottom inset and the IME bottom inset.
+     * Finally it returns [WindowInsetsCompat.CONSUMED]
      * to the caller (so that the window insets will not keep passing down to
      * descendant views).
      *
@@ -121,13 +123,15 @@ class PropertyAnimations : ComponentActivity() {
         setContentView(R.layout.activity_property_animations)
         val rootView = findViewById<LinearLayout>(R.id.container)
         ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
-            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val systemBars = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val ime = windowInsets.getInsets(WindowInsetsCompat.Type.ime())
+
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
-                leftMargin = insets.left
-                rightMargin = insets.right
-                topMargin = insets.top
-                bottomMargin = insets.bottom
+                leftMargin = systemBars.left
+                rightMargin = systemBars.right
+                topMargin = systemBars.top
+                bottomMargin = systemBars.bottom.coerceAtLeast(ime.bottom)
             }
             // Return CONSUMED if you don't want want the window insets to keep passing
             // down to descendant views.
