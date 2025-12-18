2da23dd35bb5e306e610e32013f64a32ce7f2697
markgray <mark.t.gray@gmail.com>
2025-12-18 08:24:36 -0500

    Updated kotlin to 2.3.0 and updated gradle dependency in FoldingLayout

--- FoldingLayout/app/build.gradle.kts
+++ FoldingLayout/app/build.gradle.kts
@@ -33,6 +33,6 @@ android {
     }
     namespace = "com.example.android.foldinglayout"
     dependencies {
-        implementation("androidx.activity:activity-ktx:1.12.1")
+        implementation("androidx.activity:activity-ktx:1.12.2")
     }
 }
