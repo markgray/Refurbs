4f301d6b8e4d42e3347b75d733be9971c53d95a5
markgray <mark.t.gray@gmail.com>
2025-12-20 07:14:10 -0500

    Updated kotlin to 2.3.0 and updated gradle dependency in JobScheduler

--- JobScheduler/Application/build.gradle.kts
+++ JobScheduler/Application/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.12.1")
+    implementation("androidx.activity:activity-ktx:1.12.2")
     api("androidx.legacy:legacy-support-v4:1.0.0")
     api("androidx.legacy:legacy-support-v13:1.0.0")
     api("androidx.cardview:cardview:1.0.0")
