e26d4f1f9e3d659ef0b354f2ac1d213e40203130
markgray <mark.t.gray@gmail.com>
2025-12-22 07:17:13 -0500

    Updated kotlin to 2.3.0 and updated gradle dependency in MultiWindowPlayground

--- MultiWindowPlayground/Application/build.gradle.kts
+++ MultiWindowPlayground/Application/build.gradle.kts
@@ -38,7 +38,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.12.1")
+    implementation("androidx.activity:activity-ktx:1.12.2")
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
 }
