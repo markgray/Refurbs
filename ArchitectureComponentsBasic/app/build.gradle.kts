79f4120b9e2acde6db41a6f36da8bed3d8ae424b
Mark Gray <mark.t.gray@gmail.com>
2025-12-22 01:14:32 -0500

    Updated kotlin to 2.3.0 and updated gradle dependency in ArchitectureComponentsBasic

--- ArchitectureComponentsBasic/app/build.gradle.kts
+++ ArchitectureComponentsBasic/app/build.gradle.kts
@@ -43,7 +43,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.12.1")
+    implementation("androidx.activity:activity-ktx:1.12.2")
     // Support libraries
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
