56d162cf7770b5986336f8f64d19e39cdf5c7b07
Mark Gray <mark.t.gray@gmail.com>
2025-12-22 05:09:03 -0500

    Updated kotlin to 2.3.0 and updated gradle dependency in LiveButton

--- LiveButton/app/build.gradle.kts
+++ LiveButton/app/build.gradle.kts
@@ -33,6 +33,6 @@ android {
     }
     namespace = "com.example.android.livebutton"
     dependencies {
-        implementation("androidx.activity:activity-ktx:1.12.1")
+        implementation("androidx.activity:activity-ktx:1.12.2")
     }
 }
