6847a92a18869fd479ff96c767e59ca40ff9aaf0
Mark Gray <mark.t.gray@gmail.com>
2025-12-22 22:55:34 -0500

    Updated gradle dependency in RoomRxJava

--- RoomRxJava/app/build.gradle.kts
+++ RoomRxJava/app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.12.1")
+    implementation("androidx.activity:activity-ktx:1.12.2")
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.10.0")
 
